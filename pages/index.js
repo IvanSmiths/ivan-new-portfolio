bf365240771862d17066b434b9955e37a9f55751
IvanSmiths <info@ivansmiths.com>
2021-11-21 20:44:34 +0100

    ms validate

--- pages/index.js
+++ pages/index.js
@@ -93,6 +93,10 @@ const Home = () => {
               type="application/ld+json"
               dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
             />
+            <meta
+              name="msvalidate.01"
+              content="4BBF8C331FF33E2E7BFD0EF4CDD32BEC"
+            />
           </Head>
           <Hero />
           <Marquee />
