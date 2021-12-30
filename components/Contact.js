a99777c57232b4532363641315b878f3f1f3ad16
IvanSmiths <info@ivansmiths.com>
2021-12-30 11:44:26 +0100

    kl

--- components/Contact.js
+++ components/Contact.js
@@ -26,57 +26,56 @@ function Contact() {
             <span className="medium-font">🙌</span>
           </div>
           <h2 className="medium-font highlight-main">form</h2>
-          <div className="form-cnt">
-            <form
-              action="https://formsubmit.co/info@ivansmiths.com"
-              method="POST"
-              className="form"
-            >
-              <input
-                type="hidden"
-                name="_next"
-                value="http://localhost:3000/message"
-              />
-              <input type="hidden" name="_subject" value="IvanSmiths | Info" />
-              <label htmlFor="name" className="small-font">
-                name
-              </label>
-              <input type="text" id="name" name="name" minLength="3" required />
-              <label htmlFor="object" className="small-font">
-                object
-              </label>
-              <input
-                type="text"
-                id="object"
-                name="object"
-                minLength="4"
-                required
-              />
-              <label htmlFor="email" className="small-font">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                minLength="6"
-                required
-              />
-              <label className="small-font" htmlFor="message">
-                message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                minLength="10"
-                cols="30"
-                rows="10"
-              ></textarea>
-              <button className="btn-contact" type="submit">
-                send
-              </button>
-            </form>
-          </div>
+
+          <form
+            action="https://formsubmit.co/info@ivansmiths.com"
+            method="POST"
+            className="form"
+          >
+            <input
+              type="hidden"
+              name="_next"
+              value="http://localhost:3000/message"
+            />
+            <input type="hidden" name="_subject" value="IvanSmiths | Info" />
+            <label htmlFor="name" className="small-font">
+              name
+            </label>
+            <input type="text" id="name" name="name" minLength="3" required />
+            <label htmlFor="object" className="small-font">
+              object
+            </label>
+            <input
+              type="text"
+              id="object"
+              name="object"
+              minLength="4"
+              required
+            />
+            <label htmlFor="email" className="small-font">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              minLength="6"
+              required
+            />
+            <label className="small-font" htmlFor="message">
+              message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              minLength="10"
+              cols="30"
+              rows="10"
+            ></textarea>
+            <button className="btn-contact" type="submit">
+              send
+            </button>
+          </form>
         </div>
       </div>
     </>
