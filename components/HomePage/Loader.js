71006814787d95c6c839f9ea7c9c24905dbef72d
Ivan <info@ivansmiths.com>
2022-05-23 21:07:18 +0100

    PUT (home) improve loader

--- components/HomePage/Loader.js
+++ components/HomePage/Loader.js
@@ -25,14 +25,11 @@ const itemMain = {
 };
 
 const titleMain = {
-  hidden: { opacity: 0, y: 200 },
+  hidden: { opacity: 0, y: 200, overflow: "hidden" },
   show: {
     opacity: 1,
     y: 0,
-    transition: {
-      ease: [0.6, 0.01, -0.05, 0.95],
-      duration: 3.2,
-    },
+    overflow: "hidden",
   },
 };
 
@@ -65,37 +62,54 @@ const Loader = ({ setLoading }) => {
       <motion.div variants={itemMain} className="loader-cnt">
         <motion.h2
           className="tiny-font spacing loader-sub-title"
-          layoutId="sub-title"
           variants={titleSub}
         >
           Ivan Smiths <br /> {t("common:web-react")} (React) <br /> UI/UX
           Designer
         </motion.h2>
-        <motion.h1
-          className="uppercase big-font loader-title"
-          layoutId="main-title"
-          variants={titleMain}
-        >
-          <em
-            style={{
-              paddingLeft: `${router.locale === "it" ? "10vw" : ""}`,
-            }}
-            className="loader-title-1"
-          >
-            {t("common:web-dev")} <br />
-          </em>
-          <em className="loader-title-2">
-            {t("common:web-dev-2")} <br />
-          </em>
-          <em
-            style={{
-              paddingLeft: `${router.locale === "it" ? "20vw" : ""}`,
-            }}
-            className="loader-title-3"
-          >
-            {t("common:web-dev-3")} <br />
-          </em>
-          <em className="loader-title-4">{t("common:web-dev-4")}...</em>
+        <motion.h1 className="uppercase big-font loader-title">
+          <div className="overflow">
+            <motion.em
+              variants={titleMain}
+              transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 2.0 }}
+              style={{
+                paddingLeft: `${router.locale === "it" ? "10vw" : ""}`,
+              }}
+              className="loader-title-1"
+            >
+              {t("common:web-dev")} <br />
+            </motion.em>
+          </div>
+          <div className="overflow">
+            <motion.em
+              transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 2.5 }}
+              variants={titleMain}
+              className="loader-title-2"
+            >
+              {t("common:web-dev-2")} <br />
+            </motion.em>
+          </div>
+          <div className="overflow">
+            <motion.em
+              transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 3.0 }}
+              variants={titleMain}
+              style={{
+                paddingLeft: `${router.locale === "it" ? "20vw" : ""}`,
+              }}
+              className="loader-title-3"
+            >
+              {t("common:web-dev-3")} <br />
+            </motion.em>
+          </div>
+          <div className="overflow">
+            <motion.em
+              transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 3.5 }}
+              variants={titleMain}
+              className="loader-title-4"
+            >
+              {t("common:web-dev-4")}...
+            </motion.em>
+          </div>
         </motion.h1>
       </motion.div>
     </motion.div>
