89289023e4df3efdadaeb0f193ed61001f072389
Ivan <info@ivansmiths.com>
2022-08-08 20:37:56 +0100

    add footer wip

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useContext } from "react";
+import Link from "next/link";
 import { motion } from "framer-motion";
 import { CursorContext } from "./CursorManager";
 import useTranslation from "next-translate/useTranslation";
@@ -20,10 +22,80 @@ const Footer = () => {
   let { t } = useTranslation();
 
   return (
-    <footer>
-      <div className="footer-first-row">
+    <footer className="footer">
+      <div className="footer__first-col">
+        <ul className="footer__icon">
+          <li>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/IvanSmiths"
+            >
+              <img
+                src="./instagram-icon.svg"
+                alt="instagram icon"
+                height="32px"
+                width="32px"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/ivan-fabbri/"
+            >
+              <img
+                src="./instagram-icon.svg"
+                alt="instagram icon"
+                height="32px"
+                width="32px"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/ivan_smiths/"
+            >
+              <img
+                src="./instagram-icon.svg"
+                alt="instagram icon"
+                height="32px"
+                width="32px"
+              />
+            </a>
+          </li>
+        </ul>
+        <a href="mailto:ivansmiths.com" className="medium-font">
+          info@ivansmiths.com
+        </a>
+        <p>
+          Frontend developer, with a particular attention to design and visual
+          experiences.
+        </p>
+      </div>
+      <div className="footer__second-col">
         <ul>
-          <li></li>
+          <li>{t("common:nav-home")}</li>
+          <li>
+            <div className="footer__vertical-links">
+              <Link href="/about">
+                <a>{t("common:nav-about")}</a>
+              </Link>
+              <Link href="/stuff">
+                <a>{t("common:nav-stuff")}</a>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <div className="footer__vertical-links">
+              <Link href="/post">
+                <a>{t("common:nav-post")}</a>
+              </Link>
+            </div>
+          </li>
         </ul>
       </div>
     </footer>
