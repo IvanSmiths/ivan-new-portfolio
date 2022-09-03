49bbf0a43fccd545cda41e0d79eb1eeef8943afa
Ivan <info@ivansmiths.com>
2022-09-03 15:54:35 +0100

    ADD mobile video

--- components/SUV/SuvFirstSection.js
+++ components/SUV/SuvFirstSection.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useContext } from "react";
+import React, { useRef, useEffect, useContext, useState } from "react";
 import Link from "next/link";
 import { gsap } from "gsap";
 import { motion } from "framer-motion";
@@ -34,6 +34,26 @@ function SuvFirstSection() {
   }, []);
 
   const refVideo = useRef(null);
+
+  const [isDesktop, setDesktop] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 768) {
+      setDesktop(true);
+    } else {
+      setDesktop(false);
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth > 768) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
   let { t } = useTranslation();
 
   return (
@@ -56,9 +76,15 @@ function SuvFirstSection() {
         className="case-studio__screen-image case-studio__screen-video suv-first-section"
       >
         <div className="case-studio__video" id="video">
-          <video muted autoPlay loop>
-            <source src="/wmf.webm" type="video/mp4" />
-          </video>
+          {isDesktop ? (
+            <video muted autoPlay loop>
+              <source src="/suv.mp4" type="video/mp4" />
+            </video>
+          ) : (
+            <video muted autoPlay loop>
+              <source src="/suv-mobile.mp4" type="video/mp4" />
+            </video>
+          )}
         </div>
       </div>
       <div className="case-studio-description">
