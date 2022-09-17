484fc4cd2bcc3ad19af9f03fa41e45f98c544912
Ivan <info@ivansmiths.com>
2022-09-17 09:11:37 +0100

    wip

--- pages/about.js
+++ pages/about.js
@@ -14,18 +14,18 @@ const About = () => {
   const refContainer = useRef(null);
   const refTextRoad = useRef(null);
   const ref2020 = useRef(null);
-  const ref2021 = useRef(null);
-  const ref2022 = useRef(null);
-  const firstRef = useRef(null);
-  const secondRef = useRef(null);
-  const thirdRef = useRef(null);
-  const fourthRef = useRef(null);
-  const fifthRef = useRef(null);
-  const sixthRef = useRef(null);
-  const seventhRef = useRef(null);
-  const eighthRef = useRef(null);
+  const firstWebsiteCopyRef = useRef(null);
+  const firstWebsiteImageRef = useRef(null);
+  const idImageRef = useRef(null);
+  const idCopyRef = useRef(null);
+  const reactImageRef = useRef(null);
+  const reactCopyRef = useRef(null);
+  const cgCopyRef = useRef(null);
+  const cgI = useRef(null);
   const ninethRef = useRef(null);
   const tenthRef = useRef(null);
+  const ref2021 = useRef(null);
+  const ref2022 = useRef(null);
 
   useEffect(() => {
     const delay = 1.5;
@@ -37,7 +37,7 @@ const About = () => {
       scrollTrigger: {
         trigger: refContainer.current,
         start: "top top",
-        end: "bottom bottom",
+        end: "+=6200px bottom",
         scrub: true,
         pin: true,
         markers: true,
@@ -45,11 +45,9 @@ const About = () => {
     });
 
     tl.to(refTextRoad.current, {
-      duration: `${durationScale}`,
       ease: "none",
       scale: 2,
       opacity: 0,
-      display: "none",
     });
 
     tl.to(ref2020.current, {
@@ -57,76 +55,53 @@ const About = () => {
         {
           opacity: 1,
           scale: 1.5,
-          display: "inline-block",
-          delay: `${delay}`,
-          duration: `${durationScale}`,
         },
         {
           opacity: 0,
-          display: "none",
-          duration: `${durationGlobal}`,
+
           scale: 2,
         },
       ],
     });
 
-    tl.to(secondRef.current, {
+    tl.to(firstWebsiteImageRef.current, {
       opacity: 1,
-      delay: `${delay}`,
-      duration: `${durationGlobal}`,
     });
 
-    tl.to(firstRef.current, {
+    tl.to(firstWebsiteCopyRef.current, {
       keyframes: [
-        { opacity: 1, duration: `${durationGlobal}` },
+        { opacity: 1 },
         {
           opacity: 0,
-          display: "none",
-          duration: `${durationGlobal}`,
-          delay: `${delaySection}`,
         },
       ],
     });
 
-    tl.to(secondRef.current, {
+    tl.to(firstWebsiteImageRef.current, {
       opacity: 0,
-      display: "none",
-      duration: `${durationGlobal}`,
     });
 
-    tl.to(fourthRef.current, {
+    tl.to(idCopyRef.current, {
       opacity: 1,
-      duration: `${durationGlobal}`,
-      delay: `${delay}`,
     });
 
-    tl.to(thirdRef.current, {
-      keyframes: [
-        { opacity: 1, duration: `${durationGlobal}` },
-        { opacity: 0, duration: `${durationGlobal}` },
-      ],
+    tl.to(idImageRef.current, {
+      keyframes: [{ opacity: 1 }, { opacity: 0 }],
     });
 
-    tl.to(fourthRef.current, {
+    tl.to(idCopyRef.current, {
       opacity: 0,
-      duration: `${durationGlobal}`,
     });
-    tl.to(fifthRef.current, {
+    tl.to(reactImageRef.current, {
       opacity: 1,
-      duration: `${durationGlobal}`,
-      delay: `${delay}`,
     });
 
-    tl.to(sixthRef.current, {
-      keyframes: [
-        { opacity: 1, duration: `${durationGlobal}` },
-        { opacity: 0, duration: `${durationGlobal}` },
-      ],
+    tl.to(reactCopyRef.current, {
+      keyframes: [{ opacity: 1 }, { opacity: 0 }],
     });
 
-    tl.to(fifthRef.current, {
+    tl.to(reactImageRef.current, {
       opacity: 0,
-      duration: `${durationGlobal}`,
     });
 
     tl.to(ref2021.current, {
@@ -134,68 +109,48 @@ const About = () => {
         {
           opacity: 1,
           scale: 1.5,
-          delay: `${delay}`,
-          display: "inline-block",
-          duration: `${durationScale}`,
         },
         {
           opacity: 0,
-          duration: `${durationGlobal}`,
+
           scale: 2,
-          display: "none",
         },
       ],
     });
-    tl.to(eighthRef.current, {
+    tl.to(cgI.current, {
       opacity: 1,
-      duration: `${durationGlobal}`,
-      delay: `${delay}`,
     });
 
-    tl.to(seventhRef.current, {
-      keyframes: [
-        { opacity: 1, duration: `${durationGlobal}` },
-        { opacity: 0, duration: `${durationGlobal}` },
-      ],
+    tl.to(cgCopyRef.current, {
+      keyframes: [{ opacity: 1 }, { opacity: 0 }],
     });
 
-    tl.to(eighthRef.current, {
+    tl.to(cgI.current, {
       opacity: 0,
-      duration: `${durationGlobal}`,
     });
     tl.to(ref2022.current, {
       keyframes: [
         {
           opacity: 1,
           scale: 1.5,
-          display: "inline-block",
-          delay: `${delay}`,
-          duration: `${durationScale}`,
         },
         {
           opacity: 0,
-          display: "none",
-          duration: `${durationGlobal}`,
+
           scale: 2,
         },
       ],
     });
     tl.to(ninethRef.current, {
       opacity: 1,
-      duration: `${durationGlobal}`,
-      delay: `${delay}`,
     });
 
     tl.to(tenthRef.current, {
-      keyframes: [
-        { opacity: 1, duration: `${durationGlobal}` },
-        { opacity: 0, duration: `${durationGlobal}` },
-      ],
+      keyframes: [{ opacity: 1 }, { opacity: 0 }],
     });
 
     tl.to(ninethRef.current, {
       opacity: 0,
-      duration: `${durationGlobal}`,
     });
   }, []);
   return (
@@ -214,135 +169,128 @@ const About = () => {
       </header>
       <main>
         <div ref={refContainer} className="about__container">
-          <div className="about__roadmap">
-            <div className="about__roadmap__first-text">
-              <h2 ref={refTextRoad}>ROADMAP</h2>
-              <h3 ref={ref2020}>2020</h3>
-              <img
-                ref={secondRef}
-                className="about__absolute-right about__opacity"
-                src="ivan-smiths-first-website.jpg"
-                alt=""
-              />
-              <div
-                ref={firstRef}
-                className="about__roadmap__copy about__absolute-left about__opacity"
+          <h2 ref={refTextRoad}>ROADMAP</h2>
+          <h3 ref={ref2020}>2020</h3>
+          <img
+            ref={firstWebsiteImageRef}
+            className="about__absolute-right about__opacity"
+            src="ivan-smiths-first-website.jpg"
+            alt=""
+          />
+          <div
+            ref={firstWebsiteCopyRef}
+            className="about__roadmap__copy about__absolute-left about__opacity"
+          >
+            <h4 className="small-font">01 / PASSION</h4>
+            <h5 className="medium-font">My journey begins</h5>
+            <p>
+              On 2020 my first website went online. My old portfolio was done in
+              plain HTML, Css and JavaScript. Is still hosted on Netlify and it
+              containes some really spooky designs.
+            </p>
+            <div className="about__roadmap__copy-link">
+              <a
+                href="https://ivansmiths.netlify.app/"
+                target="_blank"
+                className="btn-small"
+                rel="noreferrer noopener"
               >
-                <h4 className="small-font">01 / PASSION</h4>
-                <h5 className="medium-font">My journey begins</h5>
-                <p>
-                  On 2020 my first website went online. My old portfolio was
-                  done in plain HTML, Css and JavaScript. Is still hosted on
-                  Netlify and it containes some really spooky designs.
-                </p>
-                <div className="about__roadmap__copy-link">
-                  <a
-                    href="https://ivansmiths.netlify.app/"
-                    target="_blank"
-                    className="btn-small"
-                    rel="noreferrer noopener"
-                  >
-                    See more
-                  </a>
-                </div>
-              </div>
-              <div
-                ref={fourthRef}
-                className="about__roadmap__copy about__absolute-left about__opacity"
-              >
-                <h4 className="small-font">02 / DESING & FRONTEND</h4>
-                <h5 className="medium-font">
-                  First little steps in the Tech industry
-                </h5>
-                <p>
-                  My first working experience was mainly as a UI/UX designer,
-                  but i had the chance of showing my frontend skills, using Css
-                  and jQuery.
-                </p>
-                <div className="about__roadmap__copy-link">
-                  <Link href="/stuff/ideology">
-                    <a className="btn-small">See more</a>
-                  </Link>
-                </div>
-              </div>
-              <img
-                ref={thirdRef}
-                className="about__absolute-right about__opacity"
-                src="ideology-website-mobile-15.png"
-                alt=""
-              />
-              <div
-                ref={sixthRef}
-                className="about__roadmap__copy about__absolute-left about__opacity"
-              >
-                <h4 className="small-font">03 / REACT</h4>
-                <h5 className="medium-font">
-                  Getting inside the JAMStack world
-                </h5>
-                <p>
-                  As soon as i discovered React and Next.js, i felt in love with
-                  this technologies, and i remade my website from scratch using
-                  them.
-                </p>
-              </div>
-              <img
-                ref={fifthRef}
-                className="about__absolute-right about__opacity"
-                src="ivan-smiths-mobile.jpg"
-                alt=""
-              />
+                See more
+              </a>
             </div>
-            <h3 ref={ref2021}>2021</h3>
+
             <div
-              ref={seventhRef}
+              ref={idCopyRef}
               className="about__roadmap__copy about__absolute-left about__opacity"
             >
-              <h4 className="small-font">04 / PERSONAL WORK</h4>
-              <h5 className="medium-font">CG Prospect was born</h5>
-              <p>
-                With a more &quot;corporate like&quot; design, CG Prospect is a
-                website focused on performances and Seo, where i freely share my
-                3d models to the people.
-              </p>
-              <div className="about__roadmap__copy-link">
-                <Link href="/stuff/cg-prospect">
-                  <a className="btn-small">See more</a>
-                </Link>
-              </div>
-            </div>
-            <img
-              ref={eighthRef}
-              className="about__absolute-right about__opacity"
-              src="cg-prospect-mobile.jpg"
-              alt=""
-            />
-            <h3 ref={ref2022}>2022</h3>
-            <div
-              ref={ninethRef}
-              className="about__roadmap__copy about__absolute-left about__opacity"
-            >
-              <h4 className="small-font">05 / FRONTEND</h4>
+              <h4 className="small-font">02 / DESING & FRONTEND</h4>
               <h5 className="medium-font">
-                Rocking with Vue (Nuxt.js) and Gsap
+                First little steps in the Tech industry
               </h5>
               <p>
-                As soon as i joined S&V, I started to work on various components
-                and animations, using Gsap for building animation and Vuex as a
-                state manager.
+                My first working experience was mainly as a UI/UX designer, but
+                i had the chance of showing my frontend skills, using Css and
+                jQuery.
               </p>
               <div className="about__roadmap__copy-link">
-                <Link href="/stuff/scholz-und-volkmer">
+                <Link href="/stuff/ideology">
                   <a className="btn-small">See more</a>
                 </Link>
               </div>
             </div>
             <img
-              ref={tenthRef}
+              ref={idImageRef}
               className="about__absolute-right about__opacity"
-              src="scholz-und-volkmer-website-1.png"
+              src="ideology-website-mobile-15.png"
+              alt=""
+            />
+            <div
+              ref={reactCopyRef}
+              className="about__roadmap__copy about__absolute-left about__opacity"
+            >
+              <h4 className="small-font">03 / REACT</h4>
+              <h5 className="medium-font">Getting inside the JAMStack world</h5>
+              <p>
+                As soon as i discovered React and Next.js, i felt in love with
+                this technologies, and i remade my website from scratch using
+                them.
+              </p>
+            </div>
+            <img
+              ref={reactImageRef}
+              className="about__absolute-right about__opacity"
+              src="ivan-smiths-mobile.jpg"
               alt=""
             />
           </div>
+          <h3 ref={ref2021}>2021</h3>
+          <div
+            ref={cgCopyRef}
+            className="about__roadmap__copy about__absolute-left about__opacity"
+          >
+            <h4 className="small-font">04 / PERSONAL WORK</h4>
+            <h5 className="medium-font">CG Prospect was born</h5>
+            <p>
+              With a more &quot;corporate like&quot; design, CG Prospect is a
+              website focused on performances and Seo, where i freely share my
+              3d models to the people.
+            </p>
+            <div className="about__roadmap__copy-link">
+              <Link href="/stuff/cg-prospect">
+                <a className="btn-small">See more</a>
+              </Link>
+            </div>
+          </div>
+          <img
+            ref={cgI}
+            className="about__absolute-right about__opacity"
+            src="cg-prospect-mobile.jpg"
+            alt=""
+          />
+          <h3 ref={ref2022}>2022</h3>
+          <div
+            ref={ninethRef}
+            className="about__roadmap__copy about__absolute-left about__opacity"
+          >
+            <h4 className="small-font">05 / FRONTEND</h4>
+            <h5 className="medium-font">Rocking with Vue (Nuxt.js) and Gsap</h5>
+            <p>
+              As soon as i joined S&V, I started to work on various components
+              and animations, using Gsap for building animation and Vuex as a
+              state manager.
+            </p>
+            <div className="about__roadmap__copy-link">
+              <Link href="/stuff/scholz-und-volkmer">
+                <a className="btn-small">See more</a>
+              </Link>
+            </div>
+          </div>
+          <img
+            ref={tenthRef}
+            className="about__absolute-right about__opacity"
+            src="scholz-und-volkmer-website-1.png"
+            alt=""
+          />
         </div>
       </main>
     </>
