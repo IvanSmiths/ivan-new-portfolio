3014e4a7e4bbb416d3afa74bb6c59c8ecf9d0af4
Ivan <info@ivansmiths.com>
2022-10-13 16:54:25 +0100

    remove motion marquee

--- components/Ideology/Marquee.js
+++ components/Ideology/Marquee.js
@@ -1,34 +1,11 @@
 import React from "react";
-import { motion } from "framer-motion";
 import useTranslation from "next-translate/useTranslation";
-
-const marqueeVariants = {
-  animate: {
-    x: [0, -5735],
-    transition: {
-      x: {
-        repeat: Infinity,
-        repeatType: "loop",
-        duration: 96,
-        ease: "linear",
-      },
-    },
-  },
-};
 
 const Marquee = () => {
   let { t } = useTranslation();
   return (
-    <div className="marq ">
-      <div className="marquee">
-        <motion.div
-          className="track"
-          variants={marqueeVariants}
-          animate="animate"
-        >
-          <em className="impact big-font">{t("common:marquee")}</em>
-        </motion.div>
-      </div>
+    <div className="marquee">
+      <em className="impact large-font">{t("common:marquee")}</em>
     </div>
   );
 };
