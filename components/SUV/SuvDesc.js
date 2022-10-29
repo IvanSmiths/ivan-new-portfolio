9354e29052faa74d8adf59d3ce4ad67137793a4a
Ivan <info@ivansmiths.com>
2022-10-29 18:44:28 +0100

    add mobile image

--- components/SUV/SuvDesc.js
+++ components/SUV/SuvDesc.js
@@ -3,22 +3,23 @@ import SrcImage from "../SrcImage";
 
 function SuvDesc() {
   return (
-    <div className="cs__desc">
-      <div className="cs__desc-image flex-center">
-        <SrcImage
-          src="/scholz-und-volkmer-website-3.jpg"
-          webp="/scholz-und-volkmer-website-3.webp"
-          height="970px"
-          width="1920px"
-          alt="image of a website"
-        />
+    <div className="cs__desc-outer">
+      <div className="cs__desc-inner">
+        <div className="cs__desc-image flex-center">
+          <SrcImage
+            src="/scholz-und-volkmer-website-1.jpg"
+            webp="/scholz-und-volkmer-website-1.webp"
+            height="970px"
+            width="1920px"
+            alt="image of a website"
+          />
+        </div>
+        <ul className="cs__desc-text">
+          <li className="medium-font">
+            Stack: Vue.js (Nuxt.js), TypeScript, Gsap
+          </li>
+        </ul>
       </div>
-      <ul className="cs__desc-text">
-        <li>Frontend: Vue.js (Nuxt.js), TypeScript, Gsap</li>
-        <li>
-          Date: 12/03/2022 <br /> Current
-        </li>
-      </ul>
     </div>
   );
 }
