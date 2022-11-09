a4edb1327c8495371b01c56d54167fe47672b08a
Ivan <info@ivansmiths.com>
2022-11-09 21:12:35 +0100

    fix cursor error

--- components/SUV/SuvTextBlock.js
+++ components/SUV/SuvTextBlock.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import useTranslation from "next-translate/useTranslation";
 import { CursorContext } from "../CursorManager";
 function SuvTextBlock() {
