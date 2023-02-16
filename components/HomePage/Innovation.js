51942b76b04b7702594f0b5f3756bdb0cc725d0c
Ivan <info@ivansmiths.com>
2023-02-16 12:08:33 +0100

    reduce animation time

--- components/HomePage/Innovation.js
+++ components/HomePage/Innovation.js
@@ -33,7 +33,7 @@ function Innovation() {
             scrollTrigger: {
               trigger: innovationTriggerRef.current,
               start: "center center",
-              end: isDesktop ? "8000 top" : "4000 center",
+              end: isDesktop ? "5000 top" : "4000 center",
               scrub: true,
               pin: true,
             },
