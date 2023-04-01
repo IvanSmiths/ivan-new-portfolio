aeefbcaa454df5db146bda2aa374263dac5219b9
IvanSmiths <info@ivansmiths.com>
2023-04-01 10:21:22 +0200

    add youtube & refactor icons

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,7 @@ import { gsap } from "gsap";
 import Link from "next/link";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useRouter } from "next/router";
+import { icons } from "../utils/icons";
 
 function Footer({ link }) {
   gsap.registerPlugin(ScrollTrigger);
@@ -71,6 +72,8 @@ function Footer({ link }) {
     };
   }, [router, complete, link]);
 
+  const iconWidth = "28px"
+
   return (
     <>
       <footer>
@@ -78,54 +81,17 @@ function Footer({ link }) {
           <div className="footer-counter-inner">
             <div className="footer-counter__contact">
               <ul className="footer__icon">
-                <li>
-                  <a
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/IvanSmiths"
-                  >
-                    <img
-                      src="/github-icon.svg"
-                      alt="github icon"
-                      height="25px"
-                      width="25px"
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://www.linkedin.com/in/ivan-fabbri/"
-                  >
-                    <img
-                      src="/linkedin-icon.svg"
-                      alt="linkedin icon"
-                      height="25px"
-                      width="25px"
-                    />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://twitter.com/IvanSmiths"
-                  >
-                    <img
-                      src="/icon-twitter.svg"
-                      alt="twitter icon"
-                      height="25px"
-                      width="25px"
-                    />
-                  </a>
-                </li>
+                {icons.map((icon) => (
+                  <li>
+                    <a href={icon.link}
+                      onMouseEnter={handleMouseEnter}
+                      onMouseLeave={handleMouseLeave}
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      <img src={icon.icon} alt={icon.alt} height={iconWidth} width={iconWidth} />
+                    </a>
+                  </li>
+                ))}
               </ul>
               <div>
                 <a
---
+++ utils/icons.js
@@ -0,0 +1,22 @@
+export const icons = [
+    {
+        link: "https://github.com/IvanSmiths",
+        icon: "/github-icon.svg",
+        alt: "github icon"
+    },
+    {
+        link: "https://www.linkedin.com/in/ivan-fabbri/",
+        icon: "/linkedin-icon.svg",
+        alt: "linkedin icon"
+    },
+    {
+        link: "https://twitter.com/IvanSmiths",
+        icon: "/icon-twitter.svg",
+        alt: "twitter icon"
+    },
+    {
+        link: "https://www.youtube.com/@ivansmiths",
+        icon: "/icon-youtube.svg",
+        alt: "youtube icon"
+    },
+]
