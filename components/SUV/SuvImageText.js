9ba9c1822053e90dbccf69406f74c51da01e3596
IvanSmiths <info@ivansmiths.com>
2023-04-01 10:57:42 +0200

    remove unused code

--- components/SUV/SuvImageText.js
+++ components/SUV/SuvImageText.js
@@ -1,24 +1,8 @@
-import React from "react";
-import useTranslation from "next-translate/useTranslation";
 import SrcImage from "../SrcImage";
 
 function SuvTextImage() {
-  let { t } = useTranslation();
-
   return (
     <>
-      {/*
-      <div className="case-studio-description case-studio-description-first">
-        <div className="case-studio-description__first-column">
-          <h2 className="small-font">
-            02 / <span>TypeScript</span>
-          </h2>
-          <h3 className="medium-font">{t("suv:cs-1-h")}</h3>
-        </div>
-        <div className="case-studio-description__second-column">
-          <p>{t("suv:cs-1-p")}</p>
-        </div>
-  </div> */}
       <div className="case-studio__images margin-top">
         <div className="case-studio__screen-image">
           <SrcImage
