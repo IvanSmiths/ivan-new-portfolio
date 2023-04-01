758164a9e1fc20de7dfc94e0460c016a0b409545
IvanSmiths <info@ivansmiths.com>
2023-04-01 11:59:49 +0200

    add opacity staggered animation

--- components/HomePage/Loader.js
+++ components/HomePage/Loader.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 import useTranslation from "next-translate/useTranslation";
+import { gsap } from "gsap";
 
 const Loader = ({ setHasVisited }) => {
   useEffect(() => {
@@ -11,6 +12,33 @@ const Loader = ({ setHasVisited }) => {
   }, [setHasVisited]);
 
   let { t } = useTranslation();
+  const [lettersRef, setlettersRef] = useArrayRef();
+
+  function useArrayRef() {
+    const lettersRef = useRef([]);
+    lettersRef.current = [];
+    return [lettersRef, (ref) => ref && lettersRef.current.push(ref)];
+  }
+
+  const text = t("common:web-dev");
+  const text2 = t("common:web-dev-2");
+  const text3 = t("common:web-dev-3");
+  const text4 = t("common:web-dev-4");
+
+  useEffect(() => {
+    const anim = gsap.to(
+      lettersRef.current,
+      {
+        opacity: 1,
+        translateY: 0,
+        duration: 1,
+        stagger: 0.08,
+      }
+    );
+    return (() => {
+      anim.kill()
+    })
+  }, []);
 
   return (
     <div className="loader">
@@ -21,26 +49,36 @@ const Loader = ({ setHasVisited }) => {
         </h1>
         <div className="uppercase big-font loader-title">
           <div className="overflow loader__header">
-            <em>
-              {t("common:web-dev")} <br />
-            </em>
+            {text.split("").map((letter, index) => (
+              <em key={index} ref={setlettersRef}>
+                {letter}
+              </em>
+            ))}
           </div>
           <div className="overflow loader__header">
-            <em>
-              {t("common:web-dev-2")} <br />
-            </em>
+            {text2.split("").map((letter, index) => (
+              <em key={index} ref={setlettersRef}>
+                {letter}
+              </em>
+            ))}
           </div>
           <div className="overflow loader__header">
-            <em>
-              {t("common:web-dev-3")} <br />
-            </em>
+            {text3.split("").map((letter, index) => (
+              <em key={index} ref={setlettersRef}>
+                {letter}
+              </em>
+            ))}
           </div>
           <div className="overflow loader__header">
-            <em>{t("common:web-dev-4")}...</em>
+            {text4.split("").map((letter, index) => (
+              <em key={index} ref={setlettersRef}>
+                {letter}
+              </em>
+            ))}
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
