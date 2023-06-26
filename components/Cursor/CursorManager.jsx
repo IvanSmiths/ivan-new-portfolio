cf3f3382163e3e646f7479466530965b6c5ada9b
Ivan Smiths <info@ivansmiths.com>
2023-06-26 21:48:27 +0200

    add state for image show on hover

--- components/Cursor/CursorManager.jsx
+++ components/Cursor/CursorManager.jsx
@@ -3,11 +3,14 @@ import React, { createContext, useState } from 'react';
 export const CursorContext = createContext({
   size: 'small',
   setSize: () => {},
+  visible: false,
+  setVisible: () => {},
 });
 export default function CursorManager(props) {
   const [size, setSize] = useState('small');
+  const [visible, setVisible] = useState(false);
   return (
-    <CursorContext.Provider value={{ size, setSize }}>
+    <CursorContext.Provider value={{ size, setSize, visible, setVisible }}>
       {props.children}
     </CursorContext.Provider>
   );
