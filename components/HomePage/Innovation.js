531cd381b8a88e3a75f532b91e2bafebc9010f03
Ivan Smiths <info@ivansmiths.com>
2023-07-14 18:12:52 +0200

    add dep array

--- components/HomePage/Innovation.js
+++ components/HomePage/Innovation.js
@@ -1,47 +1,47 @@
-import React, { useRef, useEffect } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import React, {useRef, useEffect} from "react";
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
 
 function Innovation() {
-  const innovationRef = useRef(null);
-  const innovationTriggerRef = useRef(null);
-  gsap.registerPlugin(ScrollTrigger);
+    const innovationRef = useRef(null);
+    const innovationTriggerRef = useRef(null);
+    gsap.registerPlugin(ScrollTrigger);
 
-  useEffect(() => {
+    useEffect(() => {
 
-    const innHero = gsap.fromTo(
-      innovationRef.current,
-      {
-        scale: 1,
-        translateX: "14.51%",
-      },
-      {
-        scale: 0.1,
-        translateX: 0,
-        ease: "expo.inOut",
-        scrollTrigger: {
-          trigger: innovationTriggerRef.current,
-          start: "center center",
-          end: "5000 top",
-          scrub: true,
-          pin: true,
-        },
-      }
+            const innHero = gsap.fromTo(
+                innovationRef.current,
+                {
+                    scale: 1,
+                    translateX: "14.51%",
+                },
+                {
+                    scale: 0.1,
+                    translateX: 0,
+                    ease: "expo.inOut",
+                    scrollTrigger: {
+                        trigger: innovationTriggerRef.current,
+                        start: "center center",
+                        end: "5000 top",
+                        scrub: true,
+                        pin: true,
+                    },
+                }
+            );
+            return () => {
+                innHero.kill();
+            };
+        }, []
     );
-    return () => {
-      innHero.kill();
-    };
-  }
-  );
-  return (
-    <div>
-      <div ref={innovationTriggerRef} className="inn-outer">
-        <h2 ref={innovationRef} className="extreme-font">
-          INNOVATION
-        </h2>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div ref={innovationTriggerRef} className="inn-outer">
+                <h2 ref={innovationRef} className="extreme-font">
+                    INNOVATION
+                </h2>
+            </div>
+        </div>
+    );
 }
 
 export default Innovation;
