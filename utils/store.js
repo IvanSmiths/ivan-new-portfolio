e73de9a39fd182935d462978c5d509e65af64703
Ivan Smiths <info@ivansmiths.com>
2023-07-14 18:03:55 +0200

    remove test state for scrolling

--- utils/store.js
+++ utils/store.js
@@ -3,6 +3,4 @@ import {create} from 'zustand'
 export const useHoverStore = create(set => ({
     hover: "small",
     setHover: (newState) => set({hover: newState}),
-    userScroll: 0,
-    setUserScroll: (userScroll) => set({setUserScroll: userScroll}),
 }))
