d138dd036fcb0ef10b7f19da7f532f1be2d4aec3
Ivan Smiths <info@ivansmiths.com>
2023-07-15 08:45:44 +0200

    add grid and grid debug

---
+++ components/GridDebug.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function GridDebug() {
+    return (
+        <div className="grid-debug">
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+            <div className="grid-debug-item"></div>
+        </div>
+    );
+}
+
+export default GridDebug;
