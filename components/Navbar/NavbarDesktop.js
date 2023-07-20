c938db66897343d98a9788510485a899befb17cd
Ivan Smiths <info@ivansmiths.com>
2023-07-20 20:51:16 +0200

    add tailwind to desktop navbar

--- components/Navbar/NavbarDesktop.js
+++ components/Navbar/NavbarDesktop.js
@@ -6,37 +6,37 @@ const ThemeToggle = dynamic(() => import("../ThemeToggle"), {
     ssr: false,
 });
 
+const links = [
+    {
+        page: "home",
+        link: "/"
+    }, {
+        page: "what i do",
+        link: "/stuff"
+    }, {
+        page: "what i write",
+        link: "/blog"
+    },
+]
+
 function NavbarDesktop() {
     const {setHover} = useHoverStore();
 
-    return (<nav className="navbar-desktop">
-        <ul className="navbar-desktop__first-list">
-            <li>
-                <Link className="navbar__logo"
-                      onMouseEnter={() => setHover('medium')}
-                      onMouseLeave={() => setHover('small')} href="/">
-                    home
-                </Link>
-            </li>
-            <li>
-                <Link onMouseEnter={() => setHover('medium')}
-                      onMouseLeave={() => setHover('small')} href="/stuff">
-                    what i do
-                </Link>
-            </li>
-        </ul>
-        <ul className="navbar-desktop__second-list">
-            <li>
-                <Link onMouseEnter={() => setHover('medium')}
-                      onMouseLeave={() => setHover('small')} href="/blog">
-                    what i write
-                </Link>
-            </li>
-            <li>
-                <ThemeToggle/>
-            </li>
-        </ul>
-    </nav>)
+    return (
+        <nav className="fixed z-40 w-full top-small left-small">
+            <ul className="flex gap-medium">
+                {links.map((link) => (
+                    <li key={link.index}>
+                        <Link href={link.link} onMouseEnter={() => setHover('medium')}
+                              onMouseLeave={() => setHover('small')}>
+                            {link.page}
+                        </Link>
+                    </li>
+                ))}
+                <li><ThemeToggle/></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NavbarDesktop
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "primary-color": "var(--primary-color)",
+            },
+            spacing: {
+                "small": "var(--small)",
+                "medium": "var(--medium)",
+            },
+        },
+    },
+    plugins: [],
 }
