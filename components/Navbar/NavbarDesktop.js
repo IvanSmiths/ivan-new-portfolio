b73498deadae8585d907a8998fc1fa0d18b57a6d
Ivan Smiths <info@ivansmiths.com>
2023-07-20 20:36:47 +0200

    remove logo

--- components/Navbar/NavbarDesktop.js
+++ components/Navbar/NavbarDesktop.js
@@ -15,18 +15,6 @@ function NavbarDesktop() {
                 <Link className="navbar__logo"
                       onMouseEnter={() => setHover('medium')}
                       onMouseLeave={() => setHover('small')} href="/">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20px"
-                        height="20px"
-                        viewBox="0 0 45 45"
-                    >
-                        <g id="logo" fill="none" strokeWidth="6">
-                            <circle cx="22.5" cy="22.5" r="22.5" stroke="none"/>
-                            <circle cx="22.5" cy="22.5" r="19.5" fill="none"/>
-                        </g>
-                    </svg>
-                    <span className="navbar__logo-line">/</span>
                     home
                 </Link>
             </li>
