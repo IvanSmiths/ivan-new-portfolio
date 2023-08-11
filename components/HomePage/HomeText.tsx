8c469dd51eba4d663312699bc5b969dbea39b6fc
Ivan Smiths <info@ivansmiths.com>
2023-08-11 16:11:03 +0200

    add tablet media query

--- components/HomePage/HomeText.tsx
+++ components/HomePage/HomeText.tsx
@@ -5,9 +5,9 @@ function HomeText() {
             <div className="grid-column-8-11 heading-2 flex flex-wrap items-center pb-small">
                 <p className="heading-2">
                     <strong>ivan smiths.</strong> frontend ui/ux developer. 3 years of experience. seeking the limit.
-                    currently at <a className="heading-2-link" href="https://www.cowen.com/" target="_blank"
-                                    rel="noreferrer noopener"> td
-                    cowen.</a>
+                    currently at
+                    <a className="heading-2-link" href="https://www.cowen.com/" target="_blank"
+                       rel="noreferrer noopener"> td cowen.</a>
                 </p>
             </div>
         </>
