b40d22beb450ba760dfd2b71ddf671c765424774
Ivan Smiths <info@ivansmiths.com>
2023-08-12 21:36:45 +0200

    adjust css values

--- components/HomePage/HomeWorks.js
+++ components/HomePage/HomeWorks.js
@@ -170,8 +170,8 @@ function HomeWorks() {
     return (
         <section>
             <div ref={pinRef}
-                 className="w-full z-10 sm:h-2/4 h-[70%] sm:absolute max-sm:fixed bottom-0 overflow-hidden">
-                <ul className="absolute w-5/6 sm:w-3/4 md:w-2/4 h-full top-0 sm:left-2/4 left-small-mobile sm:-translate-x-2/4 -translate-x-0">
+                 className="w-full z-10 sm:h-2/4 h-[67%] sm:absolute max-sm:fixed bottom-0 overflow-hidden">
+                <ul className="absolute mt-small-mobile md:mt-0 w-5/6 sm:w-3/4 md:w-2/4 h-full top-0 sm:left-2/4 left-small-mobile sm:-translate-x-2/4 -translate-x-0">
                     {works.map((work, index) => (
                         <HomeWork key={index} title={work.title} link={work.link}
                                   img={work.img}
