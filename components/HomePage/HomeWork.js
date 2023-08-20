1ab7861d1a1a4590b2315aa75c308e4a22332095
Ivan Smiths <info@ivansmiths.com>
2023-08-20 14:07:02 +0200

    center image

--- components/HomePage/HomeWork.js
+++ components/HomePage/HomeWork.js
@@ -17,7 +17,7 @@ function HomeWork({setProjectsRef, title, img, link}) {
     return (
         <li ref={setProjectsRef} className='pb-0 sm:pb-small pt-small sm:pt-0 pl-small sm:pl-0 opacity-0 m-0 w-full h-full flex justify-center
             items-center text-center absolute top-0 left-0'>
-            <Link className="h-full w-full" href={link}>
+            <Link className="h-full w-full sm:flex sm:justify-center" href={link}>
                 {hovered === true ? <>
                     <h1 className="text-white absolute bottom-small left-small text-xs">
                         {title}
