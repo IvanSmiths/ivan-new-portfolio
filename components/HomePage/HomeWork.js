784c083e332734792cf933957db7706437ff2f75
Ivan Smiths <info@ivansmiths.com>
2023-08-29 21:49:24 +0200

    add role

--- components/HomePage/HomeWork.js
+++ components/HomePage/HomeWork.js
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import {useState} from "react";
 
-function HomeWork({setProjectsRef, title, img, link}) {
+function HomeWork({setProjectsRef, title, img, link, role}) {
     const [hovered, setHovered] = useState(false)
 
     const handleMouseEnter = () => {
@@ -18,9 +18,15 @@ function HomeWork({setProjectsRef, title, img, link}) {
         <li ref={setProjectsRef} className='pb-0 sm:pb-small pt-small sm:pt-0 pl-small sm:pl-0 opacity-0 m-0 w-full h-full flex justify-center
             items-center text-center absolute top-0 left-0'>
             <Link className="relative h-full w-full sm:flex sm:justify-center" href={link}>
-                <h2 className="paragraph absolute bottom-small left-small">
-                    {title}
-                </h2>
+                <div className="absolute bottom-small left-small text-left -mb-3">
+                    <h2 className="paragraph -mb-2">
+                        {role}
+                    </h2>
+                    <h3 className="heading-1-bold">
+                        {title}
+                    </h3>
+                </div>
+
                 <img onMouseEnter={handleMouseEnter}
                      onMouseLeave={handleMouseLeave}
                      src={img} alt={title} className='w-full h-full object-cover'/>
