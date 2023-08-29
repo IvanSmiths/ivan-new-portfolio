629f15fbc10e9424a1c74fefe0d54fe050002de9
Ivan Smiths <info@ivansmiths.com>
2023-08-29 21:48:48 +0200

    pass role as prop

--- components/HomePage/HomeWorks.js
+++ components/HomePage/HomeWorks.js
@@ -175,8 +175,7 @@ function HomeWorks() {
                  className="w-full z-10 sm:h-2/4 h-[67%] sm:absolute max-sm:fixed bottom-0 overflow-hidden">
                 <ul className="absolute mt-small-mobile md:mt-0 w-5/6 sm:w-3/4 md:w-[48%] h-full top-0 sm:left-2/4 left-small-mobile sm:-translate-x-2/4 -translate-x-0">
                     {works.map((work, index) => (
-                        <HomeWork key={index} title={work.title} link={work.link}
-                                  img={work.img}
+                        <HomeWork key={index} title={work.title} link={work.link} img={work.img} role={work.role}
                                   setProjectsRef={setProjectsRef}/>
                     ))}
                 </ul>
