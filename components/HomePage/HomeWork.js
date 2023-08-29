4af1102930fb7d2264d9e54bb6cfc56d889b3c65
Ivan Smiths <info@ivansmiths.com>
2023-08-29 22:42:19 +0200

    add heading animation

--- components/HomePage/HomeWork.js
+++ components/HomePage/HomeWork.js
@@ -6,13 +6,20 @@ import gsap from "gsap"
 
 function HomeWork({setProjectsRef, title, img, link, role}) {
     const trigger = useRef(null);
-    const myDiv = useRef(null);
+    const h2 = useRef(null);
+    const h3 = useRef(null);
 
     useEffect(() => {
         const tl = gsap.timeline({paused: true});
-        tl.fromTo(myDiv.current,
-            {opacity: 0},
-            {opacity: 1, duration: 0.2});
+        tl.to(h2.current,
+            {
+                bottom: 0,
+                duration: 0.2
+            }).to(h3.current,
+            {
+                bottom: 0,
+                duration: 0.2
+            })
 
         function handleMouseEnter() {
             tl.play();
@@ -32,18 +39,21 @@ function HomeWork({setProjectsRef, title, img, link, role}) {
         };
     }, []);
 
-
     return (
         <li ref={setProjectsRef} className='pb-0 sm:pb-small pt-small sm:pt-0 pl-small sm:pl-0 opacity-0 m-0 w-full h-full flex justify-center
             items-center text-center absolute top-0 left-0'>
             <Link className="relative h-full w-full sm:flex sm:justify-center" href={link}>
-                <div ref={myDiv} className="absolute bottom-small left-small text-left -mb-3">
-                    <h2 className="paragraph -mb-2 lowercase">
-                        {role}
-                    </h2>
-                    <h3 className="heading-1-bold uppercase">
-                        {title}
-                    </h3>
+                <div className="absolute bottom-small w-full left-small text-left">
+                    <div className="overflow-hidden relative h-10">
+                        <h2 ref={h2} className="paragraph absolute -bottom-5 left-0 lowercase">
+                            {role}
+                        </h2>
+                    </div>
+                    <div className="overflow-hidden mt-4 relative h-20">
+                        <h3 ref={h3} className="absolute -bottom-10 left-0 heading-1-bold -mb-5 uppercase">
+                            {title}
+                        </h3>
+                    </div>
                 </div>
                 <img ref={trigger} src={img} alt={title} className='w-full h-full object-cover'/>
             </Link>
