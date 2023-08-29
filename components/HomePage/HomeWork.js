6793b0bbb491d98db6cae0a79b1a1982782f5f9d
Ivan Smiths <info@ivansmiths.com>
2023-08-29 21:32:37 +0200

    add semantic html & add relative

--- components/HomePage/HomeWork.js
+++ components/HomePage/HomeWork.js
@@ -17,12 +17,10 @@ function HomeWork({setProjectsRef, title, img, link}) {
     return (
         <li ref={setProjectsRef} className='pb-0 sm:pb-small pt-small sm:pt-0 pl-small sm:pl-0 opacity-0 m-0 w-full h-full flex justify-center
             items-center text-center absolute top-0 left-0'>
-            <Link className="h-full w-full sm:flex sm:justify-center" href={link}>
-                {hovered === true ? <>
-                    <h1 className="text-white absolute bottom-small left-small text-xs">
-                        {title}
-                    </h1>
-                </> : null}
+            <Link className="relative h-full w-full sm:flex sm:justify-center" href={link}>
+                <h2 className="paragraph absolute bottom-small left-small">
+                    {title}
+                </h2>
                 <img onMouseEnter={handleMouseEnter}
                      onMouseLeave={handleMouseLeave}
                      src={img} alt={title} className='w-full h-full object-cover'/>
