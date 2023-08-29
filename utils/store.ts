a03555aa16230b475e26a3cab12123449fd4417a
Ivan Smiths <info@ivansmiths.com>
2023-08-30 00:27:29 +0200

    convert to typescript store

---
+++ utils/store.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+
+interface HoverState {
+    hover: string;
+    setHover: (newState: string) => void;
+    scrollHint: boolean;
+    setScrollHint: (newState: boolean) => void;
+}
+
+interface AnimationState {
+    duration: number;
+}
+
+export const useHoverStore = create<HoverState>()((set) => ({
+    hover: "small",
+    setHover: (newState) => set({hover: newState}),
+    scrollHint: true,
+    setScrollHint: (newState) => set({scrollHint: newState}),
+}))
+
+export const useAnimationStore = create<AnimationState>()((set) => ({
+    duration: 0.3,
+}))
