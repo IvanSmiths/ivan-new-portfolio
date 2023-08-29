b5164e07894c946d9840ddbf9d0b7e61a27a0129
Ivan Smiths <info@ivansmiths.com>
2023-08-29 22:03:55 +0200

    add skeleton of animation

--- components/HomePage/HomeWork.js
+++ components/HomePage/HomeWork.js
@@ -1,24 +1,43 @@
 /* eslint-disable @next/next/no-img-element */
 
 import Link from "next/link"
-import {useState} from "react";
+import {useEffect, useRef} from "react";
+import gsap from "gsap"
 
 function HomeWork({setProjectsRef, title, img, link, role}) {
-    const [hovered, setHovered] = useState(false)
+    const trigger = useRef(null);
+    const myDiv = useRef(null);
 
-    const handleMouseEnter = () => {
-        setHovered(true);
-    };
+    useEffect(() => {
+        const tl = gsap.timeline({paused: true});
+        tl.fromTo(myDiv.current,
+            {opacity: 0},
+            {opacity: 1, duration: 0.2});
 
-    const handleMouseLeave = () => {
-        setHovered(false);
-    };
+        function handleMouseEnter() {
+            tl.play();
+        }
+
+        function handleMouseLeave() {
+            tl.reverse();
+        }
+
+        const div = trigger.current;
+        div.addEventListener('mouseenter', handleMouseEnter);
+        div.addEventListener('mouseleave', handleMouseLeave);
+
+        return () => {
+            div.removeEventListener('mouseenter', handleMouseEnter);
+            div.removeEventListener('mouseleave', handleMouseLeave);
+        };
+    }, []);
+
 
     return (
         <li ref={setProjectsRef} className='pb-0 sm:pb-small pt-small sm:pt-0 pl-small sm:pl-0 opacity-0 m-0 w-full h-full flex justify-center
             items-center text-center absolute top-0 left-0'>
             <Link className="relative h-full w-full sm:flex sm:justify-center" href={link}>
-                <div className="absolute bottom-small left-small text-left -mb-3">
+                <div ref={myDiv} className="absolute bottom-small left-small text-left -mb-3">
                     <h2 className="paragraph -mb-2 lowercase">
                         {role}
                     </h2>
@@ -26,10 +45,7 @@ function HomeWork({setProjectsRef, title, img, link, role}) {
                         {title}
                     </h3>
                 </div>
-
-                <img onMouseEnter={handleMouseEnter}
-                     onMouseLeave={handleMouseLeave}
-                     src={img} alt={title} className='w-full h-full object-cover'/>
+                <img ref={trigger} src={img} alt={title} className='w-full h-full object-cover'/>
             </Link>
         </li>
     )
