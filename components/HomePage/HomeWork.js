d4c211963e54e68b7b232977c97e1bce3113e32c
Ivan Smiths <info@ivansmiths.com>
2023-08-29 02:27:58 +0200

    fix mobile image

--- components/HomePage/HomeWork.js
+++ components/HomePage/HomeWork.js
@@ -25,7 +25,7 @@ function HomeWork({setProjectsRef, title, img, link}) {
                 </> : null}
                 <img onMouseEnter={handleMouseEnter}
                      onMouseLeave={handleMouseLeave}
-                     src={img} alt={title} className='w-full object-cover'/>
+                     src={img} alt={title} className='w-full h-full object-cover'/>
             </Link>
         </li>
     )
