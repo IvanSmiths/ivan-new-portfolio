8af851b5f87a299d8bc2c2420682e38c6e66429c
Ivan Smiths <info@ivansmiths.com>
2023-09-05 21:11:36 +0200

    add barebone structure

---
+++ utils/works.ts
@@ -0,0 +1,21 @@
+interface Work {
+    title: string;
+    description: string;
+    images?: string[];
+}
+
+const works: Work[] = [
+    {
+        title: "title",
+        description: "description",
+        images: ["/public"],
+    }, {
+        title: "title",
+        description: "description",
+        images: ["/public"],
+    }, {
+        title: "title",
+        description: "description",
+        images: ["/public"],
+    },
+]
