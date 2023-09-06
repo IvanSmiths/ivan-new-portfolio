8fa322638557028eaebc20295d86d68de8915efe
Ivan Smiths <info@ivansmiths.com>
2023-09-06 19:47:47 +0200

    add more properties

--- utils/works.ts
+++ utils/works.ts
@@ -2,6 +2,8 @@ export interface Work {
     slug: string;
     title: string;
     description: string;
+    job: string;
+    mainImage: string;
     company: string;
     images?: string[];
 }
@@ -12,19 +14,25 @@ export const works: Work[] = [
         title: "Ivan Smiths - TD COWEN",
         description: "description",
         company: "TD COWEN",
+        job: "ui/ux developer",
         images: ["/public"],
+        mainImage: "/images/td/td-website-1.png",
     },
     {
         slug: "scholz-und-volkmer",
         title: "Ivan Smiths - SCHOLZ&VOLKMER",
         description: "description",
         company: "SCHOLZ&VOLKMER",
+        job: "frontend developer",
+        mainImage: "/images/suv/suv-website-1.png",
     },
     {
         slug: "ideology-creative-studio",
         title: "Ivan Smiths - Ideology Creative Studio",
         description: "description",
         company: "Ideology Creative Studio",
+        job: "ui/ux designer",
         images: ["/public"],
+        mainImage: "/images/id/id-website-1.png",
     },
 ]
