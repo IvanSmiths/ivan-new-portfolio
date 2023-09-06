9248a30a1ca918a65c1359f1fae486fb6abc3aa7
Ivan Smiths <info@ivansmiths.com>
2023-09-06 22:09:18 +0200

    add more fields

--- utils/works.ts
+++ utils/works.ts
@@ -6,6 +6,11 @@ export interface Work {
     mainImage: string;
     company: string;
     images?: string[];
+    website: string;
+    jobDescription: string;
+    jobStack: string[];
+    jobDates: string[];
+    jobLinkedin: string;
 }
 
 export const works: Work[] = [
@@ -17,6 +22,11 @@ export const works: Work[] = [
         job: "ui/ux developer",
         images: ["/public"],
         mainImage: "/images/td/td-website-1.png",
+        website: "https://www.cowen.com/",
+        jobDates: ["febraury 2022 -", "january 2023"],
+        jobStack: ["VUE.JS (NUXT.JS)", "REACT.JS (NEXT.JS)", "TYPESCRIPT", "GSAP"],
+        jobLinkedin: "https://www.linkedin.com/company/td-cowen-continental-europe/",
+        jobDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         slug: "scholz-und-volkmer",
@@ -25,6 +35,11 @@ export const works: Work[] = [
         company: "SCHOLZ&VOLKMER",
         job: "frontend developer",
         mainImage: "/images/suv/suv-website-1.png",
+        website: "https://www.s-v.de/en/",
+        jobDates: ["febraury 2022 -", "january 2023"],
+        jobStack: ["VUE.JS (NUXT.JS)", "REACT.JS (NEXT.JS)", "TYPESCRIPT", "GSAP"],
+        jobLinkedin: "https://www.linkedin.com/company/scholzvolkmer/",
+        jobDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         slug: "ideology-creative-studio",
@@ -34,5 +49,10 @@ export const works: Work[] = [
         job: "ui/ux designer",
         images: ["/public"],
         mainImage: "/images/id/id-website-1.png",
+        website: "https://www.ideology.it/",
+        jobDates: ["febraury 2022 -", "january 2023"],
+        jobStack: ["VUE.JS (NUXT.JS)", "REACT.JS (NEXT.JS)", "TYPESCRIPT", "GSAP"],
+        jobLinkedin: "https://www.linkedin.com/company/ideology-creative-studio",
+        jobDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
 ]
