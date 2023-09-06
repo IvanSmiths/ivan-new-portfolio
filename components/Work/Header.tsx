29ce7e39b3c0c7d32d6e3552237a4ef0ec2b2695
Ivan Smiths <info@ivansmiths.com>
2023-09-06 22:09:04 +0200

    add tsx

--- components/Work/Header.tsx
+++ components/Work/Header.tsx
@@ -12,15 +12,37 @@ const Header: FC<HeaderProps> = ({work}) => {
         <>
             <GridDebug></GridDebug>
             <header className="grid">
-                <div className="grid-column-8-11 h-[80vh] flex flex-col justify-center">
+                <div className="grid-column-8-11 h-[75vh] flex flex-col justify-center">
                     <h1 className="paragraph">{work.job}</h1>
                     <h2 className="heading-1-bold">{work.company}</h2>
                 </div>
                 <div className="grid-column-2-11">
-                    <div>
-
-                    </div>
+                    <ul className="flex justify-between items-end pb-small">
+                        <li>
+                            <a href={work.website} rel="noopener" target="_blank">
+                                website
+                            </a>
+                        </li>
+                        <li className="flex flex-col">
+                            {work.jobStack.map((date, i) => (
+                                <span key={i}>{date}</span>
+                            ))}
+                        </li>
+                        <li className="flex flex-col">
+                            {work.jobDates.map((date, i) => (
+                                <span key={i}>{date}</span>
+                            ))}
+                        </li>
+                        <li>
+                            <a href={work.jobLinkedin} rel="noopener" target="_blank">
+                                linkedin
+                            </a>
+                        </li>
+                    </ul>
                     <img src={work.mainImage} alt={`${work.company} main image`}/>
+                </div>
+                <div className="grid-column-2-11 h-[100vh] flex justify-center items-center">
+                    <p className="heading-1 ">{work.jobDescription}</p>
                 </div>
             </header>
         </>
