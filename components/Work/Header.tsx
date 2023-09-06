4ec47bf73e61bfbd6634cb7872f88b2a737e636b
Ivan Smiths <info@ivansmiths.com>
2023-09-06 19:47:37 +0200

    add more tsx tag

--- components/Work/Header.tsx
+++ components/Work/Header.tsx
@@ -1,18 +1,30 @@
-import { FunctionComponent } from 'react';
+/* eslint-disable @next/next/no-img-element */
+import { FC } from 'react';
 import { Work } from '../../utils/works';
+import GridDebug from "../GridDebug";
 
 interface HeaderProps {
     work: Work;
 }
 
-const Header: FunctionComponent<HeaderProps> = ({work}) => {
+const Header: FC<HeaderProps> = ({work}) => {
     return (
-        <div>
-            <h1>{work.title}</h1>
-            <p>{work.description}</p>
-            {work.images && work.images.map((image, index) => <img src={image} key={index} alt={work.title}/>)}
-        </div>
-    );
+        <>
+            <GridDebug></GridDebug>
+            <header className="grid">
+                <div className="grid-column-8-11 h-[80vh] flex flex-col justify-center">
+                    <h1 className="paragraph">{work.job}</h1>
+                    <h2 className="heading-1-bold">{work.company}</h2>
+                </div>
+                <div className="grid-column-2-11">
+                    <div>
+
+                    </div>
+                    <img src={work.mainImage} alt={`${work.company} main image`}/>
+                </div>
+            </header>
+        </>
+    )
 };
 
 export default Header;
