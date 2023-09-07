4eb55b9e54f826780df91fd47ec48be3608f04c5
Ivan Smiths <info@ivansmiths.com>
2023-09-07 21:10:17 +0200

    add animation

--- components/Work/Header.tsx
+++ components/Work/Header.tsx
@@ -1,20 +1,47 @@
 /* eslint-disable @next/next/no-img-element */
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { Work } from '../../utils/works';
-import GridDebug from "../GridDebug";
+import { gsap } from "gsap";
 
 interface HeaderProps {
     work: Work;
 }
 
 const Header: FC<HeaderProps> = ({work}) => {
+
+    const scopeRef = useRef<HTMLHeadElement | null>(null);
+    const jobRef = useRef<HTMLHeadingElement | null>(null);
+    const companyRef = useRef<HTMLHeadingElement | null>(null);
+
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            const tl = gsap.timeline();
+            tl.to(jobRef.current,
+                {
+                    bottom: 1,
+                    duration: 0.5,
+                    ease: "circ.out"
+                }).to(companyRef.current,
+                {
+                    bottom: 2,
+                    duration: 0.5,
+                    ease: "circ.out"
+                }, 0.1)
+        }, scopeRef);
+        return () => ctx.revert();
+    }, []);
+
     return (
         <>
-            <GridDebug></GridDebug>
-            <header className="grid">
+            <header ref={scopeRef} className="grid">
                 <div className="grid-column-8-11 h-[75vh] flex flex-col justify-center">
-                    <h1 className="paragraph">{work.job}</h1>
-                    <h2 className="heading-1-bold">{work.company}</h2>
+                    <div className="overflow-hidden relative h-10">
+                        <h1 ref={jobRef} className="paragraph absolute -bottom-5 left-0 lowercase">{work.job}</h1>
+                    </div>
+                    <div className="overflow-hidden mt-1 relative h-10">
+                        <h2 ref={companyRef}
+                            className="heading-1-bold absolute -bottom-9 left-0 -mb-5 uppercase">{work.company}</h2>
+                    </div>
                 </div>
                 <div className="grid-column-2-11">
                     <ul className="flex justify-between items-end pb-small">
