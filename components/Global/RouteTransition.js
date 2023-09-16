4e4c5b3d60acd37edcc50011483d17d165c974bf
Ivan Smiths <info@ivansmiths.com>
2023-09-16 22:46:35 +0200

    rollback

--- components/Global/RouteTransition.js
+++ components/Global/RouteTransition.js
@@ -1,55 +1,49 @@
 import {useEffect, useRef} from "react";
 import {useRouter} from "next/router";
 import {gsap} from "gsap";
-import {useAnimationStore} from "../../utils/store";
 
 function RouteTransition({children}) {
     const router = useRouter()
-
-    const scopeRef = useRef(null);
     const transitionRef = useRef(null);
 
-    const {duration} = useAnimationStore();
-
     useEffect(() => {
-        let ctx = gsap.context(() => {
-            const aniStart = () => {
-                const tl = gsap.timeline();
-                tl.to(transitionRef.current, {
-                    opacity: 0,
-                    duration: duration,
-                });
-            };
+        const aniStart = () => {
+            const tl = gsap.timeline();
+            tl.to(transitionRef.current, {
+                opacity: 0,
+                duration: 0.2,
+            });
+        };
 
-            const aniEnd = () => {
-                const tl = gsap.timeline();
-                tl.to(transitionRef.current, {
-                    opacity: 1,
-                    duration: duration,
-                });
-            };
+        const aniEnd = () => {
+            const tl = gsap.timeline();
+            tl.to(transitionRef.current, {
+                opacity: 1,
+                duration: 0.2,
+            });
+        };
 
-            const handler = () => {
-                aniStart();
-                console.log('aniStart')
-                setTimeout(() => {
-                    aniEnd();
-                    console.log('aniEnd')
-                }, 400);
-            };
+        const handler = () => {
+            aniStart();
+            console.log('aniStart')
+            setTimeout(() => {
+                aniEnd();
+                console.log('aniEnd')
+            }, 400);
+        };
 
-            router.events.on('routeChangeStart', handler);
-            return () => router.events.off('routeChangeComplete', handler);
-        }, scopeRef);
-        return () => ctx.revert();
-    }, [duration, router.events])
+        router.events.on('routeChangeStart', handler);
 
+        return () => {
+            router.events.off('routeChangeComplete', handler);
+        }
+    }, [router.events])
     return (
-        <div ref={scopeRef}>
+        <>
             <div ref={transitionRef}>
                 {children}
             </div>
-        </div>
+        </>
     )
 }
 
