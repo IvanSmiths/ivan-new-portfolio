236c2aaec0759295bd735c1611bc61b6a14cf9c2
Ivan Smiths <info@ivansmiths.com>
2023-09-24 10:28:17 +0200

    add correct city

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -4,7 +4,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const OPEN_WEATHER_API = process.env.OPEN_WEATHER_API
     const {method} = req;
     if (method === 'GET') {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${OPEN_WEATHER_API}&units=metric`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Wiesbaden&appid=${OPEN_WEATHER_API}&units=metric`);
         const data = await response.json();
         res.status(200).json(data);
     } else {
