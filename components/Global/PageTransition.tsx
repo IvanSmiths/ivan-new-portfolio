16b35a616a351900946aa7ff7588a49074a092a0
Ivan Smiths <info@ivansmiths.com>
2023-09-26 22:49:14 +0200

    add large font to path

--- components/Global/PageTransition.tsx
+++ components/Global/PageTransition.tsx
@@ -65,7 +65,7 @@ const PageTransition = () => {
                     className="h-screen w-full top-0 fixed -translate-y-full bg-primary"
                 >
                     <span ref={queryRef}
-                          className="text-secondary fixed right-2 heading-big font-bold bottom-20">{cleanPath}</span>
+                          className="text-secondary fixed left-2 font-bold bottom-20 heading-large uppercase">{cleanPath}</span>
                 </div>
             </div>
         </>
