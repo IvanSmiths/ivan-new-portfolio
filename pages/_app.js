8eb61e014f990dbd3d6b8921b43eefb0f576edfc
Ivan Smiths <info@ivansmiths.com>
2023-09-28 18:30:27 +0200

    refactor public folder

--- pages/_app.js
+++ pages/_app.js
@@ -25,75 +25,75 @@ function MyApp({Component, pageProps}) {
             <Head>
                 <meta charSet="utf-8"/>
                 <meta name="author" content="Ivan Smiths"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <link rel="icon" href="/favicon/favicon.ico"/>
                 <link
                     rel="apple-touch-icon"
                     sizes="57x57"
-                    href="/apple-icon-57x57.png"
+                    href="/favicon/apple-icon-57x57.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="60x60"
-                    href="/apple-icon-60x60.png"
+                    href="/favicon/apple-icon-60x60.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="72x72"
-                    href="/apple-icon-72x72.png"
+                    href="/favicon/apple-icon-72x72.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="76x76"
-                    href="/apple-icon-76x76.png"
+                    href="/favicon/apple-icon-76x76.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="114x114"
-                    href="/apple-icon-114x114.png"
+                    href="/favicon/apple-icon-114x114.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="120x120"
-                    href="/apple-icon-120x120.png"
+                    href="/favicon/apple-icon-120x120.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="144x144"
-                    href="/apple-icon-144x144.png"
+                    href="/favicon/apple-icon-144x144.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="152x152"
-                    href="/apple-icon-152x152.png"
+                    href="/favicon/apple-icon-152x152.png"
                 />
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
-                    href="/apple-icon-180x180.png"
+                    href="/favicon/apple-icon-180x180.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="192x192"
-                    href="/android-icon-192x192.png"
+                    href="/favicon/android-icon-192x192.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="32x32"
-                    href="/favicon-32x32.png"
+                    href="/favicon/favicon-32x32.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="96x96"
-                    href="/favicon-96x96.png"
+                    href="/favicon/favicon-96x96.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="16x16"
-                    href="/favicon-16x16.png"
+                    href="/favicon/favicon-16x16.png"
                 />
                 <meta property="og:type" content="website"/>
                 <meta property="og:url" content="https://ww.ivansmiths.com"/>
