a4e687c28b3e9fc378e668a0e4208bbae1c6d907
Ivan Smiths <info@ivansmiths.com>
2024-01-31 21:54:29 +0100

    add tsx

--- components/Work/Description.tsx
+++ components/Work/Description.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { Work } from "../../utils/works";
+import Link from "next/link";
 
 interface HeaderProps {
   work: Work;
@@ -8,7 +9,10 @@ interface HeaderProps {
 const Description: FC<HeaderProps> = ({ work }) => {
   return (
     <>
-      <div className="grid-work-description bg-gray-700 h-full w-full">
+      <div className="grid-work-description pt-5 bg-gray-700 flex flex-col gap-medium w-full">
+        <span className="pb-medium">
+          <Link href="/">back home</Link>
+        </span>
         <h1 className="heading-regular font-bold">{work.company}</h1>
       </div>
       <div className="grid-work-images bg-red-300 h-full w-full"></div>
