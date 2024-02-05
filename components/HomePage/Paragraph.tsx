aed5260ba876cef6f639f9aa361cec6faf614b02
Ivan Smiths <info@ivansmiths.com>
2024-02-05 13:58:55 +0100

    add test for Paragraph.tsx

--- components/HomePage/Paragraph.tsx
+++ components/HomePage/Paragraph.tsx
@@ -4,8 +4,8 @@ const Paragraph: FC = () => {
   return (
     <div className="flex flex-wrap items-center pb-small">
       <p className="heading-regular">
-        <strong className="font-normal">Ivan Smiths -</strong> Frontend UI/UX
-        Developer - 3 years of experience. Seeking the limit. Currently at
+        Ivan Smiths - Frontend UI/UX Developer - 3 years of experience. Seeking
+        the limit. Currently at
         <a
           className="heading-regular"
           href="https://www.cowen.com/"
