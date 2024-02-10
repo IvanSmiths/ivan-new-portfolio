cb094aab58ea5f111128de155824a6430003106c
Ivan Smiths <info@ivansmiths.com>
2024-02-10 11:28:42 +0100

    format inline

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,14 +40,14 @@ export default function RootLayout({
           id="GooglenAlytics"
           dangerouslySetInnerHTML={{
             __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                
-                            gtag('config', '${GA_TRACKING_ID}', {
-                              page_path: window.location.pathname,
-                            });
-                           `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+            `,
           }}
         />
       </body>
