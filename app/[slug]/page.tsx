2fc46655d91e9e129c193f750115c4337f75efd4
Ivan Smiths <info@ivansmiths.com>
2024-02-12 18:21:30 +0100

    add hygraph

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,17 +1,38 @@
-import { projectlist } from "../../utils/works";
+async function getWorks() {
+  const response = await fetch(process.env.HYGRAPH_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query Works {
+          works {
+            createdAt
+            id
+            publishedAt
+            slug
+            updatedAt
+            workTitle
+            }
+          }
+        `,
+    }),
+  });
+  const { data } = await response.json();
 
-export async function generateStaticParams() {
-  return projectlist.map((p) => ({
-    uniquepage: p.slug.toString(),
-  }));
+  return data.works;
 }
 
-export default function UniquePage({
-  params: { uniquepage },
-}: {
-  params: { uniquepage: string };
-}) {
-  const project = projectlist.find((p) => p.slug.toString() === uniquepage);
-  console.log(project);
-  return <main></main>;
+export default async function UniquePage() {
+  const works = await getWorks();
+  console.log(works);
+  return (
+    <main>
+      {works.map((work) => (
+        <div key={work.id}>{work.workTitle}</div>
+      ))}
+    </main>
+  );
 }
