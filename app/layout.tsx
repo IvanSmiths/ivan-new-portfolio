fe481fbf42892b760e39d99a8e354e7502785caa
Ivan Smiths <info@ivansmiths.com>
2024-02-14 20:51:39 +0100

    remove warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleAnalytics />
-      <body>
+      <body suppressHydrationWarning={true}>
         <Navbar />
         {children}
         <Script
