07c9d1e8cb8bf75ad7460300ae1a2c7dfc7ae29c
Ivan Smiths <info@ivansmiths.com>
2024-02-18 21:18:25 +0100

    add title and meta

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,5 +1,12 @@
 import Navbar from "../components/Navbar";
 import ScrollSection from "../../components/HomePage/ScrollSection";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ivan Smiths, all the works",
+  description:
+    "Explore a showcase of my diverse works as a UI/UX Developer, encompassing a range of works that highlight my expertise and creativity",
+};
 
 function Works() {
   return (
