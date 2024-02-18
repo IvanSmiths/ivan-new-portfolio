c68afca5d0469d2676f479726890766abd18af79
Ivan Smiths <info@ivansmiths.com>
2024-02-18 19:13:30 +0100

    temporarily remove dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,26 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const setInitialTheme = `
+    function getUserPreference() {
+      if(window.localStorage.getItem('theme')) {
+        return window.localStorage.getItem('theme')
+      }
+      return window.matchMedia('(prefers-color-scheme: light)').matches
+                ? 'dark'
+                : 'light'
+    }
+    document.body.dataset.theme = getUserPreference();
+  `;
   return (
     <html lang="en">
       <GoogleAnalytics />
       <body suppressHydrationWarning={true}>
         {children}
+        <Script
+          id="ThemeToggle"
+          dangerouslySetInnerHTML={{ __html: setInitialTheme }}
+        />
         <Script
           strategy="afterInteractive"
           async
