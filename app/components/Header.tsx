8e3daa4fc66559e63d90add089350dfefc9791c2
Ivan Smiths <info@ivansmiths.com>
2024-02-25 16:13:35 +0100

    add scroll to top

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 function Header() {
+  useEffect(() => {
+    window.scrollTo({ top: 0 });
+  }, []);
+
   return (
     <div className="grid z-20 relative">
       <div className="grid-home-name pt-small flex gap-4 flex-col">
