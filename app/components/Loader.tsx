6c86ffdb49e48d0fd48fc26ebf3a20c2cdda0f59
Ivan Smiths <info@ivansmiths.com>
2024-02-25 13:13:28 +0100

    add animation

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -3,11 +3,24 @@
 import Header from "./Header";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function Loader() {
   const containerRef = useRef<HTMLDivElement | null>(null);
+  const faderRef = useRef<HTMLDivElement | null>(null);
   const imageRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    function timeoutId() {
+      setTimeout(() => {
+        document.body.style.overflow = "auto";
+      }, 3000);
+    }
+
+    timeoutId();
+  }, []);
 
   useGSAP(
     () => {
@@ -19,14 +32,23 @@ function Loader() {
       });
       tl.to(imageRef.current, {
         width: "100%",
-        duration: 0.8,
+        ease: "back.in(1)",
+        duration: 1,
       });
       tl.to(imageRef.current, {
         margin: 0,
         top: 0,
-        duration: 0.8,
+        duration: 1,
+        ease: "back.inOut(1)",
       });
       tl.to(containerRef.current, {
+        display: "none",
+      });
+      tl.to(faderRef.current, {
+        opacity: 0,
+        duration: 1,
+      });
+      tl.to(faderRef.current, {
         display: "none",
       });
     },
@@ -34,17 +56,23 @@ function Loader() {
   );
 
   return (
-    <div
-      ref={containerRef}
-      className="fixed w-full h-full top-0 left-0 bg-secondary z-20"
-    >
+    <>
       <div
-        ref={imageRef}
-        className="w-1/4 h-fit inline opacity-0 absolute mx-auto my-auto inset-0"
+        ref={faderRef}
+        className="absolute w-full h-[55%] bottom-0 left-0 bg-secondary z-20"
+      ></div>
+      <div
+        ref={containerRef}
+        className="fixed w-full h-full top-0 left-0 bg-secondary z-20"
       >
-        <Header />
+        <div
+          ref={imageRef}
+          className="w-1/4 h-fit inline opacity-0 absolute mx-auto my-auto inset-0"
+        >
+          <Header />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
