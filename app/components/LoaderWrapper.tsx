663fb405b137bbe54f36e7fe4c950388749de168
Ivan Smiths <info@ivansmiths.com>
2024-02-28 11:09:12 +0100

    add types

--- app/components/LoaderWrapper.tsx
+++ app/components/LoaderWrapper.tsx
@@ -4,12 +4,12 @@ import React, { useEffect, useState } from "react";
 import Loader from "./Loader";
 
 function LoaderWrapper() {
-  const [loader, setLoader] = useState(false);
+  const [loader, setLoader] = useState<boolean>(false);
 
   useEffect(() => {
-    const hasComponentRendered = localStorage.getItem("loader");
+    const hasComponentRendered: string | null = localStorage.getItem("loader");
 
-    if (!hasComponentRendered) {
+    if (!hasComponentRendered as boolean) {
       setLoader(true);
       localStorage.setItem("loader", "true");
     }
