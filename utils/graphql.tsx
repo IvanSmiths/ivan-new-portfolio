0a963946bd1490daea130a554ea623f31336b821
Ivan Smiths <info@ivansmiths.com>
2024-02-28 11:30:17 +0100

    refactor code

---
+++ utils/graphql.tsx
@@ -0,0 +1,51 @@
+export type WorkType = {
+  slugHome: string;
+  title: string;
+  company: string;
+  role: string;
+  homeDescription: string;
+  homeLogo: string;
+  homeImage: string;
+};
+
+type QueryResult = {
+  works: WorkType[];
+};
+
+type Response = {
+  data: QueryResult;
+};
+
+export async function getWorks(): Promise<WorkType[]> {
+  if (!process.env.HYGRAPH_ENDPOINT) {
+    throw new Error("Environment variable HYGRAPH_ENDPOINT is not set.");
+  }
+  const response = await fetch(process.env.HYGRAPH_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query Works() {
+          works(orderBy: createdAt_ASC) {
+            slugHome
+            createdAt
+            company
+            role
+            homeDescription
+            homeLogo {
+              url
+            }
+            homeImage {
+              url
+            }
+            }
+          }
+        `,
+    }),
+  });
+  const { data }: Response = await response.json();
+  return data.works;
+}
