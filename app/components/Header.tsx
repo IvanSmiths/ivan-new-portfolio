0f00eb5b6922e62a04e1727ebadfb5a6ae235977
Ivan Smiths <info@ivansmiths.com>
2024-03-07 21:08:29 +0100

    update logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,15 +5,13 @@ function Header() {
   return (
     <div className="grid z-20 relative">
       <div className="grid-home-name pt-small flex gap-4 flex-col">
-        <div className="h-6 w-full bg-black"></div>
         <img
           className="w-full"
-          src="/images/IVANSMITHS.svg"
+          src="/images/full-logo.svg"
           alt="ivan smiths"
           width="700"
           height="400"
         />
-        <div className="h-6 w-full bg-black"></div>
       </div>
     </div>
   );
