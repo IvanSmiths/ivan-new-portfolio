606fef928b61d53a1d16de9b9a285c0c1a279218
Ivan Smiths <info@ivansmiths.com>
2024-03-10 14:56:05 +0100

    remove console.log

--- app/components/Overlay.tsx
+++ app/components/Overlay.tsx
@@ -5,7 +5,6 @@ import { useOverlayStore } from "../../utils/store";
 
 const Overlay: FC = () => {
   const { isHidden } = useOverlayStore();
-  console.log(isHidden);
   return (
     <div
       className={`z-50 bg-gray-50 ${isHidden ? "" : "hidden"} fixed w-full h-full top-0 left-0`}
