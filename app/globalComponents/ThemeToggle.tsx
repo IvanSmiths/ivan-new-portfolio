3485988e823c469ba89eef79f0d809b996f776dd
Ivan Smiths <info@ivansmiths.com>
2024-03-11 18:04:36 +0100

    add icons

--- app/globalComponents/ThemeToggle.tsx
+++ app/globalComponents/ThemeToggle.tsx
@@ -9,14 +9,21 @@ const ThemeToggle = () => {
   useEffect(() => {
     document.body.dataset.theme = activeTheme;
   }, [activeTheme]);
+
+  const handleTheme = () => {
+    setActiveTheme(activeTheme === "light" ? "dark" : "light");
+  };
+
   return (
-    <>
-      {activeTheme === "dark" ? (
-        <span onClick={() => setActiveTheme("light")}>dark</span>
-      ) : (
-        <span onClick={() => setActiveTheme("dark")}>light</span>
-      )}
-    </>
+    <div className="flex justify-center items-center p-1.5 rounded-full border-[2px] w-10 h-10 border-primary cursor-pointer">
+      <img
+        height="40"
+        width="40"
+        onClick={handleTheme}
+        src={activeTheme === "dark" ? "/icons/light.svg" : "/icons/dark.svg"}
+        alt={activeTheme === "dark" ? "switch to light" : "switch to dark"}
+      />
+    </div>
   );
 };
 
