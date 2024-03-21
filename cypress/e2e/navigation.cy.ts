c5f42cfe04d3a71ebcdfa4f4acfd9bff4fe02be3
Ivan Smiths <info@ivansmiths.com>
2024-03-21 17:25:54 +0100

    correct description (#124)

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -2,7 +2,7 @@ import { navigate, Pages } from "../helpers/nav.cy";
 import { wait } from "../helpers/wait.cy";
 
 describe("Navigation", () => {
-  it("should navigate to the works page", () => {
+  it("should navigate through all pages", () => {
     cy.visit("/");
     wait(5000);
     navigate(Pages.WORKS);
