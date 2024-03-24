4fda32d51cdc720d6eca4b5e318a3a8e1befbe94
Ivan Smiths <info@ivansmiths.com>
2024-03-24 20:36:31 +0100

    replace icons with text (#128)

--- app/globalComponents/ThemeToggle.tsx
+++ app/globalComponents/ThemeToggle.tsx
@@ -17,17 +17,9 @@ const ThemeToggle: FC = () => {
   };
 
   return (
-    <div
-      onClick={handleTheme}
-      className="flex justify-center items-center p-1.5 rounded-full border-[2px] w-10 h-10 border-primary cursor-pointer"
-    >
-      <img
-        height="40"
-        width="40"
-        src={activeTheme === "dark" ? "/icons/light.svg" : "/icons/dark.svg"}
-        alt={activeTheme === "dark" ? "switch to light" : "switch to dark"}
-      />
-    </div>
+    <span className="cursor-pointer mono" onClick={handleTheme}>
+      - {activeTheme === "dark" ? "light" : "dark"} mode -
+    </span>
   );
 };
 
