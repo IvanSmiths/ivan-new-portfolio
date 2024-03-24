d00d55419e8a27a7297462c1e5c67a04aadbef8b
Ivan Smiths <info@ivansmiths.com>
2024-03-24 18:10:30 +0100

    simplify using pin for fixed positioning (#128)

--- app/components/NavbarWrapper.tsx
+++ app/components/NavbarWrapper.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import { FC, useRef, useState } from "react";
+import { FC, useRef } from "react";
 import Navbar from "../globalComponents/Navbar";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
 const NavbarWrapper: FC = () => {
-  const [isFixed, setIsFixed] = useState(false);
   const triggerRef = useRef<HTMLDivElement | null>(null);
 
   gsap.registerPlugin(ScrollTrigger);
-  const handleEnter = () => {
-    setIsFixed(true);
-  };
-
-  const handleLeave = () => {
-    setIsFixed(false);
-  };
 
   useGSAP(
     () => {
@@ -25,17 +17,13 @@ const NavbarWrapper: FC = () => {
         trigger: triggerRef.current,
         start: "top top",
         end: "max",
-        onEnter: handleEnter,
-        onLeaveBack: handleLeave,
+        pin: true,
       });
     },
     { scope: triggerRef },
   );
   return (
-    <div
-      className={`${isFixed ? "fixed sm:top-0 left-0 w-full z-10" : "sticky"} pt-3`}
-      ref={triggerRef}
-    >
+    <div className="left-0 w-full z-10 pt-3" ref={triggerRef}>
       <Navbar />
     </div>
   );
