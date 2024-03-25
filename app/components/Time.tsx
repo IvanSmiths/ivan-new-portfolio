c701a808a6fbe3773366e2c66a748b23c8eabf81
Ivan Smiths <info@ivansmiths.com>
2024-03-25 21:19:10 +0100

    reduce width

--- app/components/Time.tsx
+++ app/components/Time.tsx
@@ -57,7 +57,7 @@ const Time: FC = () => {
   return (
     <div
       ref={timeScopeRef}
-      className="relative overflow-hidden h-[14px] w-[79px]"
+      className="relative overflow-hidden h-[14px] w-[75px]"
     >
       <span
         className="absolute mr-auto mono top-0 left-0 right-0 bottom-0 w-fit"
