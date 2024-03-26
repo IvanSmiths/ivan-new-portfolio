5f922be6db9a63223b308eb5b8e8d4130725c9d5
Ivan Smiths <info@ivansmiths.com>
2024-03-26 23:58:46 +0100

    finish desktop grid structure (#127)

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -9,7 +9,6 @@ type WorkProps = {
     slugHome: string;
     company: string;
     role: string;
-    homeDescription: string;
     homeImage: {
       url: string;
     };
@@ -21,28 +20,28 @@ type WorkProps = {
 
 const Work: FC<WorkProps> = ({ work, index }) => {
   return (
-    <div
+    <Link
+      href={work.slugHome}
+      className="h-[100vh] w-[100vw] flex items-center"
       key={index}
-      className="h-[100vh] w-[100vw] px-5 sm:p-0 flex justify-center items-center"
     >
-      <Link
-        href={work.slugHome}
-        className="w-full sm:w-8/12 md:w-[40rem] md:h-[30rem] h-2/3 relative flex justify-center lg:items-center flex-col p-small"
-      >
-        <img
-          src={work.homeImage.url}
-          loading="lazy"
-          className="h-full w-full lg:w-10/12 lg:h-10/12 object-cover rounded-sm"
-          alt={`${work.company} work`}
-        />
-        <div className="lg:absolute lg:top-1/2 lg:-left-60 lg:-translate-y-1/2 static h-fit">
-          <h3 className="font-bold heading-large mt-5 lg:mt-0">{work.role}</h3>
+      <div className="grid items-center w-full h-3/5 text-right">
+        <div className="col-start-4 col-end-6 justify-between items-end right-auto flex flex-col w-full h-fit">
+          <h3 className="font-bold text-4xl">{work.role}</h3>
           <h4 className="heading-regular lg:mt-small mt-2 lg:ml-1">
             {work.company}
           </h4>
         </div>
-      </Link>
-    </div>
+        <div className="col-start-6 col-end-10 flex h-full w-full">
+          <img
+            src={work.homeImage.url}
+            loading="lazy"
+            className="h-full w-full object-cover"
+            alt={`${work.company} work`}
+          />
+        </div>
+      </div>
+    </Link>
   );
 };
 
