1f4ea43f7133994dfb6fa22ed481288538a88e44
Ivan Smiths <info@ivansmiths.com>
2024-03-27 00:27:22 +0100

    finish mobile design (#127)

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -12,6 +12,8 @@ type WorkProps = {
     homeDescription: string;
     homeImage: {
       url: string;
+      height: string;
+      width: string;
     };
     homeLogo: {
       url: string;
@@ -22,20 +24,22 @@ type WorkProps = {
 const Work: FC<WorkProps> = ({ work, index }) => {
   return (
     <div key={index} className="h-[100vh] w-[100vw] flex items-center">
-      <div className="grid items-center w-full h-3/5 text-right">
-        <div className="col-start-4 col-end-6 justify-between items-end right-auto flex flex-col w-full h-full">
+      <div className="grid items-center w-full md:h-3/5 h-4/5">
+        <div className="md:col-start-4 md:col-end-6 col-start-1 col-end-7 text-left md:text-right md:justify-between justify-end md:items-end right-auto flex flex-col w-full h-full">
           <Link href={work.slugHome}>
             <h3 className="mono">{work.company}</h3>
             <h4 className="font-bold text-4xl mt-1">{work.role}</h4>
           </Link>
-          <h5>{work.homeDescription}</h5>
+          <h5 className="hidden sm:block">{work.homeDescription}</h5>
         </div>
         <Link
           href={work.slugHome}
-          className="col-start-6 col-end-10 flex h-full w-full"
+          className="md:col-start-6 md:col-end-10 col-start-1 col-end-7 flex h-full w-full"
         >
           <img
             src={work.homeImage.url}
+            height={work.homeImage.height}
+            width={work.homeImage.width}
             loading="lazy"
             className="h-full w-full object-cover rounded-md"
             alt={`${work.company} work`}
