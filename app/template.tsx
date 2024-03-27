771bf6c1949f764c131e3472251cb0bf2a40a116
Ivan Smiths <info@ivansmiths.com>
2024-03-27 14:37:21 +0100

    update animation (#145)

--- app/template.tsx
+++ app/template.tsx
@@ -7,13 +7,24 @@ export default function Template({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     animatePageIn();
   }, []);
-
   return (
     <div>
       <div
-        id="transition-element"
-        className="w-screen h-screen bg-red-500 z-[100] fixed top-0 left-0"
-      ></div>
+        id="banner-1"
+        className="min-h-screen bg-primary z-[100] fixed top-0 left-0 w-1/4"
+      />
+      <div
+        id="banner-2"
+        className="min-h-screen bg-primary z-[100] fixed top-0 left-1/4 w-1/4"
+      />
+      <div
+        id="banner-3"
+        className="min-h-screen bg-primary z-[100] fixed top-0 left-2/4 w-1/4"
+      />
+      <div
+        id="banner-4"
+        className="min-h-screen bg-primary z-[100] fixed top-0 left-3/4 w-1/4"
+      />
       {children}
     </div>
   );
