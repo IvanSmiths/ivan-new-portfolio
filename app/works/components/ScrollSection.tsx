b9418be6ccaacc8a33327cf37ce71a93b49261c9
Ivan Smiths <info@ivansmiths.com>
2024-03-27 12:19:59 +0100

    add home component to works page (#146)

--- app/works/components/ScrollSection.tsx
+++ app/works/components/ScrollSection.tsx
@@ -1,48 +1,13 @@
 "use client";
 
-import { FC, MutableRefObject, useEffect, useRef } from "react";
+import { FC, useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import Work from "./Work";
+import Work from "../../components/Works/Work";
+import Link from "next/link";
 
-type WorkProps = {
-  title: string;
-  role: string;
-  link: string;
-  image: string;
-};
-
-const works: WorkProps[] = [
-  {
-    title: "TD Cowen",
-    role: "UI/UX Developer",
-    link: "/works/td-cowen",
-    image: "/images/td/mockup.jpg",
-  },
-  {
-    title: "Scholz & Volkmer",
-    role: "Frontend developer",
-    link: "/works/scholz-und-volkmer",
-    image: "/images/suv/mockup.jpg",
-  },
-  {
-    title: "Ideology",
-    role: "UI/UX Designer",
-    link: "/works/ideology-creative-studio",
-    image: "/images/id/mockup.jpg",
-  },
-  {
-    title: "TD Cowen",
-    role: "UI/UX Developer",
-    link: "/works/td-cowen",
-    image: "/images/td/mockup.jpg",
-  },
-];
-
-type WorksRefType = any;
-type SetWorksRefType = any;
-
-const InfiniteScroll: FC = () => {
+// @ts-ignore
+const InfiniteScroll: FC = ({ works }) => {
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
@@ -57,30 +22,38 @@ const InfiniteScroll: FC = () => {
     return () => ctx.revert();
   }, []);
 
-  const [worksRef, setWorksRef]: [WorksRefType, SetWorksRefType] =
-    useArrayRef();
-
-  function useArrayRef(): [MutableRefObject<any[]>, (ref: number) => void] {
-    const worksRef = useRef<any[]>([]);
-    worksRef.current = [];
-    return [worksRef, (ref) => ref && worksRef.current.push(ref)];
-  }
-
   return (
     <div className="grid">
       <div className="fixed h-full w-full"></div>
       <div className="flex md:col-start-3 md:col-end-11 col-start-1 col-end-7">
-        <ul className="w-full flex flex-col justify-center">
+        <ul className="w-full flex flex-col items-center justify-center">
           {works.map((work, index) => (
-            <Work
-              key={index}
-              title={work.title}
-              link={work.link}
-              img={work.image}
-              role={work.role}
-              setWorksRef={setWorksRef}
-            />
+            <Work key={index} work={work} index={index} />
           ))}
+          <div className="h-[100vh] w-[100vw] flex items-center">
+            <div className="grid items-center w-full md:h-3/5 h-4/5">
+              <div className="md:col-start-4 md:col-end-6 col-start-1 col-end-7 text-left md:text-right md:justify-between justify-end md:items-end right-auto flex flex-col w-full h-full">
+                <Link href={works[0].slugHome}>
+                  <h3 className="mono">{works[0].company}</h3>
+                  <h4 className="font-bold text-4xl mt-1">{works[0].role}</h4>
+                </Link>
+                <h5 className="hidden sm:block">{works[0].homeDescription}</h5>
+              </div>
+              <Link
+                href={works[0].slugHome}
+                className="md:col-start-6 md:col-end-10 col-start-1 col-end-7 flex h-full w-full"
+              >
+                <img
+                  src={works[0].homeImage.url}
+                  height={works[0].homeImage.height}
+                  width={works[0].homeImage.width}
+                  loading="lazy"
+                  className="h-full w-full object-cover rounded-md"
+                  alt={`${works[0].company} work`}
+                />
+              </Link>
+            </div>
+          </div>
         </ul>
       </div>
     </div>
