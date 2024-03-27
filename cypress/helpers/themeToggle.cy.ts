902026e1ee3e5edc9308e72620589c991e828de9
Ivan Smiths <info@ivansmiths.com>
2024-03-27 20:44:41 +0100

    fix broken tests (#151)

--- cypress/helpers/themeToggle.cy.ts
+++ cypress/helpers/themeToggle.cy.ts
@@ -3,5 +3,5 @@ import { ThemeMode } from "../../utils/store";
 type Theme = ThemeMode;
 
 export const toggleTheme = (theme: Theme): void => {
-  cy.get(`nav div img[src="/icons/${theme}.svg"]`).click();
+  cy.contains("nav div span", `-${theme} mode-`).click();
 };
