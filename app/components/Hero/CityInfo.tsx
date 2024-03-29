b2d320c11101606c138424d2a047ff9feeb3d684
Ivan Smiths <info@ivansmiths.com>
2024-03-29 23:56:25 +0100

    add back weather

--- app/components/Hero/CityInfo.tsx
+++ app/components/Hero/CityInfo.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import Time from "./Time";
+import FetchWeather from "./Weather/FetchWeather";
 
 const CityInfo: FC = () => {
   return (
     <div className="md:col-start-[-1] md:col-end-[-6] col-start-1 col-end-6 sm:justify-end h-fit md:flex-row flex-row-reverse justify-end flex gap-3 sm:gap-small flex-wrap items-center">
       <Time />
+      <FetchWeather />
       <span className="mono">Wiesbaden (DE)</span>
     </div>
   );
