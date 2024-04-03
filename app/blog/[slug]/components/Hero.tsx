2f95dfffc8e2523bebf55147ebef3fb3f55a7cf1
Ivan Smiths <info@ivansmiths.com>
2024-04-03 16:31:09 +0200

    replace width with grid (#122)

--- app/blog/[slug]/components/Hero.tsx
+++ app/blog/[slug]/components/Hero.tsx
@@ -28,56 +28,58 @@ const bebas_neue = Bebas_Neue({
 const Hero: FC<PostProps> = ({ post }) => {
   const tags: string[] = post.tags.split(" ");
   return (
-    <div className="w-full flex flex-col items-center">
+    <>
       <Blob />
-      <div className="flex flex-col gap-small justify-center items-center sm:w-8/12 w-11/12">
-        <span className="mono">{post.category}</span>
-        <h1
-          className={`${bebas_neue.className} md:text-7xl xl:text-8xl text-3xl text-center bg-gradient-to-r from-primaryAccent via-primaryAccent to-primaryLight font-semibold text-transparent h-full bg-clip-text`}
-        >
-          {post.title}
-        </h1>
-        <h2 className="text-center text-lg text-primary leading-[24px]">
-          {post.excerpt}
-        </h2>
-        <div className="flex items-center flex-wrap gap-2">
-          <span className="mono">Published:</span>
-          <time className="mono" dateTime={post.date}>
-            {new Date(post.date).toLocaleDateString("en-us", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}
-          </time>
-          <Dot dimension="small" />
-          <span className="mono">{post.time} minutes read</span>
+      <div className="grid">
+        <div className="md:col-start-3 md:col-end-11 col-span-full flex flex-col gap-small justify-center items-center">
+          <span className="mono">{post.category}</span>
+          <h1
+            className={`${bebas_neue.className} md:text-7xl xl:text-8xl text-3xl text-center bg-gradient-to-r from-primaryAccent via-primaryAccent to-primaryLight font-semibold text-transparent h-full bg-clip-text`}
+          >
+            {post.title}
+          </h1>
+          <h2 className="text-center text-lg text-primary leading-[24px]">
+            {post.excerpt}
+          </h2>
+          <div className="flex items-center flex-wrap gap-2">
+            <span className="mono">Published:</span>
+            <time className="mono" dateTime={post.date}>
+              {new Date(post.date).toLocaleDateString("en-us", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}
+            </time>
+            <Dot dimension="small" />
+            <span className="mono">{post.time} minutes read</span>
+          </div>
+          <ul className="flex justify-center items-center gap-smallest">
+            {tags.map((tag: string, index: number) => (
+              <li
+                className="flex justify-center items-center gap-smallest"
+                key={index}
+              >
+                <span className="mono">{tag}</span>
+                <div className={index === tags.length - 1 ? "hidden" : ""}>
+                  <Dot dimension="small" />
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="flex justify-center items-center gap-smallest">
-          {tags.map((tag: string, index: number) => (
-            <li
-              className="flex justify-center items-center gap-smallest"
-              key={index}
-            >
-              <span className="mono">{tag}</span>
-              <div className={index === tags.length - 1 ? "hidden" : ""}>
-                <Dot dimension="small" />
-              </div>
-            </li>
-          ))}
-        </ul>
+        {post.cover ? (
+          <div className="col-span-full mt-small">
+            <img
+              className="w-full"
+              src={post.cover}
+              alt={post.coverAlt}
+              width={post.coverWidth}
+              height={post.coverHeight}
+            />
+          </div>
+        ) : null}
       </div>
-      {post.cover ? (
-        <div className="w-11/12 mt-small">
-          <img
-            className="w-full"
-            src={post.cover}
-            alt={post.coverAlt}
-            width={post.coverWidth}
-            height={post.coverHeight}
-          />
-        </div>
-      ) : null}
-    </div>
+    </>
   );
 };
 
