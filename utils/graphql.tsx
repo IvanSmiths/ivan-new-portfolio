eadcca03223c9a1086d61e64497670a01a173f45
Ivan Smiths <info@ivansmiths.com>
2024-04-05 22:24:15 +0200

    fix types and improve naming (#169)

--- utils/graphql.tsx
+++ utils/graphql.tsx
@@ -1,6 +1,6 @@
 import { ElementNode, RichTextContent } from "@graphcms/rich-text-types";
 
-export type WorkType = {
+export type Works = {
   slug: string;
   company: string;
   role: string;
@@ -14,11 +14,11 @@ export type WorkType = {
 
 type ApiResponse = {
   data: {
-    works: WorkType[];
+    works: Works[];
   };
 };
 
-export async function getWorks(): Promise<WorkType[]> {
+export async function getWorks(): Promise<Works[]> {
   if (!process.env.HYGRAPH_ENDPOINT) {
     throw new Error("Environment variable HYGRAPH_ENDPOINT is not set.");
   }
