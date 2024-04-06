1d67e34b629611a81e33306abf58b9b2a122b8dc
Ivan Smiths <info@ivansmiths.com>
2024-04-06 19:53:32 +0200

    preload text

--- app/works/[slug]/components/Hero.tsx
+++ app/works/[slug]/components/Hero.tsx
@@ -5,6 +5,7 @@ import { Bebas_Neue } from "next/font/google";
 const bebas_neue = Bebas_Neue({
   subsets: ["latin"],
   weight: ["400"],
+  preload: true,
   variable: "--font-bebas_neue",
   display: "swap",
 });
