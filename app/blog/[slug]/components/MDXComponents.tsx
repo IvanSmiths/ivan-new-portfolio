a9ec9c4b8d8677f5633b0ab69e62e294948a387e
Ivan Smiths <info@ivansmiths.com>
2024-04-06 19:00:45 +0200

    set proper html types (#122)

--- app/blog/[slug]/components/MDXComponents.tsx
+++ app/blog/[slug]/components/MDXComponents.tsx
@@ -1,6 +1,5 @@
 import { Bebas_Neue } from "next/font/google";
-
-type Text = { children: string };
+import { DetailedHTMLProps, HTMLAttributes } from "react";
 
 const bebas_neue = Bebas_Neue({
   subsets: ["latin"],
@@ -9,48 +8,70 @@ const bebas_neue = Bebas_Neue({
   display: "swap",
 });
 
+type HeadingProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLHeadingElement>,
+  HTMLHeadingElement
+>;
+
+type ParagraphProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLParagraphElement>,
+  HTMLParagraphElement
+>;
+
+type AnchorProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>;
+
+type ListProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLLIElement>,
+  HTMLLIElement
+>;
+
+type StrongProps = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>;
+
 export const MDXComponents = {
-  h1: (props: Text) => (
+  h1: (props: HeadingProps) => (
     <h1 {...props} className={`${bebas_neue.className} font-bold text-7xl`}>
       {props.children}
     </h1>
   ),
-  h2: (props: Text) => (
+  h2: (props: HeadingProps) => (
     <h2 {...props} className={`${bebas_neue.className} font-bold text-6xl`}>
       {props.children}
     </h2>
   ),
-  h3: (props: Text) => (
+  h3: (props: HeadingProps) => (
     <h3 {...props} className={`${bebas_neue.className} font-bold text-5xl`}>
       {props.children}
     </h3>
   ),
-  h4: (props: Text) => (
+  h4: (props: HeadingProps) => (
     <h4 {...props} className={`${bebas_neue.className} font-bold text-4xl`}>
       {props.children}
     </h4>
   ),
-  h5: (props: Text) => (
+  h5: (props: HeadingProps) => (
     <h5 {...props} className={`${bebas_neue.className} font-bold text-4xl`}>
       {props.children}
     </h5>
   ),
-  h6: (props: Text) => (
+  h6: (props: HeadingProps) => (
     <h6 {...props} className={`${bebas_neue.className} font-bold text-4xl`}>
       {props.children}
     </h6>
   ),
-  p: (props: Text) => (
+  p: (props: ParagraphProps) => (
     <p {...props} className="text-lg">
       {props.children}
     </p>
   ),
-  strong: (props: Text) => (
+  strong: (props: StrongProps) => (
     <strong {...props} className="text-lg font-bold">
       {props.children}
     </strong>
   ),
-  a: (props: Text) => (
+  a: (props: AnchorProps) => (
     <a
       {...props}
       target="_blank"
@@ -60,7 +81,7 @@ export const MDXComponents = {
       {props.children}
     </a>
   ),
-  li: (props: Text) => (
+  li: (props: ListProps) => (
     <li {...props} className="text-lg list-disc">
       {props.children}
     </li>
