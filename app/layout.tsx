ff1e9500567a2a0726aa8f99037979ea7bd01f35
Ivan Smiths <info@ivansmiths.com>
2024-04-12 12:59:51 +0200

    add react import (#173)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { azeret_mono, bebas_neue } from "../utils/fonts";
+import React from "react";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.ivansmiths.com"),
