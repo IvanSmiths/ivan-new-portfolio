b35bf5c442374fcbd7d5385b0f175e38a67f370e
Ivan Smiths <info@ivansmiths.com>
2024-04-12 14:47:37 +0200

    add lato font to blog (#122)

--- app/blog/[slug]/components/Hero.tsx
+++ app/blog/[slug]/components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero: FC<PostProps> = ({ post }) => {
     <>
       <Blob />
       <section className="md:col-start-3 md:col-end-11 col-span-full flex flex-col gap-small justify-center items-center">
-        <span className="mono">{post.category}</span>
+        <span className="lato font-bold">{post.category}</span>
         <h1 className="bebas md:text-7xl xl:text-8xl text-5xl text-center bg-gradient-to-r from-primaryAccent via-primaryAccent to-primaryLight text-transparent h-full bg-clip-text">
           {post.title}
         </h1>
@@ -31,9 +31,9 @@ const Hero: FC<PostProps> = ({ post }) => {
           {post.excerpt}
         </h2>
         <div className="flex max-sm:flex-col items-center flex-wrap gap-smallest">
-          <span className="mono">
+          <span className="lato font-bold">
             Published:{" "}
-            <time className="mono" dateTime={post.date}>
+            <time className="lato font-bold" dateTime={post.date}>
               {new Date(post.date).toLocaleDateString("en-us", {
                 year: "numeric",
                 month: "long",
@@ -44,7 +44,7 @@ const Hero: FC<PostProps> = ({ post }) => {
           <div className="hidden sm:block">
             <Dot dimension="small" />
           </div>
-          <span className="mono">{post.time} minutes read</span>
+          <span className="lato font-bold">{post.time} minutes read</span>
         </div>
         <ul className="flex justify-center items-center gap-smallest">
           {tags.map((tag: string, index: number) => (
@@ -52,7 +52,7 @@ const Hero: FC<PostProps> = ({ post }) => {
               className="flex justify-center items-center gap-smallest"
               key={index}
             >
-              <span className="mono">{tag}</span>
+              <span className="lato font-bold">{tag}</span>
               <div className={index === tags.length - 1 ? "hidden" : ""}>
                 <Dot dimension="small" />
               </div>
