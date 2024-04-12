1f3b90bae5ac987cdc830e118936a7f5df0a8a95
Ivan Smiths <info@ivansmiths.com>
2024-04-12 13:48:11 +0200

    adjust with (#173)

--- app/components/Hero/Time.tsx
+++ app/components/Hero/Time.tsx
@@ -57,7 +57,7 @@ const Time: FC = () => {
   return (
     <div
       ref={timeScopeRef}
-      className="relative overflow-hidden h-[18px] w-[75px]"
+      className="relative overflow-hidden h-[18px] w-[64px]"
     >
       <span
         className="absolute mr-auto mono top-0 left-0 right-0 bottom-0 w-fit"
