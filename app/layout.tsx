9ce5a20ea2e86b6f2e659651f41cd3aa2741eb7f
Ivan Smiths <info@ivansmiths.com>
2024-04-12 13:03:40 +0200

    remove Google Analytics (#173)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import "./globals.css";
-import GoogleAnalytics from "../utils/GoogleAnalytics";
-import { GA_TRACKING_ID } from "../utils/gtag";
-import Script from "next/script";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { azeret_mono, bebas_neue } from "../utils/fonts";
@@ -27,28 +24,8 @@ export default function RootLayout({
       lang="en"
       className={`${azeret_mono.variable} ${bebas_neue.variable}`}
     >
-      <GoogleAnalytics />
       <body className="bg-secondary" suppressHydrationWarning={true}>
         {children}
-        <Script
-          strategy="afterInteractive"
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <Script
-          id="GooglenAlytics"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-            `,
-          }}
-        />
         <Analytics />
       </body>
     </html>
