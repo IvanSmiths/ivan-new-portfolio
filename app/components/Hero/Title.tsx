2c38f41869ef3b8e053e6e8503717c41ac4e45a6
Ivan Smiths <info@ivansmiths.com>
2024-04-12 14:30:46 +0200

    simplify heading and add a tag (#173)

--- app/components/Hero/Title.tsx
+++ app/components/Hero/Title.tsx
@@ -4,8 +4,15 @@ const Title: FC = () => {
   return (
     <div className="col-start-1 col-end-8 my-medium md:mb-small">
       <h1 className="sm:text-4xl text-xl">
-        Frontend UI/UX developer with 3 years of experience. seeking the limit.
-        currently at TD Cowen.
+        UI/UX Developer with 3 years of experience. Seeking the limit. Currently
+        at {""}
+        <a
+          href="https://www.linkedin.com/company/td-cowen/"
+          target="_blank"
+          className="underline underline-offset-4 sm:text-4xl text-xl"
+        >
+          TD Cowen.
+        </a>
       </h1>
     </div>
   );
