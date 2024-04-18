2cb4c12d049c42b937aaf4473b16861995e21e3b
Ivan Smiths <info@ivansmiths.com>
2024-04-18 20:43:33 +0200

    reduce blur (#122)

--- app/blog/[slug]/components/Blob.tsx
+++ app/blog/[slug]/components/Blob.tsx
@@ -2,8 +2,8 @@ import { FC } from "react";
 
 const Blob: FC = () => {
   return (
-    <div className="w-5/12 h-64 absolute -top-40 left-1/2 transform blur-[150px] md:blur-[200px] lg:blur-[300px] bg-primaryLight -translate-x-1/2">
-      <div className="bg-primaryLight w-full h-full rounded-full"></div>
+    <div className="w-5/12 h-64 absolute -top-60 left-1/2 transform blur-[160px] -translate-x-1/2">
+      <div className="bg-primary w-full h-full rounded-full"></div>
     </div>
   );
 };
