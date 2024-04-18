4331059d8a24a1417920f0b2bb627eabd39295fc
Ivan Smiths <info@ivansmiths.com>
2024-04-18 11:10:26 +0200

    use fonts from import (#122)

--- app/blog/[slug]/components/MDXComponents.tsx
+++ app/blog/[slug]/components/MDXComponents.tsx
@@ -1,15 +1,7 @@
-import { Bebas_Neue } from "next/font/google";
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import { Code } from "bright";
-
-const bebas_neue = Bebas_Neue({
-  subsets: ["latin"],
-  weight: ["400"],
-  preload: true,
-  variable: "--font-bebas_neue",
-  display: "swap",
-});
+import { bebas_neue } from "../../../../utils/fonts";
 
 type HeadingProps = DetailedHTMLProps<
   HTMLAttributes<HTMLHeadingElement>,
