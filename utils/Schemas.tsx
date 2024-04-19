26da42448eb9af12af7fa4cedca6affed7049d40
Ivan Smiths <info@ivansmiths.com>
2024-04-19 19:38:58 +0200

    add wrongly remove schemas (#122)

--- utils/Schemas.tsx
+++ utils/Schemas.tsx
@@ -86,6 +86,49 @@ export const blogSchema = (post: BlogProps) => {
   const tags: string[] = post.tags.split(" ");
   return [
     {
+      "@context": "http://schema.org",
+      "@type": "BreadcrumbList",
+      itemListElement: [
+        {
+          "@type": "ListItem",
+          position: 1,
+          name: "Ivan Smiths, Frontend UI/UX Developer from Wiesbaden",
+          item: "https://ivansmiths.com",
+        },
+        {
+          "@type": "ListItem",
+          position: 2,
+          name: "Ivan Smiths, all blog posts",
+          item: "https://ivansmiths.com/blog",
+        },
+        {
+          "@type": "ListItem",
+          position: 3,
+          name: post.title,
+          item: `https://ivansmiths.com/blog/${post.slug}`,
+        },
+      ],
+    },
+    {
+      "@type": "Blog",
+      "@id": "https://ivansmiths.com/blog/",
+      mainEntityOfPage: "https://ivansmiths.com/blog",
+      name: "Ivan Smiths's Blog",
+      description: "Blog about React, Next.js and other frontend technologies",
+      publisher: {
+        "@type": "Person",
+        "@id": "https://ivansmiths.com",
+        name: "Ivan Smiths",
+        logo: {
+          "@type": "ImageObject",
+          "@id": "https://ivansmiths.com/home.png",
+          url: "https://ivansmiths.com/home.png",
+          width: "2880",
+          height: "1558",
+        },
+      },
+    },
+    {
       "@type": "BlogPosting",
       "@id": `https://ivansmiths.com/blog/${post.slug}/#BlogPosting`,
       mainEntityOfPage: `https://ivansmiths.com/blog/${post.slug}`,
