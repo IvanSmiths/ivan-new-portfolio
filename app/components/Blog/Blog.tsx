97bfbad97d33e8fc7829c1f922c802358675ef3e
Ivan Smiths <info@ivansmiths.com>
2024-04-20 00:12:22 +0200

    fix alignment (#122)

--- app/components/Blog/Blog.tsx
+++ app/components/Blog/Blog.tsx
@@ -9,22 +9,27 @@ const Blog: FC = async () => {
       <span className="md:col-start-1 max-sm:mb-small md:col-end-3 col-span-full lato">
         Latest blogposts
       </span>
-      <div className="md:col-start-4 md:col-end-13 col-span-full flex flex-col gap-regular">
+      <div className="md:col-start-4 md:col-end-13 col-span-full flex flex-col">
         {posts.map((post: Posts, index: number) => (
           <>
             <Link
-              className="flex justify-between items-center"
+              className="flex justify-between items-center px-small hover:bg-primary min-h-32 transition group"
               key={post?.slug}
               href={`blog/${post?.slug}`}
             >
               <div className="flex md:gap-medium gap-small items-center">
-                <span className="lato">0{index + 1}</span>
-                <h3 className="font-semibold md:text-3xl text-xl text-left">
+                <span className="lato group-hover:text-secondaryLight transition">
+                  0{index + 1}
+                </span>
+                <h3 className="font-semibold md:text-3xl text-xl text-left group-hover:text-secondary transition">
                   {post?.title}
                 </h3>
               </div>
               <div className="sm:flex ml-regular flex-col gap-smallest min-w-fit hidden text-right">
-                <time className="lato font-bold" dateTime={post?.date}>
+                <time
+                  className="lato font-bold group-hover:text-secondaryLight transition"
+                  dateTime={post?.date}
+                >
                   {post?.date
                     ? new Date(post?.date).toLocaleDateString("en-us", {
                         year: "numeric",
@@ -33,7 +38,9 @@ const Blog: FC = async () => {
                       })
                     : "Date not available"}
                 </time>
-                <span className="lato">{post?.time} min to read</span>
+                <span className="lato group-hover:text-secondaryLight transition">
+                  {post?.time} min to read
+                </span>
               </div>
             </Link>
             <div className="h-[1px] bg-primaryLight w-full"></div>
