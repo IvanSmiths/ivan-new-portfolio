809a52d02dde9698e5330479e9437468ea643c34
Ivan Smiths <info@ivansmiths.com>
2024-04-19 10:28:45 +0200

    increase margin (#122)

--- app/blog/[slug]/components/Sections.tsx
+++ app/blog/[slug]/components/Sections.tsx
@@ -3,7 +3,10 @@ import { FC } from "react";
 const Sections: FC = ({ children, ...props }: any) => {
   console.log(children);
   return (
-    <div {...props} className="absolute h-full top-medium mt-small left-0 z-40">
+    <div
+      {...props}
+      className="absolute h-full top-medium mt-regular left-0 z-40"
+    >
       <div className="sticky top-1/4 flex flex-col">{children}</div>
     </div>
   );
