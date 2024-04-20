e88b19d093bdd2f1aa0651d2986269d45b556320
Ivan Smiths <info@ivansmiths.com>
2024-04-20 12:28:58 +0200

    add optional limit to fetch (#122)

--- utils/getPosts.ts
+++ utils/getPosts.ts
@@ -20,25 +20,29 @@ export type Post = {
 
 export type Posts = Post | null;
 
-export const getPosts = cache(async () => {
+export const getPosts = cache(async (limit?: number) => {
   const posts: string[] = await fs.readdir("./blogposts/");
 
+  let filteredPosts = posts.filter(
+    (file) => path.extname(file) === ".md" || path.extname(file) === ".mdx",
+  );
+
+  if (limit !== undefined) {
+    filteredPosts = filteredPosts.slice(0, limit);
+  }
+
   return Promise.all(
-    posts
-      .filter(
-        (file) => path.extname(file) === ".md" || path.extname(file) === ".mdx",
-      )
-      .map(async (file): Promise<Post> => {
-        const filePath = `./blogposts/${file}`;
-        const postContent = await fs.readFile(filePath, "utf8");
-        const { data, content } = matter(postContent);
-        return { ...data, body: content } as Post;
-      }),
+    filteredPosts.map(async (file): Promise<Post> => {
+      const filePath = `./blogposts/${file}`;
+      const postContent = await fs.readFile(filePath, "utf8");
+      const { data, content } = matter(postContent);
+      return { ...data, body: content } as Post;
+    }),
   );
 });
 export default getPosts;
 
-export async function getPost(slug: string): Promise<Posts | undefined> {
-  const posts: Posts[] = await getPosts();
-  return posts.find((post: Posts): boolean => post?.slug === slug);
+export async function getPost(slug: string): Promise<Post | undefined> {
+  const posts: Post[] = await getPosts();
+  return posts.find((post: Post): boolean => post?.slug === slug);
 }
