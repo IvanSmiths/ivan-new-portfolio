a6a97bb55a332d83884c2ebe5df10780dbac1715
Ivan Smiths <info@ivansmiths.com>
2024-04-21 03:15:14 +0200

    try fix deployment (#122)

--- app/components/Blog/Blog.tsx
+++ app/components/Blog/Blog.tsx
@@ -3,7 +3,7 @@ import getPosts, { Posts } from "../../../utils/getPosts";
 import Link from "next/link";
 
 const Blog: FC = async () => {
-  const posts: Posts[] = await getPosts();
+  const posts: Posts[] = await getPosts(1);
   return (
     <div className="grid md:m-0">
       <span className="md:col-start-1 max-sm:mb-small md:col-end-3 col-span-full lato">
