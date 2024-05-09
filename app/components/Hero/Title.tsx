13eebac74bfa1d2a80ce76540d80b8a7036ad17d
IvanSmiths <info@ivansmiths.com>
2024-05-09 19:33:57 +0200

    remove link

--- app/components/Hero/Title.tsx
+++ app/components/Hero/Title.tsx
@@ -4,15 +4,7 @@ const Title: FC = () => {
   return (
     <div className="col-start-1 col-end-8 md:col-end-8 lg:col-end-7 my-medium md:mb-small">
       <h1 className="sm:text-4xl text-2xl">
-        UI/UX Developer with 3 years of experience. Seeking the limit. Currently
-        at {""}
-        <a
-          href="https://www.linkedin.com/company/td-cowen/"
-          target="_blank"
-          className="underline underline-offset-4 sm:text-4xl text-2xl"
-        >
-          TD Cowen.
-        </a>
+        UI/UX Developer with 3 years of experience. <br /> Seeking the limit.
       </h1>
     </div>
   );
