e50372d88ac9587c5ec5e4ae819487528243ed44
IvanSmiths <info@ivansmiths.com>
2024-05-10 14:44:01 +0200

    try  (#185)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,4 @@
 import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
-dotenv.config();
 
 export default {
     schema: "./utils/db/schema.ts",
@@ -8,7 +6,7 @@ export default {
     driver: "turso",
     dialect: "sqlite",
     dbCredentials: {
-        url: process.env.TURSO_DATABASE_URL! as string,
-        authToken: process.env.TURSO_AUTH_TOKEN as string,
+        url: process.env.TURSO_DATABASE_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN,
     },
 } satisfies Config;
