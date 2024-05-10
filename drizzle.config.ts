6eda87e12cda35b939c76408e1460f0357025c56
IvanSmiths <info@ivansmiths.com>
2024-05-10 13:38:15 +0200

    test

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,4 @@ export default {
     out: "./utils/db/migrations",
     driver: "turso",
     dialect: "sqlite",
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-        authToken: process.env.DATABASE_AUTH_TOKEN,
-    },
 } satisfies Config;
