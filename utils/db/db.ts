bfec153b7098e1886f4d1d7505d3522e6110b711
IvanSmiths <info@ivansmiths.com>
2024-05-10 19:48:54 +0200

    remove dotenv (#185)

--- utils/db/db.ts
+++ utils/db/db.ts
@@ -1,10 +1,10 @@
 import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
 import { createClient } from "@libsql/client";
-import 'dotenv/config';
 
 export const turso = createClient({
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
-export const db = drizzle(turso);
+export const db = drizzle(turso, { schema });
