028205be8c5b91e5d8b80a08d3fe6e3782424cd2
IvanSmiths <info@ivansmiths.com>
2024-05-10 14:15:16 +0200

    test green pipeline (#185)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,4 +5,8 @@ export default {
     out: "./utils/db/migrations",
     driver: "turso",
     dialect: "sqlite",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+        authToken: process.env.DATABASE_AUTH_TOKEN,
+    },
 } satisfies Config;
