01cd31bfe80a7be5c29a445af3b328ae6ced61ed
IvanSmiths <info@ivansmiths.com>
2024-05-12 22:05:55 +0200

    fix types (#186)

--- app/blog/[slug]/components/Draggable.tsx
+++ app/blog/[slug]/components/Draggable.tsx
@@ -1,32 +1,34 @@
 "use client";
-import { FC, useEffect, useRef, useState } from 'react'
+import { FC, useEffect, useRef, useState, MouseEventHandler } from 'react'
 
 type DraggableProps = {
     type: 'div' | 'span' | 'p'
 }
 
 const Draggable: FC<DraggableProps> = ({ type }) => {
-    const [width, setWidth] = useState(200);
-    const [height, setHeight] = useState(100);
-    const isResizing = useRef(false);
-    const prevMouseY = useRef(null);
-    const prevMouseX = useRef(null);
+    const [width, setWidth] = useState<number>(200);
+    const [height, setHeight] = useState<number>(100);
+    const isResizing = useRef<boolean>(false);
+    const prevMouseY = useRef<number | null>(null);
+    const prevMouseX = useRef<number | null>(null);
 
-    const handleMouseMove = (e) => {
+    const handleMouseMove = (e: MouseEvent) => {
         if (!isResizing.current) return;
 
-        const newWidth = width + (e.clientX - prevMouseX.current);
-        const newHeight = height + (e.clientY - prevMouseY.current);
-        const constrainedWidth = Math.min(Math.max(newWidth, 200), 600);
-        const constrainedHeight = Math.min(Math.max(newHeight, 100), 400);
+        if (prevMouseX.current && prevMouseY.current) {
+            const newWidth = width + (e.clientX - prevMouseX.current);
+            const newHeight = height + (e.clientY - prevMouseY.current);
+            const constrainedWidth = Math.min(Math.max(newWidth, 200), 600);
+            const constrainedHeight = Math.min(Math.max(newHeight, 100), 400);
 
-        setWidth(constrainedWidth);
-        setHeight(constrainedHeight);
-        prevMouseY.current = e.clientY;
-        prevMouseX.current = e.clientX;
+            setWidth(constrainedWidth);
+            setHeight(constrainedHeight);
+            prevMouseY.current = e.clientY;
+            prevMouseX.current = e.clientX;
+        }
     };
 
-    const handleMouseDown = (e) => {
+    const handleMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
         isResizing.current = true;
         prevMouseY.current = e.clientY;
         prevMouseX.current = e.clientX;
@@ -62,13 +64,15 @@ const Draggable: FC<DraggableProps> = ({ type }) => {
     }
 
     return (
-        <div style={{ width: width, height: height }}>
-            <div onMouseDown={handleMouseDown}
-                onMouseUp={handleMouseUp}
-                style={{ width: `${width}px`, height: `${height}px`, cursor: 'ew-resize', border: '1px solid black' }} className={classNames}>
-                <span>Centered</span>
-            </div>
+
+        <div
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
+            style={{ width: `${width}px`, height: `${height}px`, cursor: 'ew-resize' }}
+            className={classNames}>
+            <span>Centered</span>
         </div>
+
     )
 }
 
