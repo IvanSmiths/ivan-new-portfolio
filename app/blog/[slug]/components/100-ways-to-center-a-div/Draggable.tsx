af43b742ee257c8e14ee5142a67bcb687207f7b0
IvanSmiths <info@ivansmiths.com>
2024-05-16 14:52:47 +0200

    add types (#186)

--- app/blog/[slug]/components/100-ways-to-center-a-div/Draggable.tsx
+++ app/blog/[slug]/components/100-ways-to-center-a-div/Draggable.tsx
@@ -15,25 +15,28 @@ const Draggable: FC<DraggableProps> = ({ parent, child }) => {
   const prevMouseY = useRef<number | null>(null);
   const prevMouseX = useRef<number | null>(null);
 
-  const handleMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
+  const handleMouseDown: MouseEventHandler<HTMLDivElement> = (e): void => {
     isResizing.current = true;
     prevMouseY.current = e.clientY;
     prevMouseX.current = e.clientX;
   };
 
-  const handleMouseUp = () => {
+  const handleMouseUp = (): void => {
     isResizing.current = false;
   };
 
   useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
+    const handleMouseMove = (e: MouseEvent): void => {
       if (!isResizing.current) return;
 
       if (prevMouseX.current && prevMouseY.current) {
-        const newWidth = width + (e.clientX - prevMouseX.current);
-        const newHeight = height + (e.clientY - prevMouseY.current);
-        const constrainedWidth = Math.min(Math.max(newWidth, 200), 600);
-        const constrainedHeight = Math.min(Math.max(newHeight, 200), 400);
+        const newWidth: number = width + (e.clientX - prevMouseX.current);
+        const newHeight: number = height + (e.clientY - prevMouseY.current);
+        const constrainedWidth: number = Math.min(Math.max(newWidth, 200), 600);
+        const constrainedHeight: number = Math.min(
+          Math.max(newHeight, 200),
+          400,
+        );
 
         setWidth(constrainedWidth);
         setHeight(constrainedHeight);
@@ -49,9 +52,9 @@ const Draggable: FC<DraggableProps> = ({ parent, child }) => {
     };
   }, [height, width]);
 
-  let parentClassNames = "bg-primary rounded-md relative";
-  let childClassNames = `${child ? child : ""} py-4 px-6 text-primary rounded-md w-fit bg-secondary`;
-  let childText = "";
+  let parentClassNames: string = "bg-primary rounded-md relative";
+  let childClassNames: string = `${child ? child : ""} py-4 px-6 text-primary rounded-md w-fit bg-secondary`;
+  let childText: string = "";
 
   switch (parent) {
     case "flexbox":
