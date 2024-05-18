72121e7651fdd6a49fdf57a1d11462c6a6ba42e8
IvanSmiths <info@ivansmiths.com>
2024-05-18 20:03:24 +0200

    improve schema (#185)

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,12 @@
+import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const photosTable = sqliteTable("photos", {
-  id: integer("id").primaryKey(),
-  alt: text("name").notNull(),
-  desktopUrl: text("desktopUrl").notNull(),
-  mobileUrl: text("mobileUrl").notNull(),
+export const photos = sqliteTable("photos", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  desktopUrl: text("desktop_url").notNull(),
+  mobileUrl: text("mobile_url").notNull(),
+  alt: text("alt").notNull(),
 });
