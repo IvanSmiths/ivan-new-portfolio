b442b3e453523c195a16ed940bce310374cb62aa
IvanSmiths <info@ivansmiths.com>
2024-05-18 20:01:33 +0200

    add schema and type (#185)

--- db/db.ts
+++ db/db.ts
@@ -1,11 +1,10 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 import { createClient } from "@libsql/client";
 
 const client = createClient({
   url: process.env.TURSO_CONNECTION_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN!,
+  authToken: process.env.TURSO_TOKEN!,
 });
 
 export const db = drizzle(client, {
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,15 @@
-import "dotenv/config";
+import type { Config } from "drizzle-kit";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./migrations",
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
   dialect: "sqlite",
   driver: "turso",
+  verbose: true,
+  strict: true,
   dbCredentials: {
     url: process.env.TURSO_CONNECTION_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
-});
+} satisfies Config);
