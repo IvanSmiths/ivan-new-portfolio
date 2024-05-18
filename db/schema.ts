2b6bd569b5a5e3ae8398de723f72fd1f6e64070d
IvanSmiths <info@ivansmiths.com>
2024-05-18 12:52:40 +0200

    remove tablet column (#185)

--- db/schema.ts
+++ db/schema.ts
@@ -4,6 +4,5 @@ export const photos = sqliteTable("photos", {
   id: integer("id").primaryKey(),
   alt: text("name").notNull(),
   desktopUrl: text("desktopUrl").notNull(),
-  tabletUrl: text("tabletUrl").notNull(),
   mobileUrl: text("mobileUrl").notNull(),
 });
