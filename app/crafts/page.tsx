9ec59ffdf255223500ea6d4f1902000baf2aefd3
IvanSmiths <info@ivansmiths.com>
2024-05-18 20:02:38 +0200

    fetch photos (#185)

--- app/crafts/page.tsx
+++ app/crafts/page.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react";
 import Navbar, { Position } from "../globalComponents/Navbar/Navbar";
 import { db } from "../../db/db";
-import { photosTable } from "../../db/schema";
+import { photos as photosTable } from "../../db/schema";
 
 const Crafts: FC = async () => {
   const photos = await db
     .select({ desktopUrl: photosTable.desktopUrl, alt: photosTable.alt })
     .from(photosTable)
     .all();
+
+  console.log(photos);
+
   return (
     <div>
       <Navbar position={Position.Fixed} />
