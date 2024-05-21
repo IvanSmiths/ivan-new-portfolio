0ec83a831e25882753dd9faa1ccf33a043570e9e
IvanSmiths <info@ivansmiths.com>
2024-05-21 13:54:16 +0200

    add renders schema (#185)

--- db/schema.ts
+++ db/schema.ts
@@ -10,3 +10,16 @@ export const photos = sqliteTable("photos", {
   mobileUrl: text("mobile_url").notNull(),
   alt: text("alt").notNull(),
 });
+
+export const renders = sqliteTable("renders", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  createdAt: text("created_at")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  desktopUrl: text("desktop_url").notNull(),
+  mobileUrl: text("mobile_url").notNull(),
+  alt: text("alt").notNull(),
+  isHorizontal: integer("is_horizontal", { mode: "boolean" })
+    .default(false)
+    .notNull(),
+});
