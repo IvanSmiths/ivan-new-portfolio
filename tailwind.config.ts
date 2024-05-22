5369a9c44f84ca405e08becf436073fa24156c94
IvanSmiths <info@ivansmiths.com>
2024-05-22 20:09:34 +0200

    add animation (#185)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,15 @@ const config: Config = {
         large: "var(--large)",
         section: "var(--section)",
       },
+      animation: {
+        "fade-in": "fadeIn 400ms ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
     },
   },
   plugins: [],
