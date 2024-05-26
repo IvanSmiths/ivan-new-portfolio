239081ff128e36d3c7751524c2702e6c1a715c5e
IvanSmiths <info@ivansmiths.com>
2024-05-26 23:01:05 +0200

    add suspense component (#185)

---
+++ app/crafts/loading.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Loading: FC = () => {
+  return (
+    <div className="absolute left-0 top-0 h-full w-full bg-red-900">
+      Loading
+    </div>
+  );
+};
+
+export default Loading;
