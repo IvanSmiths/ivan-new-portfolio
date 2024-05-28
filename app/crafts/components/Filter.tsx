b775765be933cf55829d1f2b979b2b649699a5df
IvanSmiths <info@ivansmiths.com>
2024-05-28 17:05:50 +0200

    write filter test (#185)

--- app/crafts/components/Filter.tsx
+++ app/crafts/components/Filter.tsx
@@ -23,6 +23,7 @@ const Filter: FC<FilterProps> = ({ currentPage }) => {
     <section className="fixed bottom-4 z-20 flex w-full flex-wrap items-center justify-center gap-small gap-y-2">
       {filters.map((filter, index: number) => (
         <Link
+          data-cy={`link-${filter.page}`}
           key={index}
           href={`/crafts/${filter.url}`}
           className={`lato rounded-full p-4 py-3 font-bold text-primary ${
