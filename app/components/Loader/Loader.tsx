b9a9d3399f17f618ea4d181752f74f677a9eba63
IvanSmiths <info@ivansmiths.com>
2024-06-14 10:43:07 +0200

    add test span (#191)

--- app/components/Loader/Loader.tsx
+++ app/components/Loader/Loader.tsx
@@ -77,7 +77,7 @@ const Loader: FC = () => {
           className="absolute inset-0 z-20 mx-auto my-auto h-fit w-full opacity-0"
         >
           <Header />
-          <span>test</span>
+          <span>test 2</span>
         </div>
       </div>
     </>
