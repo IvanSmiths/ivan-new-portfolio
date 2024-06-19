86f171a1efd2c106e14c4ef381fd7c97e6179c30
IvanSmiths <info@ivansmiths.com>
2024-06-19 18:58:29 +0200

    try start server (#193)

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
+  webServer: {
+    command: "npm run dev",
+    url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     trace: "on-first-retry",
   },
