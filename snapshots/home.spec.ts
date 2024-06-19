e6465e86cb88b79e6b8cc11439df81a83c0564eb
IvanSmiths <info@ivansmiths.com>
2024-06-19 12:43:34 +0200

    add snapshot (#193)

--- snapshots/home.spec.ts
+++ snapshots/home.spec.ts
@@ -1,10 +1,24 @@
-import { expect, test } from "@playwright/test";
+import { expect, Locator, test } from "@playwright/test";
 
 test.beforeEach(async ({ page }): Promise<void> => {
   await page.goto("http://localhost:3000");
+  await page
+    .getByTestId("navbar")
+    .evaluate(
+      (element: HTMLElement): string => (element.style.display = "none"),
+    );
 });
 
 test("snapshot home hero section", async ({ page }): Promise<void> => {
-  await page.goto("http://localhost:3000");
-  await expect(page).toHaveScreenshot("home-hero.png");
+  const heroLocator: Locator = page.getByTestId("homeHero");
+  await heroLocator.click();
+  await expect(page).toHaveScreenshot("home-hero.png", {
+    maxDiffPixelRatio: 0.2,
+  });
+});
+
+test("snapshot home about section", async ({ page }): Promise<void> => {
+  const aboutLocator: Locator = page.getByTestId("homeAbout");
+  await aboutLocator.click();
+  await expect(page).toHaveScreenshot("home-about.png");
 });
