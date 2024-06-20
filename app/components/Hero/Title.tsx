39020c37b809106f004dee6c0accc397219056eb
IvanSmiths <info@ivansmiths.com>
2024-06-20 15:28:41 +0200

    test ci/cd (#193)

--- app/components/Hero/Title.tsx
+++ app/components/Hero/Title.tsx
@@ -5,6 +5,7 @@ const Title: FC = () => {
     <div className="col-start-1 col-end-8 my-medium md:col-end-8 md:mb-small lg:col-end-7">
       <h1 className="text-2xl sm:text-4xl">
         UI/UX Developer with 3 years of experience. <br /> Seeking the limit.
+        test
       </h1>
     </div>
   );
