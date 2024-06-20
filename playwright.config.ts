ebe69f0d12404fb754db0c561a3b280b1488928f
IvanSmiths <info@ivansmiths.com>
2024-06-20 11:32:40 +0200

    add mobile snapshots (#193)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig, devices } from "@playwright/test";
 
+const ci: string | undefined = process.env.CI;
+
 export default defineConfig({
   testDir: "./snapshots",
-  snapshotPathTemplate: "{testDir}/{testFilePath}-snapshots/{arg}{ext}",
+  snapshotPathTemplate:
+    "{testDir}/{testFilePath}-snapshots/{arg}-{projectName}{ext}",
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "github" : "html",
+  forbidOnly: !!ci,
+  retries: ci ? 2 : 0,
+  workers: ci ? 1 : undefined,
+  reporter: ci ? "github" : "html",
   webServer: {
     command: "npm run dev",
     url: "http://127.0.0.1:3000",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !ci,
   },
   use: {
     trace: "on-first-retry",
@@ -23,6 +26,10 @@ export default defineConfig({
         ...devices["Desktop Chrome"],
         viewport: { width: 1280, height: 900 },
       },
+    },
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
     },
   ],
 });
