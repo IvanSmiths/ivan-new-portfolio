b20d068c84b2e4c33fae7c2c1b2d88c3c6ee4c3e
IvanSmiths <info@ivansmiths.com>
2024-08-13 18:32:03 +0200

    add basic text and brand color (#197)

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -7,7 +7,7 @@ const About: FC = async () => {
       className="mt-large grid min-h-[100vh] md:mt-section"
     >
       <div className="col-span-full uppercase">
-        <div className="flex flex-row flex-wrap items-center justify-between gap-small">
+        <div className="flex flex-row flex-wrap items-center justify-between gap-small pt-5">
           <h2 className="bebas text-[13rem] leading-[13rem]">
             I expertly blend
           </h2>
@@ -23,8 +23,21 @@ const About: FC = async () => {
             Background
           </h2>
         </div>
-        <div></div>
-        <div></div>
+        <div className="flex flex-row flex-wrap items-center justify-between gap-small pt-5">
+          <h2 className="bebas text-[13rem] leading-[13rem]">With</h2>
+          <h2 className="bebas text-[13rem] leading-[13rem]">~</h2>
+          <img
+            className="-mt-4 h-40 flex-1 object-cover"
+            src="/photo-of-me-about.png"
+            alt="photo of me"
+          />
+          <h2 className="bebas text-[13rem] leading-[13rem]">My</h2>
+        </div>
+        <div className="bg-brand flex flex-row flex-wrap items-center justify-between gap-small pt-5">
+          <h2 className="bebas text-[13rem] leading-[13rem]">Development</h2>
+          <h2 className="bebas text-[13rem] leading-10">&#x2731;</h2>
+          <h2 className="bebas text-[13rem] leading-10">Mastery</h2>
+        </div>
       </div>
     </main>
   );
