0ca37e32c9e17eda406d47d9a2430ea50b961a5b
IvanSmiths <info@ivansmiths.com>
2024-08-14 14:19:54 +0200

    improve mobile style (#197)

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 
 const About: FC = async () => {
   const textStyle =
-    "bebas lg:text-[11vw] lg:leading-[11vw] md:text-[20vw] md:leading-[19vw] text-[18vw] leading-[17vw]";
+    "bebas lg:text-[11vw] lg:leading-[11vw] md:text-[20vw] md:leading-[19vw] text-[18vw] leading-[17vw] text-center lg:text-left";
 
   type WrapperProps = {
     children: ReactNode;
@@ -18,7 +18,7 @@ const About: FC = async () => {
 
   const Wrapper: FC<WrapperProps> = ({ children, bgClass = "" }) => (
     <div
-      className={`flex flex-row flex-wrap items-center justify-between gap-small px-3 pt-5 ${bgClass}`}
+      className={`flex flex-row flex-wrap items-center justify-center gap-small px-3 pt-5 lg:justify-between ${bgClass}`}
     >
       {children}
     </div>
@@ -51,14 +51,14 @@ const About: FC = async () => {
         </Wrapper>
         <Wrapper bgClass="bg-primary">
           <Content textColor="text-secondary">Design</Content>
-          <span className="h-3 flex-1 rounded-full bg-secondary"></span>
+          <span className="hidden h-3 flex-1 rounded-full bg-secondary lg:block"></span>
           <Content textColor="text-secondary">Background</Content>
         </Wrapper>
         <Wrapper>
           <Content>With</Content>
-          <Content>~</Content>
+          <Content isMobileHidden={true}>~</Content>
           <img
-            className="-mt-4 h-20 w-2/4 flex-1 object-cover md:h-40"
+            className="-mt-4 hidden h-20 w-2/4 flex-1 object-cover md:h-40 lg:block"
             src="/photo-of-me-about.png"
             alt="photo of me"
             width="978"
@@ -69,7 +69,7 @@ const About: FC = async () => {
         </Wrapper>
         <Wrapper bgClass="bg-brand">
           <Content>Development</Content>
-          <Content>&#x2605;</Content>
+          <Content isMobileHidden={true}>&#x2605;</Content>
           <Content>Mastery.</Content>
         </Wrapper>
       </section>
