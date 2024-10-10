48ca7868e557903fd4bffb5453e2d3e2db9e203a
IvanSmiths <info@ivansmiths.com>
2024-10-10 23:25:44 +0200

    add types (#209)

--- app/globalComponents/Footer/Footer.tsx
+++ app/globalComponents/Footer/Footer.tsx
@@ -2,6 +2,18 @@ import { FC } from "react";
 import Logo from "../../globalComponents/Logo/Logo";
 import Social from "./Social";
 
+type SocialLink = {
+  text: string;
+  link: string;
+};
+
+const socialLinks: SocialLink[] = [
+  { text: "Github", link: "https://github.com/IvanSmiths" },
+  { text: "LinkedIn", link: "https://www.linkedin.com/in/ivan-fabbri" },
+  { text: "YouTube", link: "https://youtube.com/@ivansmiths" },
+  { text: "info@ivansmiths.com", link: "mailto:info@ivansmiths.com" },
+];
+
 const Footer: FC = () => {
   return (
     <footer
@@ -10,13 +22,9 @@ const Footer: FC = () => {
       className="flex min-h-[70vh] flex-col justify-end gap-2 px-small pb-small md:min-h-[80vh]"
     >
       <div className="mb-medium flex flex-wrap gap-small gap-y-10">
-        <Social text="Github" link="https://github.com/IvanSmiths" />
-        <Social
-          text="LinkedIn"
-          link="https://www.linkedin.com/in/ivan-fabbri"
-        />
-        <Social text="YouTube" link="https://youtube.com/@ivansmiths" />
-        <Social text="info@ivansmiths.com" link="mailto:info@ivansmiths.com" />
+        {socialLinks.map(({ text, link }) => (
+          <Social key={text} text={text} link={link} />
+        ))}
       </div>
       <div className="flex justify-between">
         <span>©2024 Ivan Smiths</span>
