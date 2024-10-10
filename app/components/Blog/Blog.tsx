4324ed318f301b72907f52c4fa6ddc4af04e6724
IvanSmiths <info@ivansmiths.com>
2024-10-10 23:45:22 +0200

    fix hover (#209)

--- app/components/Blog/Blog.tsx
+++ app/components/Blog/Blog.tsx
@@ -5,30 +5,30 @@ import Link from "next/link";
 const Blog: FC = () => {
   const posts: Posts[] = getBlogPosts();
   return (
-    <div data-testid="homeBlogSection" className="mb-section grid">
+    <div data-testid="homeBlogSection" className="grid">
       <span className="col-span-full max-sm:mb-small md:col-start-1 md:col-end-3">
         Latest blogposts
       </span>
       <div className="col-span-full flex flex-col md:col-start-4 md:col-end-13">
         {posts.map((post: Posts, index: number) => (
           <div key={index}>
-            <div className="bg-dark dark:bg-light h-[1px] w-full"></div>
+            <div className="h-[1px] w-full bg-dark dark:bg-light"></div>
             <Link
               data-testid={`blogPostLink${index}`}
               data-cy="blogPost"
-              className="hover:bg-dark dark:hover:bg-light group flex min-h-32 flex-col items-start justify-center gap-small transition md:flex-row md:items-center md:justify-between md:px-small"
+              className="group flex min-h-32 flex-col items-start justify-center gap-small transition hover:bg-dark dark:hover:bg-light md:flex-row md:items-center md:justify-between md:px-small"
               href={`blog/${post?.slug}`}
             >
               <div className="flex flex-col items-start gap-small md:flex-row md:items-center md:gap-medium">
                 <span
                   data-cy="blogPostIndex"
-                  className="group-hover:text-light darkgroup-hover:text-dark hidden transition md:block"
+                  className="hidden transition group-hover:text-light dark:group-hover:text-dark md:block"
                 >
                   0{index + 1}
                 </span>
                 <h3
                   data-cy="blogPostTitle"
-                  className="group-hover:text-light dark:group-hover:text-dark text-left text-xl font-semibold transition md:text-3xl"
+                  className="text-left text-xl font-semibold transition group-hover:text-light dark:group-hover:text-dark md:text-3xl"
                 >
                   {post?.metadata.title}
                 </h3>
@@ -36,13 +36,13 @@ const Blog: FC = () => {
               <div className="flex min-w-fit flex-row gap-smallest md:ml-regular md:flex-col md:text-right">
                 <span
                   data-cy="blogPostCategory"
-                  className="group-hover:text-light dark:group-hover:text-dark transition"
+                  className="transition group-hover:text-light dark:group-hover:text-dark"
                 >
                   {post?.metadata.category}
                 </span>
                 <time
                   data-cy="blogPostDate"
-                  className="group-hover:text-light dark:group-hover:text-dark transition"
+                  className="transition group-hover:text-light dark:group-hover:text-dark"
                   dateTime={post?.metadata.date}
                 >
                   {post?.metadata.date
@@ -58,13 +58,13 @@ const Blog: FC = () => {
                 </time>
                 <span
                   data-cy="blogPostTime"
-                  className="group-hover:text-light dark:group-hover:text-dark transition"
+                  className="transition group-hover:text-light dark:group-hover:text-dark"
                 >
                   {post?.metadata.time} min to read
                 </span>
               </div>
             </Link>
-            <div className="bg-dark dark:bg-light h-[1px] w-full"></div>
+            <div className="h-[1px] w-full bg-dark dark:bg-light"></div>
           </div>
         ))}
       </div>
