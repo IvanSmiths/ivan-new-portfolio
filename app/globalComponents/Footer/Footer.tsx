e704c73042842436828e5e4d0b9b15ae71b58631
IvanSmiths <info@ivansmiths.com>
2024-10-10 23:22:50 +0200

    add proper links (#209)

--- app/globalComponents/Footer/Footer.tsx
+++ app/globalComponents/Footer/Footer.tsx
@@ -10,16 +10,13 @@ const Footer: FC = () => {
       className="flex min-h-[70vh] flex-col justify-end gap-2 px-small pb-small md:min-h-[80vh]"
     >
       <div className="mb-medium flex flex-wrap gap-small gap-y-10">
-        <Social text="Github" link="https://github.com/Ivan-Smith" />
+        <Social text="Github" link="https://github.com/IvanSmiths" />
         <Social
           text="LinkedIn"
-          link="https://www.linkedin.com/in/ivan-smith-b3b1b81a4/"
+          link="https://www.linkedin.com/in/ivan-fabbri"
         />
-        <Social text="YouTube" link="https://twitter.com/Ivansmiths" />
-        <Social
-          text="info@ivansmiths.com"
-          link="https://twitter.com/Ivansmiths"
-        />
+        <Social text="YouTube" link="https://youtube.com/@ivansmiths" />
+        <Social text="info@ivansmiths.com" link="mailto:info@ivansmiths.com" />
       </div>
       <div className="flex justify-between">
         <span>©2024 Ivan Smiths</span>
