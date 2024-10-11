3036735fe92d32f0377e6a8988e2f7144005954a
IvanSmiths <info@ivansmiths.com>
2024-10-11 21:22:14 +0200

    add animation and mobile adjustments (#209)

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -15,15 +15,15 @@ const About = () => {
     { label: "Blend", id: "Blend" },
   ];
   const secondRow = [
-    { label: "My", id: "4" },
-    { label: "Design", id: "5" },
-    { label: "Background", id: "6" },
-    { label: "With", id: "7" },
+    { label: "My", id: "My" },
+    { label: "Design", id: "Design" },
+    { label: "Background", id: "Background" },
+    { label: "With", id: "With" },
   ];
   const thirdRow = [
-    { label: "My", id: "8" },
-    { label: "Development", id: "9" },
-    { label: "Skills.", id: "10" },
+    { label: "My", id: "My-2" },
+    { label: "Development", id: "Development" },
+    { label: "Skills.", id: "Skills" },
   ];
 
   gsap.registerPlugin(useGSAP);
@@ -46,42 +46,66 @@ const About = () => {
       timeline.to("#Blend", {
         display: "block",
       });
+      timeline.to("#My", {
+        display: "block",
+      });
+      timeline.to("#Design", {
+        display: "block",
+      });
+      timeline.to("#Background", {
+        display: "block",
+      });
+      timeline.to("#With", {
+        display: "block",
+      });
+      timeline.to("#My-2", {
+        display: "block",
+      });
+      timeline.to("#Development", {
+        display: "block",
+      });
+      timeline.to("#Skills", {
+        display: "block",
+      });
+      timeline.to(["#Development", "#Skills"], {
+        color: "#FF4D4D",
+      });
     },
     { scope: containerRef },
   );
 
   return (
     <div ref={containerRef}>
-      <div ref={triggerRef} className="grid h-screen w-full bg-brand">
-        <div className="col-start-2 col-end-12 flex flex-col items-center justify-center gap-x-small gap-y-0 bg-red-900">
-          <div className="flex flex-wrap items-center gap-small">
+      <div ref={triggerRef} className="grid h-screen w-full">
+        <div className="col-start-1 col-end-7 flex flex-col items-center justify-center gap-x-small gap-y-0 md:col-start-2 md:col-end-12">
+          <div className="flex flex-wrap items-center justify-center gap-x-small">
             {firstRow.map(({ label, id }, index) => (
               <span
                 key={index}
                 id={id}
-                className={`${bebas_neue.className} ${index === 0 ? "block" : "hidden"} text-9xl`}
+                className={`${bebas_neue.className} ${index === 0 ? "block" : "hidden"} text-7xl md:text-8xl lg:text-9xl`}
               >
                 {label}
               </span>
             ))}
           </div>
-          <div className="flex flex-wrap items-center gap-small">
+          <div className="flex flex-wrap items-center justify-center gap-x-small">
             {secondRow.map(({ label, id }, index) => (
               <span
                 key={index}
                 id={id}
-                className={`${bebas_neue.className} hidden text-9xl`}
+                className={`${bebas_neue.className} hidden text-7xl md:text-8xl lg:text-9xl`}
               >
                 {label}
               </span>
             ))}
           </div>
-          <div className="flex flex-wrap items-center gap-small">
+          <div className="flex flex-wrap items-center justify-center gap-x-small">
             {thirdRow.map(({ label, id }, index) => (
               <span
                 key={index}
                 id={id}
-                className={`${bebas_neue.className} hidden text-9xl`}
+                className={`${bebas_neue.className} hidden text-7xl md:text-8xl lg:text-9xl`}
               >
                 {label}
               </span>
