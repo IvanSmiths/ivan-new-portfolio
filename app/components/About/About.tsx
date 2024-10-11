5cfcbcc4fbe8d654bf176d5e02c9fc3c628a1f43
IvanSmiths <info@ivansmiths.com>
2024-10-11 21:29:24 +0200

    add types (#209)

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -5,22 +5,29 @@ import { bebas_neue } from "../../../utils/fonts";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 
+type RowElement = {
+  label: string;
+  id: string;
+};
+
 const About = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
   const triggerRef = useRef<HTMLDivElement | null>(null);
 
-  const firstRow = [
+  const firstRow: RowElement[] = [
     { label: "I", id: "I" },
     { label: "Expertly", id: "Expertly" },
     { label: "Blend", id: "Blend" },
   ];
-  const secondRow = [
+
+  const secondRow: RowElement[] = [
     { label: "My", id: "My" },
     { label: "Design", id: "Design" },
     { label: "Background", id: "Background" },
     { label: "With", id: "With" },
   ];
-  const thirdRow = [
+
+  const thirdRow: RowElement[] = [
     { label: "My", id: "My-2" },
     { label: "Development", id: "Development" },
     { label: "Skills.", id: "Skills" },
@@ -29,7 +36,7 @@ const About = () => {
   gsap.registerPlugin(useGSAP);
 
   useGSAP(
-    () => {
+    (): void => {
       const timeline = gsap.timeline({
         scrollTrigger: {
           trigger: triggerRef.current,
@@ -40,36 +47,23 @@ const About = () => {
         },
       });
 
-      timeline.to("#Expertly", {
-        display: "block",
+      const elementIDs: string[] = [
+        "#Expertly",
+        "#Blend",
+        "#My",
+        "#Design",
+        "#Background",
+        "#With",
+        "#My-2",
+        "#Development",
+        "#Skills",
+      ];
+
+      elementIDs.forEach((id: string): void => {
+        timeline.to(id, { display: "block" });
       });
-      timeline.to("#Blend", {
-        display: "block",
-      });
-      timeline.to("#My", {
-        display: "block",
-      });
-      timeline.to("#Design", {
-        display: "block",
-      });
-      timeline.to("#Background", {
-        display: "block",
-      });
-      timeline.to("#With", {
-        display: "block",
-      });
-      timeline.to("#My-2", {
-        display: "block",
-      });
-      timeline.to("#Development", {
-        display: "block",
-      });
-      timeline.to("#Skills", {
-        display: "block",
-      });
-      timeline.to(["#Development", "#Skills"], {
-        color: "#FF4D4D",
-      });
+
+      timeline.to(["#Development", "#Skills"], { color: "#FF4D4D" });
     },
     { scope: containerRef },
   );
@@ -79,7 +73,7 @@ const About = () => {
       <div ref={triggerRef} className="grid h-screen w-full">
         <div className="col-start-1 col-end-7 flex flex-col items-center justify-center gap-x-small gap-y-0 md:col-start-2 md:col-end-12">
           <div className="flex flex-wrap items-center justify-center gap-x-small">
-            {firstRow.map(({ label, id }, index) => (
+            {firstRow.map(({ label, id }: RowElement, index: number) => (
               <span
                 key={index}
                 id={id}
@@ -90,7 +84,7 @@ const About = () => {
             ))}
           </div>
           <div className="flex flex-wrap items-center justify-center gap-x-small">
-            {secondRow.map(({ label, id }, index) => (
+            {secondRow.map(({ label, id }: RowElement, index: number) => (
               <span
                 key={index}
                 id={id}
@@ -101,7 +95,7 @@ const About = () => {
             ))}
           </div>
           <div className="flex flex-wrap items-center justify-center gap-x-small">
-            {thirdRow.map(({ label, id }, index) => (
+            {thirdRow.map(({ label, id }: RowElement, index: number) => (
               <span
                 key={index}
                 id={id}
