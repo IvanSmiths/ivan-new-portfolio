776332107736b43c3e34ccf671d8d3f46585be02
IvanSmiths <info@ivansmiths.com>
2024-10-13 19:32:25 +0200

    update snapshots (#211)

--- snapshots/global.spec.ts
+++ snapshots/global.spec.ts
@@ -5,10 +5,6 @@ test.beforeEach(async ({ page }): Promise<void> => {
   await page.goto("http://localhost:3000");
 });
 
-test("snapshot navbar", async ({ page }): Promise<void> => {
-  await snapshotElement(page, "navbar", "global-navbar.png");
-});
-
 test("snapshot footer", async ({ page }): Promise<void> => {
   await page
     .getByTestId("navbar")
