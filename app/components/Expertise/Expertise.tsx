0ef7478a5bee9828c1c8b0ad9cb4e3bf35a80b41
IvanSmiths <info@ivansmiths.com>
2024-10-16 16:48:09 +0200

    add component (#214)

--- app/components/Expertise/Expertise.tsx
+++ app/components/Expertise/Expertise.tsx
@@ -1,5 +1,13 @@
+import { bebas_neue } from "../../../utils/fonts";
+
 const Expertise = () => {
-  return <div></div>;
+  return (
+    <div className="h-screen w-full overflow-hidden">
+      <div className="h-full w-full">
+        <h2 className={`${bebas_neue.className} text-9xl`}>Expertises</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Expertise;
