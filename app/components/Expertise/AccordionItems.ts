835f2eb9445f32232e5ac62f07d3ea11b4db3261
IvanSmiths <info@ivansmiths.com>
2024-10-17 12:48:57 +0200

    refactor code (#214)

---
+++ app/components/Expertise/AccordionItems.ts
@@ -0,0 +1,50 @@
+export type AccordionItem = {
+  title: string;
+  rotation: number;
+  left: number;
+  top: number;
+  content: string;
+};
+
+export const accordionItems: AccordionItem[] = [
+  {
+    title: "Frontend",
+    rotation: 10,
+    left: 10,
+    top: 20,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title: "Backend",
+    rotation: -6,
+    left: 30,
+    top: 60,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title: "Design",
+    rotation: 8,
+    left: 45,
+    top: 35,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title: "Testing",
+    rotation: -8,
+    left: 60,
+    top: 60,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+  {
+    title: "3D Modeling",
+    rotation: 5,
+    left: 80,
+    top: 30,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+  },
+];
