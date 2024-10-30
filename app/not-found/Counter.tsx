b65ebc428b1a93835ae0ee8adcd28cd4af777527
IvanSmiths <info@ivansmiths.com>
2024-10-30 15:12:55 +0100

    remove on no attempts (#214)

--- app/not-found/Counter.tsx
+++ app/not-found/Counter.tsx
@@ -4,6 +4,9 @@ import { useButtonStore } from "../../utils/store";
 
 const Counter = () => {
   const { attempts, resetAll } = useButtonStore();
+
+  if (attempts === 0) return null;
+
   return (
     <div className="absolute right-small top-small z-30 px-4 py-2">
       Attempts: {attempts}
--- app/not-found/Main.tsx
+++ app/not-found/Main.tsx
@@ -12,7 +12,7 @@ const Main: FC = () => {
       <h1 className="absolute left-1/2 top-1/2 z-30 -translate-x-1/2 -translate-y-1/2 transform text-center text-7xl font-extrabold uppercase">
         How did you get here?
       </h1>
-      {attempts >= 5 && (
+      {attempts >= 3 && (
         <div className="fade-in absolute left-1/2 top-[60%] z-30 -translate-x-1/2 -translate-y-1/2 transform cursor-pointer">
           <Button label="Easy home button" link="/" />
         </div>
