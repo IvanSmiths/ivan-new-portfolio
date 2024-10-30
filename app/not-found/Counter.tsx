8323c5890e7e3fe9069fd90cf8b2801383219386
IvanSmiths <info@ivansmiths.com>
2024-10-30 11:33:28 +0100

    mod style (#214)

--- app/not-found/Counter.tsx
+++ app/not-found/Counter.tsx
@@ -5,7 +5,7 @@ import { useButtonStore } from "../../utils/store";
 const Counter = () => {
   const { attempts, resetAll } = useButtonStore();
   return (
-    <div className="absolute right-4 top-4 z-30 rounded-lg px-4 py-2">
+    <div className="absolute right-small top-small z-30 px-4 py-2">
       Attempts: {attempts}
       <button className="ml-2 rounded-lg px-4 py-2" onClick={resetAll}>
         Try again
