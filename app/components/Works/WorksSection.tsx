7d7f4020ab1b5710130e221917b5ebff5c68a3ee
IvanSmiths <info@ivansmiths.com>
2024-10-31 23:07:34 +0100

    add barebone pin (#214)

--- app/components/Works/WorksSection.tsx
+++ app/components/Works/WorksSection.tsx
@@ -1,103 +1,52 @@
 "use client";
 
-import { FC, Key, useRef } from "react";
+import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
-import { Works } from "../../../utils/graphql";
 import Link from "next/link";
+import { FC, useRef } from "react";
+import { Works } from "../../../utils/graphql";
 
 type WorkProps = {
   works: Works[];
 };
 
 const WorksSection: FC<WorkProps> = ({ works }) => {
-  const perspectiveRef = useRef<HTMLDivElement>(null);
   const triggerRef = useRef<HTMLDivElement>(null);
 
   gsap.registerPlugin(ScrollTrigger);
 
   useGSAP(
     (): void => {
-      gsap
-        .timeline({
-          defaults: {
-            ease: "none",
-          },
-          scrollTrigger: {
-            trigger: triggerRef.current,
-            start: "top top",
-            end: "2000 bottom",
-            scrub: 0.6,
-            pin: true,
-            snap: 1 / works.length,
-          },
-        })
-        .to(perspectiveRef.current, {
-          transform: "translate3d(0px, 0px, 35rem)",
-        })
-        .set(
-          "#clsj5j1c7siwu0alfg7gm8kjk",
-          {
-            opacity: 0,
-          },
-          0.25,
-        )
-        .set(
-          "#clsj5jsg5sh530ak4qcqekxm4",
-          {
-            opacity: 0,
-          },
-          0.4,
-        );
+      let panels = gsap.utils.toArray<HTMLElement>(".panel");
+
+      panels.forEach((panel, i) => {
+        ScrollTrigger.create({
+          trigger: panel,
+          start: "top top",
+          pin: i === panels.length - 1 ? false : true,
+          end: "bottom 100",
+          pinSpacing: false,
+        });
+      });
     },
-    { scope: perspectiveRef },
+    { scope: triggerRef },
   );
 
   return (
     <div className="my-section">
-      <div
-        ref={triggerRef}
-        style={{ perspective: "25rem" }}
-        className="perspective relative h-screen w-full overflow-hidden"
-      >
-        <h2 className="absolute left-1/2 top-small block origin-center -translate-x-1/2 -translate-y-1/2 text-xl font-bold">
-          All My Works
-        </h2>
-        <div
-          ref={perspectiveRef}
-          className="relative h-full"
-          style={{ transformStyle: "preserve-3d" }}
-        >
-          {works.map((work: Works, index: Key) => (
-            <div
-              id={work.id}
-              className={work.class}
-              key={index}
-              style={{
-                transform:
-                  index === 1
-                    ? "translate3d(0px, 0px, -10rem) translateY(-50%)"
-                    : index === 2
-                      ? "translate3d(0px, 0px, -20rem) translateY(-50%)"
-                      : "translateY(-50%)",
-              }}
-            >
-              <Link href={`works/${work.slug}`}>
-                <h2 className="absolute bottom-small right-small rounded-sm bg-dark px-4 py-2 text-xs font-semibold text-white transition hover:bg-light hover:text-dark">
-                  {work.company}
-                </h2>
-                <img
-                  className="inline w-full rounded-md"
-                  src={work.homeImage.url}
-                  width={work.homeImage.width}
-                  height={work.homeImage.height}
-                  alt={work.company}
-                />
-              </Link>
-            </div>
-          ))}
-        </div>
+      <div ref={triggerRef} className="relative flex h-full w-full flex-col">
+        {works.map((work: Works) => (
+          <div
+            id={work.id}
+            className="panel relative flex h-screen w-full items-center justify-center bg-white dark:bg-dark"
+            key={work.id}
+          >
+            <Link href={`works/${work.slug}`}>
+              <h3 className="text-center text-7xl font-bold">{work.company}</h3>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
