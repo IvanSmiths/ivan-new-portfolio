eb149365560ea3a2b8f3eb55aaea9217c38a8ec2
IvanSmiths <info@ivansmiths.com>
2024-11-01 17:37:09 +0100

    add company name and logo (#214)

--- app/works/components/Work.tsx
+++ app/works/components/Work.tsx
@@ -19,10 +19,17 @@ const Work: FC<WorkProps> = ({ work, index }) => {
         id="description"
         className="relative z-20 h-full w-full md:h-5/6 md:w-7/12"
       >
+        <img
+          className="absolute left-small top-small z-20 h-fit w-20 object-cover"
+          src={work.homeLogo.url}
+          alt={work.company}
+          width={work.homeLogo.width}
+          height={work.homeLogo.height}
+        />
         <div className="absolute inset-0 z-10 rounded-xl bg-black/50"></div>
         <div className="absolute bottom-small left-small z-20 pr-small text-light">
-          <h4 className="pb-smallest text-xl font-bold">{work.role}</h4>
-          <p className="text-lg">{work.homeDescription}</p>
+          <h2 className="pb-smallest text-3xl font-bold">{work.role}</h2>
+          <h3 className="text-xl">{work.company}</h3>
         </div>
         <img
           className="relative h-full w-full rounded-xl object-cover"
