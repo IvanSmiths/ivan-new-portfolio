ef89e00b002b7ba88afe354a506d13a4515a03b3
IvanSmiths <info@ivansmiths.com>
2024-11-01 15:14:14 +0100

    add text into panel  (#214)

--- app/components/Works/WorksSection.tsx
+++ app/components/Works/WorksSection.tsx
@@ -36,18 +36,13 @@ const WorksSection: FC<WorkProps> = ({ works }) => {
       ease: "power4.out",
     });
 
-    tl.set(img, {
-      zIndex: 200,
-    });
-
     tl.to(img, {
       position: "fixed",
       top: "50%",
       left: "50%",
       xPercent: -50,
       yPercent: -50,
-      zIndex: 50,
-      duration: 2,
+      duration: 200000,
       ease: "power2.inOut",
     });
   };
@@ -65,21 +60,13 @@ const WorksSection: FC<WorkProps> = ({ works }) => {
           pinSpacing: false,
           onEnter: () => {
             const tl = gsap.timeline();
-            tl.to(panel.querySelector("img"), {
-              opacity: 1,
-              duration: 0.5,
-            });
-            tl.to(panel.querySelector(".description"), {
+            tl.to(panel.querySelector("#description"), {
               opacity: 1,
               duration: 0.5,
             });
           },
           onLeaveBack: () => {
-            gsap.to(panel.querySelector("img"), {
-              opacity: 0,
-              duration: 0.5,
-            });
-            gsap.to(panel.querySelector(".description"), {
+            gsap.to(panel.querySelector("#description"), {
               opacity: 0,
               duration: 0.5,
             });
@@ -91,37 +78,38 @@ const WorksSection: FC<WorkProps> = ({ works }) => {
   );
 
   return (
-    <div className="my-section">
-      <div ref={triggerRef} className="relative flex h-full w-full flex-col">
-        {works.map((work: Works) => (
-          <Link
-            key={work.id}
-            className="panel relative grid h-screen w-full items-center justify-center bg-white dark:bg-dark"
-            href={`works/${work.slug}`}
-            onClick={handlePanelClick}
+    <div ref={triggerRef} className="my-section h-fit w-full">
+      <div className="overlay fixed left-0 top-0 z-10 h-0 w-full bg-light dark:bg-dark"></div>
+      {works.map((work: Works) => (
+        <Link
+          key={work.id}
+          className="panel relative grid h-screen w-full items-center justify-center bg-light dark:bg-dark"
+          href={`works/${work.slug}`}
+        >
+          <h3
+            className={`${bebas_neue.className} absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-[19rem] font-bold leading-[15rem]`}
           >
-            <h3
-              className={`${bebas_neue.className} absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-[19rem] font-bold leading-[15rem]`}
-            >
-              {work.company}
-            </h3>
-            <div className="z-20 col-start-5 col-end-9">
-              <img
-                className="relative z-30 h-[40rem] object-cover opacity-0"
-                src={work.homeImage.url}
-                alt={work.company}
-                width={work.homeImage.width}
-                height={work.homeImage.height}
-              />
-              <div className="description col-start-6 col-end-12 opacity-0">
-                <h4 className="text-lg">{work.role}</h4>
-                <p className="text-lg">{work.homeDescription}</p>
-              </div>
+            {work.company}
+          </h3>
+          <div
+            id="description"
+            className="relative z-20 col-start-5 col-end-9 opacity-0"
+          >
+            <div className="absolute inset-0 z-10 bg-black/50"></div>
+            <div className="absolute bottom-small left-small z-20 pr-small text-light">
+              <h4 className="pb-smallest text-xl font-bold">{work.role}</h4>
+              <p className="text-lg">{work.homeDescription}</p>
             </div>
-          </Link>
-        ))}
-      </div>
-      <div className="overlay fixed left-0 top-0 z-10 h-0 w-full bg-red-500"></div>
+            <img
+              className="relative h-[40rem] object-cover"
+              src={work.homeImage.url}
+              alt={work.company}
+              width={work.homeImage.width}
+              height={work.homeImage.height}
+            />
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
