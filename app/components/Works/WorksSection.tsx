3206eebcc10fc1421600d865293a0c2a8ec03f91
IvanSmiths <info@ivansmiths.com>
2024-11-01 11:55:15 +0100

    add image animation (#214)

--- app/components/Works/WorksSection.tsx
+++ app/components/Works/WorksSection.tsx
@@ -33,9 +33,17 @@ const WorksSection: FC<WorkProps> = ({ works }) => {
               opacity: 1,
               duration: 0.5,
             });
+            gsap.to(panel.querySelector(".description"), {
+              opacity: 1,
+              duration: 0.5,
+            });
           },
           onLeaveBack: () => {
             gsap.to(panel.querySelector("img"), {
+              opacity: 0,
+              duration: 0.5,
+            });
+            gsap.to(panel.querySelector(".description"), {
               opacity: 0,
               duration: 0.5,
             });
@@ -50,26 +58,30 @@ const WorksSection: FC<WorkProps> = ({ works }) => {
     <div className="my-section">
       <div ref={triggerRef} className="relative flex h-full w-full flex-col">
         {works.map((work: Works) => (
-          <div
-            id={work.id}
-            className="panel relative grid h-screen  w-full items-center justify-center bg-white dark:bg-dark"
+          <Link
             key={work.id}
+            className="panel relative grid h-screen  w-full items-center justify-center bg-white dark:bg-dark"
+            href={`works/${work.slug}`}
           >
-            <Link href={`works/${work.slug}`}>
-              <h3
-                className={`${bebas_neue.className} absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-[14rem] font-bold leading-[12rem]`}
-              >
-                {work.company}
-              </h3>
-            </Link>
-            <img
-              className="col-start-5 col-end-9 opacity-0"
-              src={work.homeImage.url}
-              alt={work.company}
-              width={work.homeImage.width}
-              height={work.homeImage.height}
-            />
-          </div>
+            <h3
+              className={`${bebas_neue.className} absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-[19rem] font-bold leading-[15rem]`}
+            >
+              {work.company}
+            </h3>
+            <div className="z-20 col-start-5 col-end-9">
+              <img
+                className="h-[40rem] object-cover opacity-0"
+                src={work.homeImage.url}
+                alt={work.company}
+                width={work.homeImage.width}
+                height={work.homeImage.height}
+              />
+              <div className="description col-start-6 col-end-12 opacity-0">
+                <h4 className="text-lg">{work.role}</h4>
+                <p className="text-lg">{work.homeDescription}</p>
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
