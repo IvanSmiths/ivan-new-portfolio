66154d858f1b8c4f8056850d5a75d339fffa1bc7
IvanSmiths <info@ivansmiths.com>
2024-11-01 16:01:25 +0100

    add separate component  (#214)

---
+++ app/components/Works/SingleWork.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { forwardRef } from "react";
+import { bebas_neue } from "../../../utils/fonts";
+import { Works } from "../../../utils/graphql";
+
+type WorkPanelProps = {
+  work: Works;
+};
+
+const SingleWork = forwardRef<HTMLAnchorElement, WorkPanelProps>(
+  ({ work }, ref) => {
+    return (
+      <Link
+        ref={ref}
+        className="relative grid h-screen w-full items-center justify-center bg-light dark:bg-dark"
+        href={`works/${work.slug}`}
+      >
+        <h3
+          className={`${bebas_neue.className} absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-9xl font-bold md:text-[19rem] md:leading-[15rem]`}
+        >
+          {work.company}
+        </h3>
+        <div
+          id="description"
+          className="relative z-20 col-span-full opacity-0 md:col-start-5 md:col-end-9"
+        >
+          <div className="absolute inset-0 z-10 bg-black/50"></div>
+          <div className="absolute bottom-small left-small z-20 pr-small text-light">
+            <h4 className="pb-smallest text-xl font-bold">{work.role}</h4>
+            <p className="text-lg">{work.homeDescription}</p>
+          </div>
+          <img
+            className="relative h-[40rem] object-cover"
+            src={work.homeImage.url}
+            alt={work.company}
+            width={work.homeImage.width}
+            height={work.homeImage.height}
+          />
+        </div>
+      </Link>
+    );
+  },
+);
+
+SingleWork.displayName = "WorkPanel";
+
+export default SingleWork;
--- app/components/Works/WorksSection.tsx
+++ app/components/Works/WorksSection.tsx
@@ -3,10 +3,9 @@
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import Link from "next/link";
 import { createRef, FC, RefObject, useRef } from "react";
-import { bebas_neue } from "../../../utils/fonts";
 import { Works } from "../../../utils/graphql";
+import SingleWork from "./SingleWork";
 
 type WorkProps = {
   works: Works[];
@@ -52,35 +51,7 @@ const WorksSection: FC<WorkProps> = ({ works }) => {
   return (
     <div ref={scopeRef} className="my-section h-fit w-full">
       {works.map((work: Works, index: number) => (
-        <Link
-          ref={panelRefs.current[index]}
-          key={work.id}
-          className="panel relative grid h-screen w-full items-center justify-center bg-light dark:bg-dark"
-          href={`works/${work.slug}`}
-        >
-          <h3
-            className={`${bebas_neue.className} absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 text-center text-9xl font-bold md:text-[19rem] md:leading-[15rem]`}
-          >
-            {work.company}
-          </h3>
-          <div
-            id="description"
-            className="relative z-20 col-span-full opacity-0 md:col-start-5 md:col-end-9"
-          >
-            <div className="absolute inset-0 z-10 bg-black/50"></div>
-            <div className="absolute bottom-small left-small z-20 pr-small text-light">
-              <h4 className="pb-smallest text-xl font-bold">{work.role}</h4>
-              <p className="text-lg">{work.homeDescription}</p>
-            </div>
-            <img
-              className="relative h-[40rem] object-cover"
-              src={work.homeImage.url}
-              alt={work.company}
-              width={work.homeImage.width}
-              height={work.homeImage.height}
-            />
-          </div>
-        </Link>
+        <SingleWork key={work.id} work={work} ref={panelRefs.current[index]} />
       ))}
     </div>
   );
