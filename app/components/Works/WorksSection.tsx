0af74201ffa2e769724c36c978ae4f1854d5d023
IvanSmiths <info@ivansmiths.com>
2024-11-05 19:57:52 +0100

    add proper width (#214)

--- app/components/Works/WorksSection.tsx
+++ app/components/Works/WorksSection.tsx
@@ -10,18 +10,16 @@ const WorksSection: FC<WorksSectionProps> = ({ works }) => {
   return (
     <section className="relative flex h-full flex-col items-center justify-center px-small">
       <WorkText />
-      <div className="flex h-full w-full flex-col flex-wrap gap-small">
+      <div className="flex h-full w-full flex-wrap gap-small">
         {works.map((work) => (
-          <div className="relative h-[600px] w-full" key={work.id}>
-            <div className="absolute left-0 top-0">
-              <img
-                className="h-full w-full object-cover"
-                src={work.homeImage.url}
-                height={work.homeImage.height}
-                width={work.homeImage.width}
-                alt={work.company}
-              />
-            </div>
+          <div className="w-[calc(50%-10px)]" key={work.id}>
+            <img
+              className="h-full w-full object-cover"
+              src={work.homeImage.url}
+              height={work.homeImage.height}
+              width={work.homeImage.width}
+              alt={work.company}
+            />
           </div>
         ))}
       </div>
