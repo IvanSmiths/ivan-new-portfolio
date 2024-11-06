8468677079a79f24c88e05a223ebf0e384db74bb
IvanSmiths <info@ivansmiths.com>
2024-11-06 13:28:59 +0100

    add return (#214)

--- utils/Schemas.tsx
+++ utils/Schemas.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "./getPosts";
+import { description, keywords } from "./metadata/homeMetadata";
 
 type WorkProps = {
   slug: string;
@@ -7,45 +8,49 @@ type WorkProps = {
 
 type BlogSchema = Metadata & { slug?: string };
 
-export const homeSchema = {
-  "@context": "http://schema.org",
-  "@type": "WebSite",
-  name: "IvanSmiths",
-  url: "https://www.ivansmiths.com",
-  image: "https://www.ivansmiths.com/home.png",
-  description: "Frontend developer with 3 years of experience",
-  sameAs: "https://www.ivansmiths.com",
-  author: {
-    "@type": "Person",
-    name: "Ivan",
-    familyName: "Smiths",
+export const homeSchema = () => {
+  return {
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    name: "IvanSmiths",
     url: "https://www.ivansmiths.com",
-  },
-  inLanguage: "en",
-  copyrightYear: 2020,
-  genre: "http://vocab.getty.edu/aat/300179434",
-  headline: "Seeking the limit.",
-  keywords: "next.js, ui/ux developer, wiesbaden, react.js, frontend developer",
-  locationCreated: "Wiesbaden",
+    image: "https://www.ivansmiths.com/home.png",
+    description,
+    sameAs: "https://www.ivansmiths.com",
+    author: {
+      "@type": "Person",
+      name: "Ivan",
+      familyName: "Smiths",
+      url: "https://www.ivansmiths.com",
+    },
+    inLanguage: "en",
+    copyrightYear: 2020,
+    genre: "http://vocab.getty.edu/aat/300179434",
+    headline: "Fullstack developer specialized in design and user experience.",
+    keywords,
+    locationCreated: "Wiesbaden",
+  };
 };
 
-export const worksSchema = {
-  "@context": "http://schema.org",
-  "@type": "BreadcrumbList",
-  itemListElement: [
-    {
-      "@type": "ListItem",
-      position: 1,
-      name: "Ivan Smiths, Frontend UI/UX Developer from Wiesbaden",
-      item: "https://ivansmiths.com",
-    },
-    {
-      "@type": "ListItem",
-      position: 2,
-      name: "Ivan Smiths, all the works",
-      item: "https://ivansmiths.com/works",
-    },
-  ],
+export const worksSchema = (works: WorkProps) => {
+  return {
+    "@context": "http://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Ivan Smiths, Frontend UI/UX Developer from Wiesbaden",
+        item: "https://ivansmiths.com",
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "Ivan Smiths, all the works",
+        item: "https://ivansmiths.com/works",
+      },
+    ],
+  };
 };
 
 export const workSchema = (works: WorkProps) => {
