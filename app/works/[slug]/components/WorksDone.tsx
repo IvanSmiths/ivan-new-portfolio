57afbf3defb060f00449cf212f0465db15ee40a6
IvanSmiths <info@ivansmiths.com>
2024-11-06 16:35:23 +0100

    add worksDone component (#214)

---
+++ app/works/[slug]/components/WorksDone.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const worksDoneList = [
+  {
+    label: "Adidas",
+    link: "www.exampe.com",
+  },
+];
+
+const WorksDone: FC = () => {
+  return <div>WorksDone</div>;
+};
+
+export default WorksDone;
