0607762916ed9c46b099b86c5446392041804c9f
IvanSmiths <info@ivansmiths.com>
2024-11-06 17:52:12 +0100

    add works list animation (#214)

--- app/works/[slug]/components/WorksDone.tsx
+++ app/works/[slug]/components/WorksDone.tsx
@@ -18,6 +18,18 @@ const worksDoneList = [
     label: "Adidas",
     link: "www.exampe.com",
   },
+  {
+    label: "Adidas",
+    link: "www.exampe.com",
+  },
+  {
+    label: "Adidas",
+    link: "www.exampe.com",
+  },
+  {
+    label: "Adidas",
+    link: "www.exampe.com",
+  },
 ];
 
 gsap.registerPlugin(ScrollTrigger);
@@ -42,7 +54,6 @@ const WorksDone: FC = () => {
       timeline.to(worksRef.current, {
         left: "0",
         transformOrigin: "left",
-        duration: 1,
         scale: 0.3,
         ease: "power2.inOut",
       });
@@ -52,7 +63,6 @@ const WorksDone: FC = () => {
           right: "0",
           transformOrigin: "right",
           scale: 0.3,
-          duration: 1,
           ease: "power2.inOut",
         },
         "<",
@@ -70,7 +80,7 @@ const WorksDone: FC = () => {
         ref={containerRef}
         className="flex h-screen w-full items-center justify-between"
       >
-        <div className="relative h-5 w-1/2 bg-slate-500">
+        <div className="relative h-5 w-1/2">
           <span
             ref={worksRef}
             className="absolute right-0 top-1/2 origin-right -translate-y-1/2 text-9xl"
@@ -78,7 +88,7 @@ const WorksDone: FC = () => {
             Works
           </span>
         </div>
-        <div className="relative h-5 w-1/2 bg-slate-500">
+        <div className="relative h-5 w-1/2">
           <span
             ref={doneRef}
             className="absolute left-0 top-1/2 origin-left -translate-y-1/2 text-right text-9xl"
@@ -87,7 +97,7 @@ const WorksDone: FC = () => {
           </span>
         </div>
       </div>
-      <div className="flex w-full flex-col flex-wrap items-center justify-center gap-small px-small">
+      <div className="flex w-full flex-col flex-wrap items-center justify-center gap-small px-small pb-[40rem] pt-[20rem]">
         {worksDoneList.map((work) => (
           <div key={work.label} className="text-8xl">
             {work.label}
