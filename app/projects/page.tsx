819d95410b9eea3bd76eeeec251a4dc34a0c5512
IvanSmiths <info@ivansmiths.com>
2024-12-01 16:48:35 +0100

    test fix build (#223)

---
+++ app/projects/page.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import { projectSchema } from "../../utils/metadata/Schemas";
+import Header from "../crafts/components/Header";
+import { worksHeaderProps } from "../crafts/components/headerProps";
+import Footer from "../globalComponents/Footer/Footer";
+import Navbar, { Position } from "../globalComponents/Navbar/Navbar";
+import ProjectsSection from "./components/ProjectsSection";
+import { Projects } from "../../utils/graphql/graphqlTypes";
+import { getProjects } from "../../utils/graphql";
+
+const ProjectPage: FC = async () => {
+  const projects: Projects[] = await getProjects();
+
+  return (
+    <>
+      <Header
+        h1={worksHeaderProps.h1}
+        h2={worksHeaderProps.h2}
+        paragraph={worksHeaderProps.paragraph}
+      />
+      <Navbar position={Position.Fixed} />
+      <ProjectsSection projects={projects} />
+      <Footer />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(projectSchema) }}
+      />
+    </>
+  );
+};
+
+export default ProjectPage;
