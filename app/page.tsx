0237acc5867e577baa458dcb98ec658e11060acd
IvanSmiths <info@ivansmiths.com>
2024-12-01 19:25:02 +0100

    rename directory to lowercase (#223)

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ import { Works } from "../utils/graphql/graphqlTypes";
 import { homeMetadata } from "../utils/metadata/homeMetadata";
 import { homeSchema } from "../utils/metadata/Schemas";
 import Footer from "./components/global/Footer/Footer";
+import Hero from "./components/home/Hero/Hero";
+import Overlay from "./components/home/Loader/Overlay";
+import LoaderWrapper from "./components/home/Loader/LoaderWrapper";
+import About from "./components/home/About/About";
+import WorksSection from "./components/home/Works/WorksSection";
+import Expertise from "./components/home/Expertise/Expertise";
 
 export const metadata: Metadata = homeMetadata;
 
@@ -12,7 +18,12 @@ const Home = async () => {
 
   return (
     <>
-      hi
+      <Hero />
+      <Overlay />
+      <LoaderWrapper />
+      <About />
+      <WorksSection works={works} />
+      <Expertise />
       <Footer />
       <script
         type="application/ld+json"
