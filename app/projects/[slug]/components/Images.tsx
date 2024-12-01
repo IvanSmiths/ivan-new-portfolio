a3cefc3cff5c91777a42daa76fe37e205349a2c5
IvanSmiths <info@ivansmiths.com>
2024-12-01 14:38:58 +0100

    add projects page (#223)

--- app/projects/[slug]/components/Images.tsx
+++ app/projects/[slug]/components/Images.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 const Images: FC<HeaderProps> = ({ project }) => {
   if (project.images) {
     return (
-      <div className="mb-medium mt-medium grid md:mb-0">
+      <div className="mb-medium mt-section grid md:mb-0">
         <div className="col-span-full w-full md:col-start-2 md:col-end-12">
           <ul className="flex flex-col gap-small">
             <RichText
