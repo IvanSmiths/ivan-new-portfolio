d714cde5695232f174bb0e4e4a2725b10804685c
IvanSmiths <info@ivansmiths.com>
2024-12-01 16:56:07 +0100

    test fix build (#223)

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,24 +1,17 @@
 import { FC } from "react";
-import Header from "../crafts/components/Header";
-import { worksHeaderProps } from "../crafts/components/headerProps";
 import Footer from "../globalComponents/Footer/Footer";
 import Navbar, { Position } from "../globalComponents/Navbar/Navbar";
-import ProjectsSection from "./components/ProjectsSection";
 import { Projects } from "../../utils/graphql/graphqlTypes";
 import { getProjects } from "../../utils/graphql";
 
 const ProjectPage: FC = async () => {
   const projects: Projects[] = await getProjects();
 
+  console.log(projects);
+
   return (
     <>
-      <Header
-        h1={worksHeaderProps.h1}
-        h2={worksHeaderProps.h2}
-        paragraph={worksHeaderProps.paragraph}
-      />
       <Navbar position={Position.Fixed} />
-      <ProjectsSection projects={projects} />
       <Footer />
     </>
   );
