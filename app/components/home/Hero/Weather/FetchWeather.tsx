215c6ce1afb666b3f6e66ea5bbc8c10422015eb1
IvanSmiths <info@ivansmiths.com>
2025-02-05 20:39:44 +0100

    add response type (#224)

--- app/components/home/Hero/Weather/FetchWeather.tsx
+++ app/components/home/Hero/Weather/FetchWeather.tsx
@@ -10,7 +10,7 @@ type WeatherData = {
 };
 
 async function getData() {
-  const res = await fetch(
+  const res: Response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=Wiesbaden&appid=${process.env.OPEN_WEATHER_API}&units=metric`,
     {
       method: "GET",
