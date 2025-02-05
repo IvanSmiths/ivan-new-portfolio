4f005bbc373d82c092352f2a3edeabacf9c74f6d
IvanSmiths <info@ivansmiths.com>
2025-02-05 22:42:35 +0100

    add try catch (#224)

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,25 +8,43 @@ import { ProjectBase } from "../../utils/fetch/graphql/graphqlTypes";
 import type { Metadata } from "next";
 import { projectsMetadata } from "../../utils/metadata/projectsMetadata";
 import Footer from "../components/global/Footer/Footer";
+import { FC, ReactNode } from "react";
 
 export const metadata: Metadata = projectsMetadata;
 
-export default async function Page() {
-  const projects: ProjectBase[] = await getProjects();
-  return (
-    <>
-      <Header
-        h1={projectsHeaderProps.h1}
-        h2={projectsHeaderProps.h2}
-        paragraph={projectsHeaderProps.paragraph}
-      />
-      <Navbar position={Position.Fixed} />
-      <ProjectsSection projects={projects} />
-      <Footer />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(projectsSchema) }}
-      />
-    </>
-  );
-}
+type ProjectsLayoutProps = {
+  children?: ReactNode;
+};
+
+const ProjectsLayout: FC<ProjectsLayoutProps> = ({ children }) => (
+  <>
+    <Header
+      h1={projectsHeaderProps.h1}
+      h2={projectsHeaderProps.h2}
+      paragraph={projectsHeaderProps.paragraph}
+    />
+    <Navbar position={Position.Fixed} />
+    {children}
+    <Footer />
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(projectsSchema) }}
+    />
+  </>
+);
+
+const ProjectsPage: FC = async () => {
+  try {
+    const projects: ProjectBase[] = await getProjects();
+    return (
+      <ProjectsLayout>
+        {projects && <ProjectsSection projects={projects} />}
+      </ProjectsLayout>
+    );
+  } catch (error) {
+    console.error("Failed to fetch projects:", error);
+    return <ProjectsLayout />;
+  }
+};
+
+export default ProjectsPage;
