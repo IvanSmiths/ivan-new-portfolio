58b9f219e32b153d0bd4bc725f57143a5e8e0886
IvanSmiths <info@ivansmiths.com>
2025-05-19 18:52:52 +0200

    add neugelb

--- utils/pages/works/index.ts
+++ utils/pages/works/index.ts
@@ -1,7 +1,8 @@
 import { td } from "./td";
 import { suv } from "./suv";
 import { ideology } from "./ideology";
+import { neugelb } from "./neugelb";
 
-const worksData = [td, suv, ideology];
+const worksData = [neugelb, td, suv, ideology];
 
 export default worksData;
---
+++ utils/pages/works/neugelb.ts
@@ -0,0 +1,35 @@
+import { WorkProjectPage } from "../types";
+
+export const neugelb: WorkProjectPage = {
+  type: "Work",
+  slug: "neugelb",
+  name: "NeuGelb",
+  date: "March 2023 - September 2024",
+  shortDescription: "hsbshbshsbbsh",
+  description: "sbsjhsbs",
+  role: "Fullstack Developer",
+  linkedinLink: "https://www.linkedin.com/company/neugelb",
+  websiteLink: "https://www.neugelb.com/",
+  worksDone: [{ label: "Commerzbank", link: "https://www.td.com/" }],
+  stack: [
+    "TypeScript",
+    "Angular",
+    "RxJs",
+    "Cypress",
+    "SQL",
+    "Nest.js",
+    "Figma",
+    "Docker",
+  ],
+  title: "Ivan Smiths - Achievements at TD Cowen",
+  metaDescription:
+    "At TD Cowen, I implemented advanced CRM platform features for a top global bank, focusing on data-driven enhancements to improve user engagement and functionality. I ensured platform robustness through comprehensive Cypress testing and redesigned the mobile app with an emphasis on intuitive interfaces, resulting in a more seamless user experience.",
+  homeImage: {
+    url: "https://res.cloudinary.com/deino2cjx/image/upload/v1747345769/portfolio/td/td-cover_zsqijq.png",
+    height: 606,
+    width: 809,
+  },
+  images: [
+    "https://res.cloudinary.com/deino2cjx/image/upload/v1747341943/portfolio/td/td-website-1_kf37zf.jpg",
+  ],
+};
