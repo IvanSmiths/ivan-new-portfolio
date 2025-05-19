06dea537611d579bd43964026cd50c6d8e995f93
IvanSmiths <info@ivansmiths.com>
2025-05-20 01:02:04 +0200

    add images

--- components/home/About/About.tsx
+++ components/home/About/About.tsx
@@ -3,10 +3,33 @@
 const About = () => {
   return (
     <div className="py-3xl flex w-full items-center justify-center">
-      <div className="flex w-9/12 items-center justify-center">
-        <h2 className="text-center text-9xl font-black uppercase">
-          FROM TOP TIER GLOBAL COMPANIES TO LOCAL REALITIES, I GOT YOUR BACK
-          WITH DESIGN AND CODE AT MAXIMUM LEVEL.
+      <div className="flex w-8/12 items-center justify-center">
+        <h2 className="leading-4xl text-center text-8xl font-black uppercase">
+          FROM{" "}
+          <img
+            className="inline h-24 pb-4"
+            src="https://res.cloudinary.com/deino2cjx/image/upload/v1747341876/portfolio/ideology/ideology-website-6_cfwfw2.jpg"
+            alt=""
+          />{" "}
+          TOP TIER GLOBAL COMPANIES{" "}
+          <img
+            className="inline h-24 pb-4"
+            src="https://res.cloudinary.com/deino2cjx/image/upload/v1747341878/portfolio/ideology/ideology-website-9_jdwyyf.jpg"
+            alt=""
+          />{" "}
+          TO LOCAL REALITIES, I GOT{" "}
+          <img
+            className="inline h-24 pb-4"
+            src="https://res.cloudinary.com/deino2cjx/image/upload/v1747341883/portfolio/ideology/racina_kpctx3.jpg"
+            alt=""
+          />{" "}
+          YOUR BACK WITH DESIGN{" "}
+          <img
+            className="inline h-24 pb-4"
+            src="https://res.cloudinary.com/deino2cjx/image/upload/v1747341881/portfolio/ideology/lemon-soda-2_kuj5ha.jpg"
+            alt=""
+          />{" "}
+          AND CODE AT MAXIMUM LEVEL.
         </h2>
       </div>
     </div>
