0e624b6230d898e1676a708f581b17e9b1897248
IvanSmiths <info@ivansmiths.com>
2025-05-20 11:29:20 +0200

    remove scrollTrigger

--- utils/hooks/useScrollPercentage.tsx
+++ utils/hooks/useScrollPercentage.tsx
@@ -1,39 +1,46 @@
 import { useEffect, useRef } from "react";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
 import { usePathname } from "next/navigation";
-
-gsap.registerPlugin(ScrollTrigger);
 
 export function useScrollPercentage() {
   const percentRef = useRef<HTMLSpanElement>(null);
   const pathname = usePathname();
 
-  useGSAP(() => {
-    gsap.set(percentRef.current, { innerText: 0 });
-    const trigger = ScrollTrigger.create({
-      start: 0,
-      end: "max",
-      onUpdate: (self) => {
-        const progress = Math.round(self.progress * 100);
-        if (percentRef.current) {
-          gsap.to(percentRef.current, {
-            innerText: progress,
-            duration: 0.2,
-            ease: "power1.out",
-            snap: "innerText",
-          });
-        }
-      },
-    });
+  useEffect(() => {
+    const calculateScrollPercentage = () => {
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrollTop = window.scrollY;
+      const maxScroll = documentHeight - windowHeight;
 
-    return () => trigger.kill();
+      // disable if the page is not scrollable
+      if (maxScroll <= 5) return 0;
+      if (scrollTop + windowHeight >= documentHeight - 5) return 100;
+      return Math.max(
+        0,
+        Math.min(100, Math.round((scrollTop / maxScroll) * 100)),
+      );
+    };
+
+    const updatePercentage = () => {
+      if (percentRef.current) {
+        percentRef.current.innerText = calculateScrollPercentage().toString();
+      }
+    };
+    updatePercentage();
+    window.addEventListener("scroll", updatePercentage, { passive: true });
+    window.addEventListener("resize", updatePercentage, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", updatePercentage);
+      window.removeEventListener("resize", updatePercentage);
+    };
   }, []);
 
-  // call ScrollTrigger.refresh to refresh the end/start position when route changes
   useEffect(() => {
-    ScrollTrigger.refresh();
+    const timeout = setTimeout(() => {
+      window.dispatchEvent(new Event("scroll"));
+    }, 300);
+
+    return () => clearTimeout(timeout);
   }, [pathname]);
 
   return { percentRef };
