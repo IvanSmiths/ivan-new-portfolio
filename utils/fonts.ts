fa9e8aede9507d1aeeadc2879e14664d93d9bd50
IvanSmiths <info@ivansmiths.com>
2025-05-23 17:50:49 +0200

    remove fonts

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,20 +1,5 @@
 import { NextFontWithVariable } from "next/dist/compiled/@next/font";
-import { Bebas_Neue, DM_Mono, DM_Sans, Lato } from "next/font/google";
-
-export const lato: NextFontWithVariable = Lato({
-  subsets: ["latin"],
-  weight: ["300", "400", "700"],
-  variable: "--font-lato",
-  display: "swap",
-});
-
-export const bebas_neue: NextFontWithVariable = Bebas_Neue({
-  subsets: ["latin"],
-  weight: ["400"],
-  preload: true,
-  variable: "--font-bebas_neue",
-  display: "swap",
-});
+import { DM_Mono, DM_Sans } from "next/font/google";
 
 export const dm_mono: NextFontWithVariable = DM_Mono({
   subsets: ["latin"],
