8482d2be8736c0bcc3884508d80e616de714812f
IvanSmiths <info@ivansmiths.com>
2025-05-23 09:46:30 +0200

    improve responsiveness

--- components/home/Hero/BottomHero/Showreel.tsx
+++ components/home/Hero/BottomHero/Showreel.tsx
@@ -1,19 +1,31 @@
+import { dm_mono } from "../../../../utils/fonts";
+
 const Showreel = () => {
   return (
-    <video className="w-36 object-cover" autoPlay loop muted>
-      <source
-        src="https://utfs.io/f/aCaScRJubtiP2mWMJSOEGVHWUKY8TpulMShIcw9X3Asb6rBn"
-        media="(min-width: 768px)"
-        type="video/mp4"
-      />
-      <track src="/videos/caption.vtt" kind="captions" />
-      <source
-        src="https://utfs.io/f/aCaScRJubtiPkBbnx8gxYRIj0MPlmQb12ZBuf38FwgGHAsL9"
-        media="(max-width: 767px)"
-        type="video/mp4"
-      />
-      Your browser does not support the video tag.
-    </video>
+    <a
+      href="https://youtu.be/osf7rWGB9qw"
+      className="group relative cursor-pointer"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      <div className="absolute inset-0 flex cursor-pointer items-center justify-center bg-black/50 opacity-0 transition group-hover:opacity-100">
+        <span className={`text-xs ${dm_mono} uppercase`}>Watch on Youtube</span>
+      </div>
+      <video className="h-40 w-fit object-cover" autoPlay loop muted>
+        <source
+          src="https://utfs.io/f/aCaScRJubtiP2mWMJSOEGVHWUKY8TpulMShIcw9X3Asb6rBn"
+          media="(min-width: 768px)"
+          type="video/mp4"
+        />
+        <track src="/videos/caption.vtt" kind="captions" />
+        <source
+          src="https://utfs.io/f/aCaScRJubtiPkBbnx8gxYRIj0MPlmQb12ZBuf38FwgGHAsL9"
+          media="(max-width: 767px)"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+    </a>
   );
 };
 
