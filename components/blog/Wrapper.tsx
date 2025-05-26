cb7fb6182ab1e64f7f130fc262efc06ce809c1d8
IvanSmiths <info@ivansmiths.com>
2025-05-26 16:27:01 +0200

    improve style

--- components/blog/Wrapper.tsx
+++ components/blog/Wrapper.tsx
@@ -5,9 +5,7 @@ const Wrapper = ({ children }: { children: ReactNode }) => {
   return (
     <article className="gap-lg pt-2xl flex w-full flex-col items-center">
       <div className="flex w-full pl-60">
-        <div className="gap-md p-md border-background-muted flex w-9/12 flex-col border border-r-0">
-          {children}
-        </div>
+        <div className="gap-md p-md flex w-9/12 flex-col">{children}</div>
         <TableOfContents />
       </div>
     </article>
