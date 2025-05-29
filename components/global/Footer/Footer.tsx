ab59201e27c0b899f184065c5fec87407b315b64
IvanSmiths <info@ivansmiths.com>
2025-05-29 18:50:58 +0200

    improve mobile

--- components/global/Footer/Footer.tsx
+++ components/global/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import FooterText from "./FooterText";
 
 const Footer = () => {
   return (
-    <footer className="px-sm pb-sm mt-xl md:mt-5xl flex flex-col justify-end gap-2 md:mt-0 md:min-h-[80vh]">
+    <footer className="px-sm pb-sm mt-xl md:mt-5xl flex flex-col justify-end gap-2 md:min-h-[80vh]">
       <Social />
       <FooterText />
       <Logo />
