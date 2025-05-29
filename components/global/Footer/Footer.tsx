5efe2ccd1c634efb67279c1850e89a312236ac64
IvanSmiths <info@ivansmiths.com>
2025-05-29 18:50:29 +0200

    improve mobile

--- components/global/Footer/Footer.tsx
+++ components/global/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import FooterText from "./FooterText";
 
 const Footer = () => {
   return (
-    <footer className="px-sm pb-sm md:mt-5xl flex min-h-[60vh] flex-col justify-end gap-2 md:min-h-[80vh]">
+    <footer className="px-sm pb-sm mt-xl md:mt-5xl flex flex-col justify-end gap-2 md:mt-0 md:min-h-[80vh]">
       <Social />
       <FooterText />
       <Logo />
--- components/home/About/AnimatedTextWithImages.tsx
+++ components/home/About/AnimatedTextWithImages.tsx
@@ -29,7 +29,7 @@ export const AnimatedTextWithImages = ({
   let imageIndex = 0;
 
   return (
-    <h2 className="lg:leading-4xl md:leading-2xl leading-xl flex flex-wrap justify-center gap-x-2 gap-y-4 text-center text-5xl font-black uppercase md:text-6xl lg:text-8xl">
+    <h2 className="lg:leading-4xl md:leading-2xl leading-lg flex flex-wrap justify-center gap-x-2 gap-y-4 text-center text-4xl font-black uppercase md:text-6xl lg:text-8xl">
       {words.map((word, index) => {
         if (word === " ") {
           const src = images[imageIndex++];
