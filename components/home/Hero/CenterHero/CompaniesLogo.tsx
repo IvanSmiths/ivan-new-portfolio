d2da836634815d2b1fe3f58f34a35b32f49ea75d
IvanSmiths <info@ivansmiths.com>
2025-06-04 16:10:11 +0200

    simplify animation

--- components/home/Hero/CenterHero/CompaniesLogo.tsx
+++ components/home/Hero/CenterHero/CompaniesLogo.tsx
@@ -1,40 +1,21 @@
-import { useRef } from "react";
 import CompanyLogo from "./CompanyLogo";
 import { logos } from "./logos";
-import { useFadeInOnLoad } from "../../../../utils/hooks/animations/useFadeInOnLoad";
 
 const CompaniesLogo = ({
   onHover,
 }: {
   onHover: (company: string | null) => void;
 }) => {
-  const logoRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const refsAsRefObjects = useRef(
-    logos.map((_, i) => ({
-      get current() {
-        return logoRefs.current[i] || null;
-      },
-    })),
-  );
-
-  const setRef = (index: number) => (el: HTMLDivElement | null) => {
-    logoRefs.current[index] = el;
-  };
-
-  useFadeInOnLoad({
-    refs: refsAsRefObjects.current,
-    options: {
-      duration: 0.5,
-      yOffset: 40,
-      blurAmount: 8,
-      stagger: 0.05,
-    },
-  });
-
   return (
     <div className="gap-md flex w-full flex-wrap justify-start">
       {logos.map(({ src, name, slug }, index) => (
-        <div key={index} className="flex w-20 lg:flex-1" ref={setRef(index)}>
+        <div
+          key={index}
+          className="animate-fadeInUp flex w-20 opacity-0 lg:flex-1"
+          style={{
+            animationDelay: `${index * 80}ms`,
+          }}
+        >
           <CompanyLogo
             src={`/logo/${src}`}
             alt={name}
