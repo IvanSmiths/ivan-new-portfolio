f0f4296d590bd9277fca071e8dd2b1b42a42f01e
Ivan <info@ivansmiths.com>
2026-02-10 13:55:20 +0100

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,27 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
   app: {
     head: {
-      title: "Ivan Smiths - Fullstack Developer Specialized in Design and User Experience",
+      title:
+        'Ivan Smiths - Fullstack Developer Specialized in Design and User Experience',
       htmlAttrs: {
-        lang: "en"
+        lang: 'en',
       },
-      link: [
-        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
-      ]
-    }
-  }
-});
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  css: ['./app/main.css'],
+  compatibilityDate: '2025-07-15',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
