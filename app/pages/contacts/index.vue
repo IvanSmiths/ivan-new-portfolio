1c1fe876061d866b614bc372c57ba311d1649ebf
Ivan <info@ivansmiths.com>
2026-02-18 21:03:33 +0100

    simplify component

--- app/pages/contacts/index.vue
+++ app/pages/contacts/index.vue
@@ -8,27 +8,14 @@ const socials = computed(() => appConfig.socials ?? []);
 <template>
   <main class="h-dvh w-screen grid place-items-center px-6 bg-background">
     <section class="flex flex-col text-sm text-foreground items-start gap-2.5">
-      <span class="tracking-tight hover:opacity-80 transition-opacity">
-        <a
-          :href="`mailto:${email}`"
-          class="focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-4"
-        >
+      <span class="hover:opacity-80 transition-opacity">
+        <a :href="`mailto:${email}`">
           {{ email }}
         </a>
       </span>
-
-      <span
-        v-for="s in socials"
-        :key="s.url"
-        class="tracking-tight hover:opacity-80 transition-opacity"
-      >
-        <a
-          :href="s.url"
-          class="focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-4"
-          rel="noreferrer"
-          target="_blank"
-        >
-          {{ s.label }}
+      <span v-for="social in socials" :key="social.url" class="hover:opacity-80 transition-opacity">
+        <a :href="social.url" rel="noreferrer" target="_blank">
+          {{ social.label }}
         </a>
       </span>
     </section>
