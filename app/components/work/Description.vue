705192c782fd63bda3cf81d37cde18fdc1198862
Ivan <info@ivansmiths.com>
2026-02-26 16:57:20 +0100

    improve spacing

--- app/components/work/Description.vue
+++ app/components/work/Description.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import type { WorkProjectPage } from "~/domain/works/types";
 import { useWorkSectionAnimation } from "~/composables/animations/work/useWorkSectionAnimation";
+import ExternalLink from "~/components/global/icons/ExternalLink.vue";
 
 const props = defineProps<{
   work: WorkProjectPage;
@@ -11,54 +12,56 @@ const { shortDescriptionRef } = useWorkSectionAnimation();
 <template>
   <section class="bg-background text-foreground w-full px-48 pb-20">
     <div class="flex w-full flex-row justify-between gap-3">
-      <span class="mb-2 block text-xs uppercase">TLdR;</span>
+      <span class="mb-5 block text-xs">TL;DR</span>
       <p ref="shortDescriptionRef" class="block pl-36 text-8xl font-black uppercase">
         {{ work?.shortDescription }}
       </p>
     </div>
-    <div class="flex w-full flex-col gap-3 py-20">
-      <span class="mb-2 block text-xs uppercase">The Project</span>
+    <div class="flex w-full flex-col py-20">
+      <span class="mb-5 block text-xs">The Project</span>
       <p class="text-2xl">
         {{ work?.description }}
       </p>
     </div>
     <div class="flex flex-row gap-24">
       <div v-if="work?.worksDone?.length">
-        <span class="mb-2 block text-xs uppercase">Clients</span>
+        <span class="mb-5 block text-xs">Clients</span>
         <ul class="space-y-1.5">
           <li v-for="(client, i) in work.worksDone" :key="i" class="text-sm md:text-base">
             <a
               :href="client.link"
-              class="text-xs font-bold uppercase transition-opacity duration-200 hover:opacity-90"
+              class="flex flex-row justify-start gap-1 text-xs font-bold uppercase transition-opacity duration-200 hover:opacity-90"
               rel="noopener noreferrer"
               target="_blank"
             >
-              {{ client.label }}
+              <span>{{ client.label }}</span>
+              <ExternalLink />
             </a>
           </li>
         </ul>
       </div>
       <div>
-        <span class="block text-xs uppercase">Links</span>
+        <span class="mb-5 block text-xs">Links</span>
         <ul class="space-y-1.5">
           <li>
             <a
               :href="work.websiteLink"
-              class="text-xs font-bold uppercase transition-opacity duration-200 hover:opacity-90"
+              class="flex flex-row justify-start gap-1 text-xs font-bold uppercase transition-opacity duration-200 hover:opacity-90"
               rel="noopener noreferrer"
               target="_blank"
             >
-              Website
+              <span>Website</span>
+              <ExternalLink />
             </a>
           </li>
           <li>
             <a
               :href="work.linkedinLink"
-              class="text-xs font-bold uppercase transition-opacity duration-200 hover:opacity-90"
+              class="flex flex-row items-center justify-center gap-1 text-xs font-bold uppercase transition-opacity duration-200 hover:opacity-90"
               rel="noopener noreferrer"
               target="_blank"
             >
-              Linkedin
+              Linkedin <ExternalLink />
             </a>
           </li>
         </ul>
