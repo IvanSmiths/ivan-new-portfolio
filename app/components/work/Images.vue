1c3c7b002d86467a7812d68a2c669cfa61c80783
Ivan <info@ivansmiths.com>
2026-02-26 10:38:22 +0100

    fix image spacing

--- app/components/work/Images.vue
+++ app/components/work/Images.vue
@@ -8,22 +8,25 @@ const props = defineProps<{
 
 <template>
   <div class="w-full">
-    <div class="flex h-full w-full flex-col gap-5">
+    <div class="flex h-full w-full flex-col items-center gap-5">
       <template v-for="(imagees, index) in work.images" :key="index">
         <img
           v-if="imagees.layout === 'single'"
           :alt="`${work.name} image ${index + 1}`"
           :src="imagees.src"
-          class="aspect-video w-full object-cover"
+          class="aspect-video w-8/12 object-contain"
         />
 
-        <div v-else-if="imagees.layout === 'row'" class="flex flex-col gap-5 md:flex-row">
+        <div
+          v-else-if="imagees.layout === 'row'"
+          class="flex w-full flex-col justify-center gap-5 md:flex-row"
+        >
           <img
             v-for="(src, i) in imagees.src"
             :key="src"
             :alt="`${work.name} image ${index + 1}-${i + 1}`"
             :src="src"
-            class="aspect-video w-full object-cover md:w-1/2"
+            class="aspect-video w-full object-contain md:w-5/12"
           />
         </div>
       </template>
