ce2c93e7a889e4592be108c2a324384a55ed17eb
Ivan <info@ivansmiths.com>
2026-03-03 10:48:17 +0100

    wip add hero

---
+++ app/components/about/hero/AboutHero.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import AboutText from "~/components/about/hero/AboutText.vue";
+</script>
+
+<template>
+  <div class="relative flex min-h-screen w-full flex-col items-center justify-center">
+    <AboutText />
+  </div>
+</template>
+
+<style scoped></style>
--- app/pages/about/index.vue
+++ app/pages/about/index.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import AboutClients from "~/components/about/clients/AboutClients.vue";
 import AboutDescription from "~/components/about/description/AboutDescription.vue";
+import AboutHero from "~/components/about/hero/AboutHero.vue";
 </script>
 
 <template>
-  <section>
+  <section class="bg-background px-5">
+    <AboutHero />
     <AboutDescription />
     <AboutClients />
   </section>
