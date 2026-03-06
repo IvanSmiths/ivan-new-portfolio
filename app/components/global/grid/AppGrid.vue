742247ea558a5c09dbfd3fdc3a6ce993444e7853
Ivan <info@ivansmiths.com>
2026-03-06 23:16:11 +0100

    remove max width

--- app/components/global/grid/AppGrid.vue
+++ app/components/global/grid/AppGrid.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
   <component
     :is="as ?? 'div'"
-    class="lg:gap-x-md gap-x-sm lg:px-md px-sm mx-auto grid w-full max-w-screen-2xl grid-cols-8 lg:grid-cols-16"
+    class="lg:gap-x-md gap-x-sm lg:px-md px-sm mx-auto grid w-full grid-cols-8 lg:grid-cols-16"
   >
     <slot />
   </component>
