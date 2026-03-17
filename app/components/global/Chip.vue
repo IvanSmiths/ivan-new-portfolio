24f1667906e40b9d0d668935cc45d8ba3652d0b4
Ivan <info@ivansmiths.com>
2026-03-17 10:35:06 +0100

    set optional component prop

--- app/components/global/Chip.vue
+++ app/components/global/Chip.vue
@@ -14,7 +14,7 @@ defineProps<{
 <template>
   <component
     :is="as || 'span'"
-    class="text-foreground bg-background/20 px-sm flex w-fit flex-row items-center gap-1 rounded-full border border-white/20 py-1 text-xs tracking-widest uppercase shadow-lg backdrop-blur-lg"
+    class="text-foreground bg-background/20 px-sm flex w-fit flex-row items-center gap-1 rounded-full border border-white/20 py-1 text-xs shadow-lg backdrop-blur-lg"
     v-bind="$attrs"
   >
     {{ label }}
